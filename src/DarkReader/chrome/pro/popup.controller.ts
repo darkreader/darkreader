abcdee2f61102bbf9c2ecba323f6b8426c59e9ec
Alexander Shutov <shutovby@gmail.com>
2014-12-08 23:13:13 +0300

    Controls initialization. Toggle button works (sometimes).

---
+++ src/DarkReader/chrome/pro/popup.controller.ts
@@ -0,0 +1,96 @@
+﻿module DarkReader.Chrome.Pro.Popup {
+    /**
+     * Pop-up page controller.
+     */
+    export class PopupController {
+        constructor(private app: Application<Generation.FilterConfig>) {
+            this.initControls();
+
+            this.subscriptions = [];
+            this.registerEventsHandlers();
+
+            // Subscribe for window closing
+            window.onbeforeunload = (e) => {
+                this.unregisterEventsHandlers();
+            };
+
+            // Set values for the first time
+            this.app.onConfigSetup.invoke(this.app.config);
+        }
+
+
+        //-------------------
+        // EVENT REGISTRATION
+        //-------------------
+
+        private subscriptions: Subscription<any>[];
+
+        /**
+         * Registers events handlers.
+         */
+        private registerEventsHandlers() {
+            //------------------------
+            // From background to view
+            //------------------------
+
+            // On toggle
+            this.subscriptions.push(this.app.onToggle.addHandler(isOn => {
+                this.toggleApp.toggle.isOn = isOn;
+            }, this));
+
+            // On config setup
+            this.subscriptions.push(this.app.onConfigSetup.addHandler((args) => {
+
+            }, this));
+
+
+            //------------------------
+            // From view to background
+            //------------------------
+
+            this.toggleApp.toggle.onUserToggle.addHandler((isOn) => {
+                console.log('toggle');
+                if (isOn) {
+                    this.app.enable();
+                }
+                else {
+                    this.app.disable();
+                }
+            }, this);
+        }
+
+        /**
+         * Unregisters events handlers.
+         */
+        private unregisterEventsHandlers() {
+            this.subscriptions.forEach((s) => {
+                s.event.removeHandler(s.handler);
+            });
+        }
+
+
+        //---------
+        // CONTROLS
+        //---------
+
+        toggleApp: ToggleWithStatus;
+        toggleMode: ToggleWithButtons;
+        updownBrightness: UpDown;
+        updownContrast: UpDown;
+        updownGrayscale: UpDown;
+        updownSepia: UpDown;
+        fontSet: FontSet;
+        updownTextStroke: UpDown;
+
+        private initControls() {
+            this.toggleApp = new ToggleWithStatus('toggle-app');
+            this.toggleMode = new ToggleWithButtons('toggle-mode');
+            this.updownBrightness = new UpDown('updown-brightness');
+            this.updownContrast = new UpDown('updown-contrast');
+            this.updownGrayscale = new UpDown('updown-grayscale');
+            this.updownSepia = new UpDown('updown-sepia');
+            this.fontSet = new FontSet('control-usefont');
+            this.updownTextStroke = new UpDown('updown-textstroke');
+        }
+    }
+} 
---
+++ src/DarkReader/chrome/pro/popup.controls.ts
@@ -0,0 +1,344 @@
+﻿module DarkReader.Chrome.Pro.Popup {
+    //------------
+    // UI CONTROLS
+    //------------
+
+    /**
+     * Base control.
+     */
+    export class Control {
+        protected element: HTMLElement;
+
+        constructor(elementId: string);
+        constructor(element: HTMLElement);
+        constructor(elementOrId) {
+            var element = typeof elementOrId === 'string' ?
+                document.getElementById(elementOrId)
+                : <HTMLElement>elementOrId;
+
+            if (!element)
+                throw new Error('Unable to create control. Parameter: "' + elementOrId + '".');
+
+            this.element = element;
+            this.initContent();
+            this.initEvents();
+        }
+
+        protected initContent() { }
+        protected initEvents() { }
+    }
+
+
+    /**
+     * Toggle control.
+     */
+    export class Toggle extends Control {
+        /*
+            <div class="toggle">
+                <span class="on active">On</span><span class="off">Off</span>
+            </div>
+         */
+        private elementOn: HTMLElement;
+        private elementOff: HTMLElement;
+        private _isOn: boolean = false;
+
+        protected initContent() {
+            this.elementOn = getChildByClassName(this.element, 'on');
+            this.elementOff = getChildByClassName(this.element, 'off');
+
+            if (!this.elementOn || !this.elementOff)
+                throw new Error('Unable to create toggle. Wrong DOM.');
+
+            // Set default value
+            this.isOn = false;
+        }
+
+        /**
+         * Gets or sets toggle value.
+         */
+        get isOn() {
+            return this._isOn;
+        }
+        set isOn(on) {
+            this._isOn = on;
+
+            // DOM
+            if (on) {
+                console.log('set toggle on');
+                this.elementOn.classList.add('active');
+                this.elementOff.classList.remove('active');
+            }
+            else {
+                console.log('set toggle off');
+                this.elementOff.classList.add('active');
+                this.elementOn.classList.remove('active');
+            }
+        }
+
+        protected initEvents() {
+            this.onUserToggle = new Event<boolean>();
+
+            // Switch on
+            this.elementOn.onclick = () => {
+                if (this.isOn === false) {
+                    this.onUserToggle.invoke(true);
+                }
+            };
+            // Switch off
+            this.elementOff.onclick = () => {
+                console.log('element off clicked');
+                if (this.isOn === true) {
+                    this.onUserToggle.invoke(false);
+                }
+            };
+        }
+
+        /**
+         * Fires when user toggles the control.
+         */
+        onUserToggle: Event<boolean>;
+    }
+
+
+    /**
+     * Simple button control.
+     */
+    export class Button extends Control {
+        /*
+            <span class="icon-button"></span>
+        */
+
+        protected initEvents() {
+            this.onClick = new Event<{}>();
+            this.element.onclick = (e) => {
+                this.onClick.invoke({});
+            };
+        }
+
+        /**
+         * Fires when button is clicked.
+         */
+        onClick: Event<{}>;
+    }
+
+
+    /**
+     * Status control.
+     */
+    export class Status extends Control {
+        /*
+            <p class="status">Status</p>
+         */
+
+        private _message: string;
+
+        /**
+        * Gets or sets status message.
+        */
+        get message() {
+            return this._message;
+        }
+        set message(status) {
+            this._message = status;
+
+            // DOM
+            this.element.textContent = status;
+        }
+    }
+
+
+    /**
+     * Control containing toggle and status controls.
+     */
+    export class ToggleWithStatus extends Control {
+        toggle: Toggle;
+        status: Status;
+
+        protected initContent() {
+            this.toggle = new Toggle(getChildByClassName(this.element, 'toggle'));
+            this.status = new Status(getChildByClassName(this.element, 'status'));
+        }
+    }
+
+
+    /**
+     * Specific control with toggle, status and buttons (just to fit design).
+     */
+    export class ToggleWithButtons extends ToggleWithStatus {
+        buttonOn: Button;
+        buttonOff: Button;
+
+        protected initContent() {
+            super.initContent();
+            this.buttonOn = new Button(getChildByClassName(this.element, 'button-down'));
+            this.buttonOff = new Button(getChildByClassName(this.element, 'button-up'));
+        }
+
+        protected initEvents() {
+            // Switch on
+            this.buttonOn.onClick.addHandler(() => {
+                if (!this.toggle.isOn) {
+                    this.toggle.onUserToggle.invoke(true);
+                }
+            }, this);
+            // Switch off
+            this.buttonOff.onClick.addHandler(() => {
+                if (this.toggle.isOn) {
+                    this.toggle.onUserToggle.invoke(false);
+                }
+            }, this);
+        }
+    }
+
+
+    /**
+     * Track bar.
+     */
+    export class TrackBar extends Control {
+        /*
+            <span class="trackbar">
+                <span class="value">&nbsp;</span>
+                <label>Label</label>
+            </span>
+         */
+
+        private elementValue: HTMLElement;
+        private elementLabel: HTMLElement;
+        private _value: number;
+
+        protected initContent() {
+            this.elementValue = getChildByClassName(this.element, 'value');
+            this.elementLabel = this.element.getElementsByTagName('label').item(0);
+
+            if (!this.elementValue || !this.elementLabel)
+                throw new Error('Unable to create track bar. Wrong DOM.');
+        }
+
+        // TODO: Trackbar click...
+
+        /**
+         * Gets or sets track bar value.
+         * Value must be between 0 and 100.
+         */
+        get value() {
+            return this._value;
+        }
+        set value(value) {
+            if (value < 0 || value > 100)
+                throw new Error('Track bar value must be between 0 and 100');
+
+            this._value = value;
+
+            // DOM
+            this.elementValue.style.width = value + '%';
+        }
+
+        protected initEvents() {
+            this.onUserSetValue = new Event<number>();
+        }
+
+        /**
+         * Fires when user sets value.
+         */
+        onUserSetValue: Event<number>;
+    }
+
+
+    /**
+     * Control for adjusting value using up/down buttons.
+     */
+    export class UpDown extends Control {
+        buttonUp: Button;
+        buttonDown: Button;
+        trackBar: TrackBar;
+        status: Status;
+        protected step = 10;
+
+        protected initContent() {
+            this.buttonUp = new Button(getChildByClassName(this.element, 'button-down'));
+            this.buttonDown = new Button(getChildByClassName(this.element, 'button-up'));
+            this.trackBar = new TrackBar(getChildByClassName(this.element, 'trackbar'));
+            this.status = new Status(getChildByClassName(this.element, 'status'));
+        }
+
+        protected initEvents() {
+            // Increment
+            this.buttonUp.onClick.addHandler(() => {
+                if (this.trackBar.value < 100) {
+                    var value = this.trackBar.value + this.step;
+                    if (value > 100)
+                        value = 100;
+                    this.trackBar.onUserSetValue.invoke(value);
+                }
+            }, this);
+            // Decrement
+            this.buttonDown.onClick.addHandler(() => {
+                if (this.trackBar.value > 0) {
+                    var value = this.trackBar.value - this.step;
+                    if (value < 0)
+                        value = 0;
+                    this.trackBar.onUserSetValue.invoke(value);
+                }
+            }, this);
+        }
+    }
+
+
+    /**
+     * Control for setting font.
+     */
+    export class FontSet extends Control {
+        status: Status;
+        private checkbox: HTMLInputElement;
+        private textbox: HTMLInputElement;
+
+        protected initContent() {
+            this.status = new Status(getChildByClassName(this.element, 'status'));
+            var inputs = this.element.getElementsByTagName('input');
+            this.checkbox = inputs.item(0);
+            this.textbox = inputs.item(1);
+        }
+
+        protected initEvents() {
+            // On check change
+            this.checkbox.onchange = (e) => {
+                this.onUserCheckChange.invoke(this.checkbox.checked);
+            };
+            // On text change
+            this.textbox.onchange = (e) => {
+                this.onUserTextChange.invoke(this.textbox.value);
+            };
+            // No focus on 'Enter' key
+            this.textbox.onkeypress = (e) => {
+                (<any>document.activeElement).blur();
+            };
+            // Select all text on click
+            this.textbox.onclick = (e) => {
+                this.textbox.select();
+            };
+        }
+
+        /**
+         * Fires when user changes the checkbox value.
+         */
+        onUserCheckChange: Event<boolean>;
+        /**
+         * Fires when user has edited the text.
+         */
+        onUserTextChange: Event<string>;
+    }
+
+
+    //--------
+    // HELPERS
+    //--------
+
+    /**
+     * Returns the first element's child with the given class name.
+     * @param element Container element.
+     * @param className Class name.
+     */
+    function getChildByClassName(element: HTMLElement, className: string): HTMLElement {
+        return <HTMLElement>element.getElementsByClassName(className).item(0);
+    }
+} 

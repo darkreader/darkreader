bba372e82ee957d88ea85ca8295530f2d9b86eee
Alexander Shutov <shutovby@gmail.com>
2015-09-29 23:09:00 +0300

    Extension auto-reload.

--- README.md
+++ README.md
@@ -5,52 +5,16 @@ DarkReader for Google Chrome
 This extension inverts brightness of web pages and aims to reduce eyestrain while browsing the web.
 [Visit Chrome Web Store](https://chrome.google.com/webstore/detail/dark-reader/eimadpbcbfnmbkopoojfekhnkhdbieeh) for more info.
 
-## How it works
-The extension simply adds a stylesheet which inverts colors of the whole page and reverts necessary blocks back.
-```HTML
-<style id="dark-reader-style" type="text/css">
-    html {
-        -webkit-filter: invert(100%);
-    }
-    img, iframe, video {
-        -webkit-filter: invert(100%);
-    }
-</style>
-```
-## Fixing the inversion of specific blocks
-If some blocks of web page should not be inverted, the [sites_fixes.json](https://github.com/alexanderby/darkreader/blob/master/src/config/sites_fixes.json) file should be edited. The ```url``` value contains URL-template for matching page's URL and ```selectors``` value contains [CSS selectors](https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Getting_Started/Selectors) for matching the specific blocks that should not be inverted. If there would be no matches found for page URL, then ```commonSelectors``` values would be used to generate CSS. The ```rules``` value is used to fix things which cannot be corrected with contrary revertions.
-```JSON
-﻿{
-    "commonSelectors": "img, iframe, video",
-    "specials": [
-        {
-            "url": "youtube.com",
-            "selectors": "iframe, .html5-video-player, div:not(.html5-video-player) img"
-        },
-        {
-            "url": "google.*",
-            "selectors": "img, iframe, video, span.gb_X.gbii",
-            "rules": "body { background-color: white !important; }"
-        }
-    ]
-}
-```
+## How to contribute
 
-## List of sites with dark background
-Besides using settings-page site list, dark-themed web-sites can be added into global dark sites list ([dark_sites.json](https://github.com/alexanderby/darkreader/blob/master/src/config/dark_sites.json)) to make them not inverted by default. Possible values for matching the web-site URL are listed below.
-```JSON
-﻿[
-    "google.*",
-    "mail.google.com",
-    "google.com/mail"
-]
-```
+If some site is already dark, you can add it to [dark_sites.json](https://github.com/alexanderby/darkreader/blob/master/src/config/dark_sites.json) file.
 
-## Building and debugging
-In order to build and debug the project install the [nodejs](https://nodejs.org/) and [grunt task runner](http://gruntjs.com/). Launch the ```> grunt debug``` task. Open the [Chrome's extensions page](https://support.google.com/chrome/answer/187443). Enable the **Developer mode**, press **Load unpacked extension** button, navigate to project's ```src/``` folder.
+If some parts of web-pages are wrongly inverted, you can specify necessary CSS selectors at [sites_fixes.json](https://github.com/alexanderby/darkreader/blob/master/src/config/sites_fixes.json).
 
-After making any code changes, the project should be recompiled (```debug-watch``` task may be used for auto-compile) and reloaded in **Extension page** by pressing the **Reload (Ctrl+R)** hyperlink.
+In order to build and debug the extension install the [nodejs](https://nodejs.org/) and [grunt task runner](http://gruntjs.com/). Launch the ```> grunt debug``` task. Open the [Chrome's extensions page](https://support.google.com/chrome/answer/187443). Enable the **Developer mode**, press **Load unpacked extension** button, navigate to project's ```src/``` folder.
 
-For editing the [sites_fixes.json](https://github.com/alexanderby/darkreader/blob/master/src/config/sites_fixes.json) and [dark_sites.json](https://github.com/alexanderby/darkreader/blob/master/src/config/dark_sites.json) files any text editor may be used. For editing TypeScript and LESS files you may use [Visual Studio Code](https://code.visualstudio.com), [Visual Studio Community 2013](https://www.visualstudio.com/en-us/products/visual-studio-community-vs.aspx) or any other web IDE.
+After making any code changes, the project should be recompiled (```debug-watch``` task may be used for auto-compile) and reloaded in **Extension page** by pressing the **Reload (Ctrl+R)** hyperlink ([Chrome Unpacked Extension Auto Reload](https://chrome.google.com/webstore/detail/chrome-unpacked-extension/fddfkmklefkhanofhlohnkemejcbamln) can be installed for extensions auto-reloading). 
+
+For editing the code you can use any text editor or web IDE (like [Visual Studio Code](https://code.visualstudio.com), [Visual Studio Community](https://www.visualstudio.com/en-us/products/visual-studio-community-vs.aspx), [WebStorm](https://www.jetbrains.com/webstorm/)).
 
 ## Good luck!
--- gruntfile.js
+++ gruntfile.js
@@ -10,25 +10,21 @@ module.exports = function (grunt) {
                 'build/'
             ])
             .sub('typescript', {
-                //cwd: 'src/',
                 src: config.tsFiles,
                 dest: 'build/',
-                options: { target: 'es5' },
-                //expand: true
+                options: { target: 'es5' }
             })
             .sub('less', {
                 files: {
                     'build/popup/style/style.css': 'src/popup/style/style.less'
                 },
-                options: { 
-                    paths: ['src/'] 
+                options: {
+                    paths: ['src/']
                 }
             })
             .sub('copy', {
                 cwd: 'src/',
                 src: [
-                    //'**/*.js',
-                    //'**/*.css',
                     '**/*.html',
                     'config/*.*',
                     'img/*.*',
@@ -39,34 +35,35 @@ module.exports = function (grunt) {
                 ],
                 dest: 'build/',
                 expand: true
-            });
+            })
+            .sub('ext-reload');
 
         //
         // --- Debug tasks ---
         
         create.task('debug')
             .sub('debug-js')
-            .sub('debug-css');
+            .sub('debug-css')
+            .sub('ext-reload');
 
         create.task('debug-js')
             .sub('typescript', {
                 cwd: 'src/',
                 src: config.tsFiles,
-                options: { 
-                    sourceMap: true, 
+                options: {
+                    sourceMap: true,
                     declaration: false,
-                    target: 'es5',
-                    //watch: 'src'
+                    target: 'es5'
                 }
             });
-            
+
         create.task('debug-css')
             .sub('less', {
                 files: {
                     'src/popup/style/style.css': 'src/popup/style/style.less'
                 },
-                options: { 
-                    paths: ['src/'] 
+                options: {
+                    paths: ['src/']
                 }
             });
             
@@ -79,31 +76,83 @@ module.exports = function (grunt) {
                     logConcurrentOutput: true
                 },
             });
-            
+
         create.task('debug-watch-js')
             .sub('typescript', {
                 cwd: 'src/',
                 src: config.tsFiles,
-                options: { 
-                    sourceMap: true, 
+                options: {
+                    sourceMap: true,
                     declaration: false,
                     target: 'es5',
-                    watch: 'src'
+                    watch: {
+                        path: 'src',
+                        after: ['ext-reload']
+                    }
                 }
             });
-            
+
         create.task('debug-watch-css')
             .sub('watch', {
                 files: ['src/**/*.less'],
-                tasks: ['debug-css']
-            });   
+                tasks: ['debug-css', 'ext-reload']
+            });
 
+        // --- Reload chrome extensions ---
+        create.task('ext-reload')
+            .sub(function () {
+                // Create server to communicate with Google Chrome reload extension
+                // https://chrome.google.com/webstore/detail/chrome-unpacked-extension/fddfkmklefkhanofhlohnkemejcbamln?utm_source=chrome-app-launcher-info-dialog
+                var done = this.async();
+                var server = require('http').createServer();
+                var io = require('socket.io')(server);
+                var connected = false;
+                var sockets = {};
+                var socketId = 0;
+                var closeServer = function () {
+                    server.close(function () {
+                        grunt.log.writeln('Server closed gracefully.');
+                        done();
+                    });
+                    for (var id in sockets) {
+                        sockets[id].destroy();
+                    }
+                    setTimeout(function () {
+                        grunt.log.writeln('Server was not closed gracefully.');
+                        done();
+                    }, 2000);
+                };
+                server.on('connection', function (socket) {
+                    sockets[socketId++] = socket;
+                });
+                io.sockets.on('connection', function (socket) {
+                    if (!connected) {
+                        grunt.log.writeln('Auto-reloader connected.');
+                        socket.emit('file.change', {});
+                        connected = true;
+                        setTimeout(function () {
+                            closeServer();
+                        }, 1000);
+                    }
+                });
+                io.sockets.on('error', function (e) {
+                    grunt.log.writeln('Server error: ' + e.description);
+                    closeServer();
+                });
+                server.listen(8890);
+                
+                // Close server if no listeners
+                setTimeout(function () {
+                    grunt.log.writeln('Auto-reloader did not connect.');
+                    closeServer();
+                }, 5000);
+            });
     });
 };
 
 function getConfig() {
     return {
-        tsFiles:[
+        tsFiles: [
             'src/background/background.ts',
             'src/background/config_management.ts',
             'src/background/extension.ts',

c7256c1c6ca577f30e37807c8fb5273caca55c47
Alexander Shutov <shutovby@gmail.com>
2018-02-21 20:14:00 +0300

    Bundle JS using async awaits

--- tasks/bundle-js.js
+++ tasks/bundle-js.js
@@ -1,43 +1,32 @@
-const mergeStream = require('merge-stream');
 const rollup = require('rollup');
 const rollupPluginNodeResolve = require('rollup-plugin-node-resolve');
 const rollupPluginTypescript = require('rollup-plugin-typescript');
-const rollupStream = require('rollup-stream');
-const sourceStream = require('vinyl-source-stream');
 const typescript = require('typescript');
 
 const { getDestDir } = require('./paths');
 
 module.exports = function createJSBundleTasks(gulp) {
-    gulp.task('js-release', () => bundleJS({ production: true }));
-    gulp.task('js-debug', () => bundleJS({ production: false }));
+    gulp.task('js-release', async () => await bundleJS({ production: true }));
+    gulp.task('js-debug', async () => await bundleJS({ production: false }));
 
-    function bundleJS({ production }) {
+    async function bundleJS({ production }) {
         const dir = getDestDir({ production });
         const files = {
-            'src/background/index.ts': `${dir}/background`,
-            'src/ui/popup/index.tsx': `${dir}/ui/popup`,
+            'src/background/index.ts': `${dir}/background/index.js`,
+            'src/ui/popup/index.tsx': `${dir}/ui/popup/index.js`,
         };
-        return mergeStream(
-            ...Object.keys(files)
+        await Promise.all(
+            Object.keys(files)
                 .map((src) => bundleJSEntry({
                     src,
                     dest: files[src],
-                    name: 'index.js',
                     production,
-                }))
-        );
+                })));
     }
 
-    function bundleJSEntry({ src, dest, name, production }) {
-        return rollupStream({
+    async function bundleJSEntry({ src, dest, name, production }) {
+        const bundle = await rollup.rollup({
             input: src,
-            rollup,
-            output: {
-                strict: true,
-                format: 'iife',
-                sourcemap: production ? false : 'inline',
-            },
             plugins: [
                 rollupPluginTypescript({
                     typescript,
@@ -45,8 +34,12 @@ module.exports = function createJSBundleTasks(gulp) {
                 }),
                 rollupPluginNodeResolve(),
             ].filter((x) => x)
-        })
-            .pipe(sourceStream(name))
-            .pipe(gulp.dest(dest));
+        });
+        await bundle.write({
+            file: dest,
+            strict: true,
+            format: 'iife',
+            sourcemap: production ? false : 'inline',
+        });
     }
 };

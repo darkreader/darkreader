3370844946190751025fae458d427ad6aba4f963
Alexander Shutov <shutovby@gmail.com>
2018-03-21 22:02:43 +0300

    Fixed removing SVG

--- src/inject/remove-style.ts
+++ src/inject/remove-style.ts
@@ -2,4 +2,4 @@ const style = document.getElementById('dark-reader-style');
 style && style.parentElement.removeChild(style);
 
 const svg = document.getElementById('dark-reader-svg');
-svg && svg.parentElement.removeChild(style);
+svg && svg.parentElement.removeChild(svg);

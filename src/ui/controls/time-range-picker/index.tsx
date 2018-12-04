04e9d1c23de9cc6503989b300f90a9ee1d4b8386
Alexander Shutov <shutovby@gmail.com>
2018-12-04 10:51:25 +0300

    Fixed 12 hours time parsing

--- src/ui/controls/time-range-picker/index.tsx
+++ src/ui/controls/time-range-picker/index.tsx
@@ -1,6 +1,7 @@
 import {html} from 'malevic';
 import TextBox from '../textbox';
 import {getUILanguage} from '../../../utils/locales';
+import {parseTime} from '../../../utils/time';
 
 interface TimePickerProps {
     startTime: string;
@@ -9,31 +10,6 @@ interface TimePickerProps {
 }
 
 const is12H = (new Date()).toLocaleTimeString(getUILanguage()).endsWith('M');
-
-function parseTime($time: string) {
-    const parts = $time.split(':').slice(0, 2);
-    const lowercased = $time.trim().toLowerCase();
-    const isAM = lowercased.endsWith('am') || lowercased.endsWith('a.m.');
-    const isPM = lowercased.endsWith('pm') || lowercased.endsWith('p.m.');
-
-    let hours = parts.length > 0 ? parseInt(parts[0]) : 0;
-    if (isNaN(hours) || hours > 23) {
-        hours = 0;
-    }
-    if (isAM && hours === 12) {
-        hours = 0;
-    }
-    if (isPM) {
-        hours += 12;
-    }
-
-    let minutes = parts.length > 1 ? parseInt(parts[1]) : 0;
-    if (isNaN(minutes) || minutes > 59) {
-        minutes = 0;
-    }
-
-    return [hours, minutes];
-}
 
 function toLocaleTime($time: string) {
     const [hours, minutes] = parseTime($time);
--- tests/time.tests.ts
+++ tests/time.tests.ts
@@ -1,4 +1,4 @@
-import {isInTimeInterval} from '../src/utils/time';
+import {isInTimeInterval, parseTime, getDuration} from '../src/utils/time';
 
 test('Time interval', () => {
     expect(isInTimeInterval(new Date(2018, 11, 4, 10), '9:00', '12:00')).toBe(true);
@@ -6,4 +6,24 @@ test('Time interval', () => {
     expect(isInTimeInterval(new Date(2018, 11, 4, 10), '9:00', '10:01')).toBe(true);
     expect(isInTimeInterval(new Date(2018, 11, 4, 10), '18:00', '12:00')).toBe(true);
     expect(isInTimeInterval(new Date(2018, 11, 4, 10), '18:00', '9:00')).toBe(false);
+});
+
+test('Time parse', () => {
+    expect(parseTime('10:30')).toEqual([10, 30]);
+    expect(parseTime('10:30AM')).toEqual([10, 30]);
+    expect(parseTime('10:30 a.m.')).toEqual([10, 30]);
+    expect(parseTime('10:30PM')).toEqual([22, 30]);
+    expect(parseTime('10:30 p.m.')).toEqual([22, 30]);
+    expect(parseTime('0:30')).toEqual([0, 30]);
+    expect(parseTime('12:30am')).toEqual([0, 30]);
+    expect(parseTime('12:30pm')).toEqual([12, 30]);
+});
+
+test('Duration', () => {
+    expect(getDuration({
+        seconds: 48,
+        minutes: 24,
+        hours: 8,
+        days: 3
+    })).toEqual(289488000);
 });

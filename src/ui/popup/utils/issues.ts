c0c89f26a64e9ee5c32e5bee20b8200ba0b77ef0
Alexander Shutov <shutovby@gmail.com>
2019-01-05 13:34:27 +0300

    Extension borders in Canary dark mode are black

--- src/ui/popup/utils/issues.ts
+++ src/ui/popup/utils/issues.ts
@@ -20,7 +20,7 @@ export function popupHasBuiltInHorizontalBorders() {
         !isYaBrowser() &&
         !isOpera() && (
             (isWindows() && compareChromeVersions(chromeVersion, '62.0.3167.0') >= 0) ||
-            (isMacOS() && compareChromeVersions(chromeVersion, '67.0.3373.0') >= 0)
+            (isMacOS() && compareChromeVersions(chromeVersion, '67.0.3373.0') >= 0 && compareChromeVersions(chromeVersion, '73.0.3661.0') < 0)
         )
     );
 }

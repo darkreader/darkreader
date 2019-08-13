8106428edb9171ab1024ea99b6f873bf43d9989a
Aliaksandr Shutau <shutovby@gmail.com>
2019-08-13 11:45:07 +0300

    Fixed API not working on some websites

--- src/api/chrome.ts
+++ src/api/chrome.ts
@@ -2,12 +2,24 @@ if (!window.chrome) {
     window.chrome = {} as any;
 }
 if (!window.chrome.runtime) {
-    window.chrome.runtime = {
-        sendMessage() {
-            throw new Error('Access to some of your resources was blocked by cross-origin policy');
-        },
-        onMessage: {
-            addListener: Function.prototype,
-        },
+    window.chrome.runtime = {} as any;
+}
+const throwCORSError = () => {
+    throw new Error('Access to some of your resources was blocked by cross-origin policy');
+};
+if (window.chrome.runtime.sendMessage) {
+    const nativeSendMessage = window.chrome.runtime.sendMessage;
+    window.chrome.runtime.sendMessage = (...args) => {
+        if (args[0] && args[0].type === 'fetch') {
+            throwCORSError();
+        }
+        nativeSendMessage.apply(window.chrome.runtime, args);
+    };
+} else {
+    window.chrome.runtime.sendMessage = throwCORSError
+}
+if (!window.chrome.runtime.onMessage) {
+    window.chrome.runtime.onMessage = {
+        addListener: Function.prototype,
     } as any;
 }

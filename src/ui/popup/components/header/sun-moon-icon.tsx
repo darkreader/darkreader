c9e8f600c59cfb996248037611d823dac01b46b3
Alexander Shutau <shutovby@gmail.com>
2019-09-12 11:47:00 +0300

    Simplified icons

--- src/ui/popup/components/header/sun-moon-icon.tsx
+++ src/ui/popup/components/header/sun-moon-icon.tsx
@@ -5,8 +5,15 @@ export default function SunMoonIcon({date, latitude, longitude}) {
     if (latitude == null || longitude == null) {
         // question mark icon
         return (
-            <svg viewBox="-2.5 -2.5 21 21">
-                <path fill="white" stroke="none" d="M7.78 0C10.55 0 13.28 1.27 13.28 4.32C13.28 7.14 10.05 8.21 9.36 9.23C8.84 9.99 9.02 11.05 7.59 11.05C6.66 11.05 6.21 10.29 6.21 9.6C6.21 7.03 9.99 6.45 9.99 4.32C9.99 3.16 9.21 2.46 7.91 2.46C5.14 2.46 6.23 5.32 4.13 5.32C3.38 5.32 2.73 4.86 2.73 4C2.72 1.88 5.14 0 7.79 0Z M7.68 12.45C8.65 12.45 9.45 13.25 9.45 14.23C9.45 15.2 8.65 16 7.68 16C6.7 16 5.9 15.2 5.9 14.23C5.9 13.25 6.7 12.45 7.68 12.45Z" />
+            <svg viewBox="0 0 16 16">
+                <text
+                    fill="white"
+                    font-size="16"
+                    font-weight="bold"
+                    text-anchor="middle"
+                    x="8"
+                    y="14"
+                >?</text>
             </svg>
         );
     }
@@ -22,21 +29,27 @@ export default function SunMoonIcon({date, latitude, longitude}) {
 
     // sun icon
     return (
-        <svg viewBox="-4.5 -4.5 41 41">
-            <circle fill="white" stroke="none" cx="16" cy="16" r="8" />
-            {...(Array.from({length: 8}).map((_, i) => (
-                <line
-                    x1="2"
-                    y1="16"
-                    x2="4"
-                    y2="16"
-                    stroke="white"
-                    stroke-width="5"
-                    stroke-linecap="round"
-                    transform-origin="16 16"
-                    transform={`rotate(${i * 45})`}
-                />
-            )))}
+        <svg viewBox="0 0 16 16">
+            <circle fill="white" stroke="none" cx="8" cy="8" r="3" />
+            <g fill="none" stroke="white" stroke-linecap="round" stroke-width="1.5">
+                {...(Array.from({length: 8}).map((_, i) => {
+                    const cx = 8;
+                    const cy = 8;
+                    const angle = i * Math.PI / 4 + Math.PI / 8;
+                    const pt = [5, 6].map((l) => [
+                        cx + l * Math.cos(angle),
+                        cy + l * Math.sin(angle),
+                    ]);
+                    return (
+                        <line
+                            x1={pt[0][0]}
+                            y1={pt[0][1]}
+                            x2={pt[1][0]}
+                            y2={pt[1][1]}
+                        />
+                    );
+                }))}
+            </g>
         </svg>
     );
 }

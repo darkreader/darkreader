dce206167681ebde59106af8a56773636186c93d
Alexander Shutau <shutovby@gmail.com>
2020-03-12 10:50:24 +0300

    API: DarkReader.auto() method.
    - Calling `auto(theme)` enables or disables Dark Reader according to system color scheme.
    - Calling `auto(false)` stops watching for system color scheme.

--- src/api/index.ts
+++ src/api/index.ts
@@ -17,7 +17,16 @@ const defaultTheme: Theme = {
     stylesheet: '',
 };
 
-export function enable(themeOptions: Partial<Theme>, fixes: DynamicThemeFix = null, isIFrame = false) {
+const isIFrame = (() => {
+    try {
+        return window.self !== window.top;
+    } catch (err) {
+        console.warn(err);
+        return true;
+    }
+})();
+
+export function enable(themeOptions: Partial<Theme> = {}, fixes: DynamicThemeFix = null) {
     const theme = {...defaultTheme, ...themeOptions};
 
     if (theme.engine !== ThemeEngines.dynamicTheme) {
@@ -29,6 +38,31 @@ export function enable(themeOptions: Partial<Theme>, fixes: DynamicThemeFix = nu
 
 export function disable() {
     removeDynamicTheme();
+}
+
+const darkScheme = matchMedia('(prefers-color-scheme: dark)');
+let store = {
+    themeOptions: null as Partial<Theme>,
+    fixes: null as DynamicThemeFix,
+};
+
+function handleColorScheme() {
+    if (darkScheme.matches) {
+        enable(store.themeOptions, store.fixes);
+    } else {
+        disable();
+    }
+}
+
+export function auto(themeOptions: Partial<Theme> | false = {}, fixes: DynamicThemeFix = null) {
+    if (themeOptions) {
+        store = {themeOptions, fixes};
+        handleColorScheme();
+        darkScheme.addListener(handleColorScheme);
+    } else {
+        darkScheme.removeListener(handleColorScheme);
+        disable();
+    }
 }
 
 export const setFetchMethod = setFetch;

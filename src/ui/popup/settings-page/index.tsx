d2ebc282019d216a1c5cf3d1a1f036218d3fca9c
Gusted <williamzijl7@hotmail.com>
2020-04-28 13:15:15 +0200

    PDF's don't work for Firefox So let's not show it...

--- src/ui/popup/settings-page/index.tsx
+++ src/ui/popup/settings-page/index.tsx
@@ -5,12 +5,13 @@ import EnabledByDefaultGroup from './enabled-by-default';
 import HelpGroup from './help';
 import InvertPDF from './invert-pdf';
 import ScrollbarTheming from './scrollbar';
+import {isFirefox} from '../../../utils/platform';
 
 export default function SettingsPage(props: ViewProps) {
     return (
         <section class="m-section">
             <EnabledByDefaultGroup {...props} />
-            <InvertPDF {...props} />
+            {isFirefox() ? null : <InvertPDF {...props} />}
             <ScrollbarTheming {...props} />
             <DevToolsGroup {...props} />
             <HelpGroup />

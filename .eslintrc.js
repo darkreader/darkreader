c207ca6652bdc8fea4f3a55e6d4a79f426897f90
Gusted <williamzijl7@hotmail.com>
2020-04-30 16:40:57 +0200

    Override

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     overrides: [
         {
-            files: ['tasks/**/*.js'],
+            files: ['tasks/**/*.js', 'tests/**/*.js'],
             rules: {
                 '@typescript-eslint/no-var-requires': 'off',
             },

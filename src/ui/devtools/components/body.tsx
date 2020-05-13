1898a5571888d196840522045f3fb9a1d6c90de4
Alexander Shutau <shutovby@gmail.com>
2020-05-13 18:09:26 +0300

    Fixed text indentation in Firefox

--- src/ui/devtools/components/body.tsx
+++ src/ui/devtools/components/body.tsx
@@ -3,6 +3,7 @@ import {withState, useState} from 'malevic/state';
 import {Button} from '../../controls';
 import ThemeEngines from '../../../generators/theme-engines';
 import {DEVTOOLS_DOCS_URL} from '../../../utils/links';
+import {isFirefox} from '../../../utils/platform';
 import {ExtWrapper} from '../../../definitions';
 
 type BodyProps = ExtWrapper;
@@ -38,7 +39,20 @@ function Body({data, actions}: BodyProps) {
         node.addEventListener('keydown', (e) => {
             if (e.key === 'Tab') {
                 e.preventDefault();
-                document.execCommand('insertText', false, '    ');
+                const indent = ' '.repeat(4);
+                if (isFirefox()) {
+                    // https://bugzilla.mozilla.org/show_bug.cgi?id=1220696
+                    const start = node.selectionStart;
+                    const end = node.selectionEnd;
+                    const before = node.value.substring(0, start);
+                    const after = node.value.substring(end);
+                    node.focus();
+                    node.value = `${before}${indent}${after}`;
+                    const cursorPos = start + indent.length;
+                    node.setSelectionRange(cursorPos, cursorPos);
+                } else {
+                    document.execCommand('insertText', false, indent);
+                }
             }
         });
     }

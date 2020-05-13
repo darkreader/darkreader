227f66ba3998289642b1f080aba48687a4c1933c
Gusted <williamzijl7@hotmail.com>
2020-05-13 21:02:41 +0200

    REEEE

--- src/utils/ipv6.ts
+++ src/utils/ipv6.ts
@@ -1,17 +1,17 @@
 /**
  * Compares if IPV6 addresses are the same.
- * @param firstIp First IP to compare with
- * @param secondIp Second IP to compare with
+ * @param firstIP First IP to compare with
+ * @param secondIP Second IP to compare with
  */
-export function CompareIPV6(firstIp: string, secondIp: string) {
-    firstIp = (firstIp
+export function compareIPV6(firstIP: string, secondIP: string) {
+    firstIP = (firstIP
         .replace(/^\^/, '') // Remove ^ at start
         .replace(/\$$/, '') // Remove $ at end
         .replace(/^.*?\/{2,3}/, '') // Remove scheme
         .replace(/\?.*$/, '') // Remove query
         .replace(/\/$/, '') // Remove last slash
     );
-    const ip = secondIp.toLowerCase();
-    const ip2 = firstIp.toLowerCase();
+    const ip = secondIP.toLowerCase();
+    const ip2 = firstIP.toLowerCase();
     return ip === ip2;
 }

3c3b0c8139c1fd333654ceeac475c5a873da921a
Alexander Shutau <shutovby@gmail.com>
2020-06-05 16:25:00 +0300

    Move theme controls into collapsible groups (#2736)

--- src/ui/popup/theme/page/index.tsx
+++ src/ui/popup/theme/page/index.tsx
@@ -1,17 +1,20 @@
 import {m} from 'malevic';
 import {DEFAULT_SETTINGS} from '../../../../defaults';
+import {Theme} from '../../../../definitions';
 import {ViewProps} from '../../types';
-import {Brightness, Contrast, Grayscale, Mode, Scheme, Sepia, Scrollbar, SelectionColorEditor} from '../controls';
+import {Brightness, Contrast, Grayscale, Mode, ResetButton, Scheme, Scrollbar, SelectionColorEditor, Sepia} from '../controls';
 import ThemePresetPicker from '../preset-picker';
 import {getCurrentThemePreset} from '../utils';
-import ResetButton from '../controls/reset-button';
+import Collapsible from './collapsible-panel';
 
-export default function ThemePage(props: ViewProps) {
-    const {theme, change} = getCurrentThemePreset(props);
+interface ThemeGroupProps {
+    theme: Theme;
+    change: (theme: Partial<Theme>) => void;
+}
 
+function MainGroup({theme, change}: ThemeGroupProps) {
     return (
-        <section class="m-section theme-page">
-            <ThemePresetPicker {...props} />
+        <Array>
             <Brightness
                 value={theme.brightness}
                 onChange={(v) => change({brightness: v})}
@@ -36,16 +39,41 @@ export default function ThemePage(props: ViewProps) {
                 mode={theme.engine}
                 onChange={(mode) => change({engine: mode})}
             />
+        </Array>
+    );
+}
+
+function ColorsGroup({theme, change}: ThemeGroupProps) {
+    return (
+        <Array>
             <Scrollbar
                 value={theme.scrollbarColor}
-                onChange={(v) => props.actions.setTheme({scrollbarColor: v})}
-                onReset={() => props.actions.setTheme({scrollbarColor: DEFAULT_SETTINGS.theme.scrollbarColor})}
+                onChange={(v) => change({scrollbarColor: v})}
+                onReset={() => change({scrollbarColor: DEFAULT_SETTINGS.theme.scrollbarColor})}
             />
             <SelectionColorEditor
                 value={theme.selectionColor}
-                onChange={(v) => props.actions.setTheme({selectionColor: v})}
-                onReset={() => props.actions.setTheme({selectionColor: DEFAULT_SETTINGS.theme.selectionColor})}
+                onChange={(v) => change({selectionColor: v})}
+                onReset={() => change({selectionColor: DEFAULT_SETTINGS.theme.selectionColor})}
             />
+        </Array>
+    );
+}
+
+export default function ThemePage(props: ViewProps) {
+    const {theme, change} = getCurrentThemePreset(props);
+
+    return (
+        <section class="m-section theme-page">
+            <ThemePresetPicker {...props} />
+            <Collapsible>
+                <Collapsible.Group id="main" label="Brightness, contrast, mode">
+                    <MainGroup theme={theme} change={change} />
+                </Collapsible.Group>
+                <Collapsible.Group id="colors" label="Colors">
+                    <ColorsGroup theme={theme} change={change} />
+                </Collapsible.Group>
+            </Collapsible>
             <ResetButton {...props} />
         </section>
     );

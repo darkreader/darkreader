285da998bfc4f9040b9d3373a82116baecdeb6fe
Gusted <williamzijl7@hotmail.com>
2020-06-12 13:33:56 +0200

    no message

--- src/inject/dynamic-theme/adopted-style-manger.ts
+++ src/inject/dynamic-theme/adopted-style-manger.ts
@@ -5,18 +5,18 @@ import {forEach} from '../../utils/array';
 const adoptedSheetOverride = new WeakMap<CSSStyleSheet, CSSStyleSheet>();
 
 export function createAdoptedStyleSheetOverride(node: Document | ShadowRoot, theme: Theme, variables: Map<string, string>): void {
+    let index = 0;
     forEach(node.adoptedStyleSheets, (sheet) => {
-
+        const currentIndex = index;
         if (adoptedSheetOverride.has(sheet)) {
             const exisiting = node.adoptedStyleSheets as any;
             node.adoptedStyleSheets = [...exisiting, adoptedSheetOverride.get(sheet)];
+            index++;
             return;
         }
         const rules = sheet.rules;
         const override = new CSSStyleSheet();
 
-        // TODO Research:
-        // Will this not make a lot of functions and increase a lot of memory?
         function prepareOverridesSheet() {
             const exisiting = node.adoptedStyleSheets as any;
             node.adoptedStyleSheets = [...exisiting, override];
@@ -33,5 +33,6 @@ export function createAdoptedStyleSheetOverride(node: Document | ShadowRoot, the
             force: false,
             isAsyncCancelled: () => false,
         });
+        index++;
     })
 }

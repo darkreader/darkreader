bbe19d6e9d38783c0bf633bc366bc53bdc263909
Gusted <williamzijl7@hotmail.com>
2020-06-12 16:18:28 +0200

    Better Engine detection (#2728)
    - If you had `Only for x` on and custom engine it will now open the correct engine open for the correspond site.

--- src/ui/devtools/components/body.tsx
+++ src/ui/devtools/components/body.tsx
@@ -4,22 +4,25 @@ import {Button} from '../../controls';
 import ThemeEngines from '../../../generators/theme-engines';
 import {DEVTOOLS_DOCS_URL} from '../../../utils/links';
 import {isFirefox} from '../../../utils/platform';
-import {ExtWrapper} from '../../../definitions';
+import {ExtWrapper, TabInfo} from '../../../definitions';
+import {isURLInList} from '../../../utils/url';
 
-type BodyProps = ExtWrapper;
+type BodyProps = ExtWrapper & {tab: TabInfo};
 
-function Body({data, actions}: BodyProps) {
+function Body({data, tab, actions}: BodyProps) {
     const {state, setState} = useState({errorText: null as string});
     let textNode: HTMLTextAreaElement;
     const previewButtonText = data.settings.previewNewDesign ? 'Switch to old design' : 'Preview new design';
-
-    const wrapper = (data.settings.theme.engine === ThemeEngines.staticTheme
+    const custom = data.settings.customThemes.find(({url: urlList}) => isURLInList(tab.url, urlList));
+    const theme = custom ? custom.theme : data.settings.theme;
+    
+    const wrapper = (theme.engine === ThemeEngines.staticTheme
         ? {
             header: 'Static Theme Editor',
             fixesText: data.devtools.staticThemesText,
             apply: (text) => actions.applyDevStaticThemes(text),
             reset: () => actions.resetDevStaticThemes(),
-        } : data.settings.theme.engine === ThemeEngines.cssFilter || data.settings.theme.engine === ThemeEngines.svgFilter ? {
+        } : theme.engine === ThemeEngines.cssFilter || theme.engine === ThemeEngines.svgFilter ? {
             header: 'Inversion Fix Editor',
             fixesText: data.devtools.filterFixesText,
             apply: (text) => actions.applyDevInversionFixes(text),

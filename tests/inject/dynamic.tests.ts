cbd561ef6da0b7af363e796d5905cadc17f58a92
Alexander Shutau <shutovby@gmail.com>
2020-07-09 16:59:14 +0300

    Fixed resolving circular CSS variables (#3056)
    - Fixed stack overflow on web.dev

--- tests/inject/dynamic.tests.ts
+++ tests/inject/dynamic.tests.ts
@@ -1,4 +1,5 @@
 import {getAbsoluteURL} from '../../src/inject/dynamic-theme/url';
+import {replaceCSSVariables} from '../../src/inject/dynamic-theme/css-rules';
 
 test('Absolute URL', () => {
     expect(getAbsoluteURL('https://www.google.com', 'image.jpg')).toBe('https://www.google.com/image.jpg');
@@ -21,4 +22,76 @@ test('Absolute URL', () => {
     expect(getAbsoluteURL('https://www.google.com/path/page.html', '../image.jpg')).toBe('https://www.google.com/image.jpg');
     expect(getAbsoluteURL('path/index.html', 'image.jpg')).toBe(`${location.origin}/path/image.jpg`);
     expect(getAbsoluteURL('path/index.html', '/image.jpg?size=128')).toBe(`${location.origin}/image.jpg?size=128`);
+});
+
+test('Replace CSS variables', () => {
+    expect(
+        replaceCSSVariables(
+            'body { background: var(--bg); color: var(--text); }',
+            new Map(),
+        )
+    ).toBe('body { background: var(--bg); color: var(--text); }');
+
+    expect(
+        replaceCSSVariables(
+            'body { background: var(--bg); color: var(--text); } h1 { color: var(--text); }',
+            new Map([['--bg', 'white'], ['--text', 'black']]),
+        )
+    ).toBe('body { background: white; color: black; } h1 { color: black; }');
+
+    expect(
+        replaceCSSVariables(
+            'body { color: var(--text, red); }',
+            new Map([['--text', 'black']]),
+        )
+    ).toBe('body { color: black; }');
+
+    expect(
+        replaceCSSVariables(
+            'body { color: var(--text, red); }',
+            new Map(),
+        )
+    ).toBe('body { color: red; }');
+
+    expect(
+        replaceCSSVariables(
+            'body { color: var(--text, var(--alert)); }',
+            new Map([['--alert', 'red']]),
+        )
+    ).toBe('body { color: red; }');
+
+    expect(
+        replaceCSSVariables(
+            'body { color: var(--text, var(--alert, green)); }',
+            new Map(),
+        )
+    ).toBe('body { color: green; }');
+
+    expect(
+        replaceCSSVariables(
+            'body { color: var(--text); }',
+            new Map([['--text', 'var(--text)']]),
+        )
+    ).toBe('body { color: var(--text); }');
+
+    expect(
+        replaceCSSVariables(
+            'body { color: var(--text); }',
+            new Map([['--text', 'var(--alert)'], ['--alert', 'var(--text)']]),
+        )
+    ).toBe('body { color: var(--text); }');
+
+    expect(
+        replaceCSSVariables(
+            'body { color: var(--text, var(--alert)); } h1 { color: var(--text); }',
+            new Map([['--text', 'var(--alert)'], ['--alert', 'var(--text)']]),
+        )
+    ).toBe('body { color: var(--text); } h1 { color: var(--text); }');
+
+    expect(
+        replaceCSSVariables(
+            'body { color: var(--text); } h1 { color: var(--alert); }',
+            new Map([['--text', 'var(--alert)'], ['--alert', 'red']]),
+        )
+    ).toBe('body { color: red; } h1 { color: red; }');
 });

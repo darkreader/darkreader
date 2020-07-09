2d1a1610064c22ba7e2ccfec0467c9ab3ac87d7b
Alexander Shutau <shutovby@gmail.com>
2020-07-09 14:08:18 +0300

    Fixed resolving absolute paths
    - Fixed resolving "/index.html" base paths again.
    - Use native URL class for parsing URLs.
    - Note: it can throw an error for invalid URLs.

--- src/inject/dynamic-theme/url.ts
+++ src/inject/dynamic-theme/url.ts
@@ -1,41 +1,25 @@
-export function parseURL(url: string) {
-    const a = document.createElement('a');
-    a.href = url;
-    return a;
-}
-
-function backwards($base: string, $relative: string) {
-    const b = parseURL($base);
-    let pathParts = b.pathname.split('/');
-    pathParts = pathParts.concat(...$relative.split('/')).filter((p) => p);
-    let backwardIndex: number;
-    while ((backwardIndex = pathParts.indexOf('..')) > 0) {
-        pathParts.splice(backwardIndex - 1, 2);
+export function parseURL($url: string) {
+    if ($url.startsWith('//')) {
+        $url = `${location.protocol}${$url}`;
     }
-    return pathParts;
+    return new URL($url);
 }
 
 export function getAbsoluteURL($base: string, $relative: string) {
-    if ($relative.match(/^.*?\/\//) || $relative.match(/^data\:/)) {
-        if ($relative.startsWith('//')) {
-            if ($relative.includes('..')) {
-                return `${location.protocol}${backwards($base, $relative).join('/')}`;
-            }
-            return `${location.protocol}${$relative}`;
-        }
+    if ($relative.match(/^data\:/)) {
         return $relative;
     }
+
+    if ($relative.match(/^.*?\/\//)) {
+        return parseURL($relative).href;
+    }
+
     const b = parseURL($base);
     if ($relative.startsWith('/')) {
-        const u = parseURL(`${b.protocol}//${b.host}${$relative}`);
-        return u.href;
+        return parseURL(`${b.origin}${$relative}`).href;
     }
-    let pathParts = b.pathname.split('/');
-    const lastPathPart = pathParts[pathParts.length - 1];
-    if (lastPathPart.match(/\.[a-z]+$/i)) {
-        pathParts.pop();
-    }
-    pathParts = backwards($base, $relative);
-    const u = parseURL(`${b.protocol}//${b.host}/${pathParts.join('/')}`);
-    return u.href;
+
+    const basePath = b.pathname.replace(/\/[^\/]+\.[a-z]+$/i, '');
+    const fullPath = `${basePath}${b.pathname.endsWith('/') ? '' : '/'}${$relative}`;
+    return parseURL(`${b.origin}${fullPath}`).href;
 }
--- tests/inject/dynamic.tests.ts
+++ tests/inject/dynamic.tests.ts
@@ -1,10 +1,18 @@
 import {getAbsoluteURL} from '../../src/inject/dynamic-theme/url';
 
 test('Absolute URL', () => {
+    expect(getAbsoluteURL('https://www.google.com', 'image.jpg')).toBe('https://www.google.com/image.jpg');
     expect(getAbsoluteURL('https://www.google.com', '/image.jpg')).toBe('https://www.google.com/image.jpg');
     expect(getAbsoluteURL('https://www.google.com/path', '/image.jpg')).toBe('https://www.google.com/image.jpg');
+    expect(getAbsoluteURL('//www.google.com', '/image.jpg')).toBe(`${location.protocol}//www.google.com/image.jpg`);
     expect(getAbsoluteURL('https://www.google.com/path', 'image.jpg')).toBe('https://www.google.com/path/image.jpg');
     expect(getAbsoluteURL('https://www.google.com/long/path', '../image.jpg')).toBe('https://www.google.com/long/image.jpg');
+    expect(getAbsoluteURL('https://www.google.com/long/path', '../another/image.jpg')).toBe('https://www.google.com/long/another/image.jpg');
+    expect(getAbsoluteURL('https://www.google.com/path/page.html', '/image.jpg')).toBe('https://www.google.com/image.jpg');
+    expect(getAbsoluteURL('https://www.google.com', '//www.google.com/path/image.jpg')).toBe(`${location.protocol}//www.google.com/path/image.jpg`);
+    expect(getAbsoluteURL('https://www.google.com', '//www.google.com/path/../another/image.jpg')).toBe(`${location.protocol}//www.google.com/another/image.jpg`);
+    expect(getAbsoluteURL('https://www.google.com', 'https://www.google.com/path/image.jpg')).toBe('https://www.google.com/path/image.jpg');
+    expect(getAbsoluteURL('https://www.google.com', 'https://www.google.com/path/../another/image.jpg')).toBe('https://www.google.com/another/image.jpg');
     expect(getAbsoluteURL('https://www.google.com/path/page.html', 'image.jpg')).toBe('https://www.google.com/path/image.jpg');
     expect(getAbsoluteURL('https://www.google.com/path/page.html', '../image.jpg')).toBe('https://www.google.com/image.jpg');
 });

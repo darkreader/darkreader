258c026911e26e68819ff5090b195c7795c3c2b3
Gusted <williamzijl7@hotmail.com>
2020-07-16 15:29:25 +0200

    Bug Fix

--- src/inject/dynamic-theme/css-collection.ts
+++ src/inject/dynamic-theme/css-collection.ts
@@ -4,7 +4,7 @@ function beautify(text: string) {
     const CSS = (text
         .replace(/\{/g,'{%--%') // {
         .replace(/\}/g,'%--%}%--%') // }
-        .replace(/\;/g,';%--%') // ;
+        .replace(/\;(?![^"]*")/g,';%--%') // ; and do not target between " "
         .replace(/%--%\s{0,}%--%/g,'%--%') // Remove %--% Without any characters between to the next %--%
         .split('%--%'));
     let deep = 0;

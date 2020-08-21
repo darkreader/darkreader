e8f5a4f0a8d6d911aea9f1aacd9bb749d564a55f
Gusted <williamzijl7@hotmail.com>
2020-08-21 23:01:04 +0200

    Update math.ts

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -10,7 +10,7 @@ export function multiplyMatrices(m1: number[][], m2: number[][]) {
     const result: number[][] = [];
     for (let i = 0, len = m1.length; i < len; i++) {
         result[i] = [];
-        for (let j = 0, len2 = m2[0].length ; j < len2; j++) {
+        for (let j = 0, len2 = m2[0].length; j < len2; j++) {
             let sum = 0;
             for (let k = 0, len3 = m1[0].length; k < len3; k++) {
                 sum += m1[i][k] * m2[k][j];

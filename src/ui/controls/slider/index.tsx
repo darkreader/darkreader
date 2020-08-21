219afaeef63e298def84e3e132bc9ceb97a02c7e
Gusted <williamzijl7@hotmail.com>
2020-08-21 14:16:10 +0200

    no message

--- src/ui/controls/slider/index.tsx
+++ src/ui/controls/slider/index.tsx
@@ -178,7 +178,6 @@ export default function Slider(props: SliderProps) {
         onChange(finalValue);
         store.activeValue = finalValue;
         context.refresh();
-        return;
     }
 
     return (

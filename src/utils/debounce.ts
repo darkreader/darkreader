be61333f3b560ca96172fab642c367e4b6614610
Gusted <williamzijl7@hotmail.com>
2020-09-14 19:46:51 +0200

    NPM Update @types/chrome                     0.0.122  →  0.0.123 @types/jest                        26.0.9  →  26.0.13 @types/node                       14.0.27  →  14.10.1 @typescript-eslint/eslint-plugin    3.9.0  →    4.1.1 @typescript-eslint/parser           3.9.0  →    4.1.1 eslint                              7.7.0  →    7.9.0 jest                               26.4.0  →   26.4.2 prettier                            2.0.5  →    2.1.1 rollup                             2.25.0  →  2.26.11 ts-jest                            26.2.0  →   26.3.0 ts-node                            8.10.2  →    9.0.0 typescript                          3.9.7  →    4.0.2
    Special notice to update from Typescript 3.9 -> Typescript 4.0

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,4 +1,4 @@
-export function debounce<F extends (...args: any[]) => any>(delay: number, fn: F): F {
+export function debounce<F extends(...args: any[]) => any>(delay: number, fn: F): F {
     let timeoutId: number = null;
     return ((...args: any[]) => {
         if (timeoutId) {

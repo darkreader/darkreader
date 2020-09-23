4c0040f1ad336e7a790ff4aab3aeaba519c9639a
Gusted <williamzijl7@hotmail.com>
2020-09-23 21:31:47 +0200

    Improve mechanism for Fallback style (#3691)
    - Use the `createNodeAsap` logic to make sure the fallback will be injected correctly into the site.
    - Use the logic to already create a `head` and append the fallback style into that.
    - Added a extra check to just inject into HTML sites.
    - Resolves #3653
    - Resolves #3593

--- src/inject/fallback.ts
+++ src/inject/fallback.ts
@@ -1,11 +1,33 @@
+import {createNodeAsap} from './utils/dom';
+
+const fallBackCSS = 'html, body, body :not(iframe) { background-color: #181a1b !important; border-color: #776e62 !important; color: #e8e6e3 !important; }';
+
 if (
     matchMedia('(prefers-color-scheme: dark)').matches &&
-    !document.querySelector('.darkreader--fallback')
+    !document.querySelector('.darkreader--fallback') &&
+    document.documentElement instanceof HTMLHtmlElement
 ) {
-    const fallbackStyle = document.createElement('style');
-    fallbackStyle.textContent = 'html, body, body :not(iframe) { background-color: #181a1b !important; border-color: #776e62 !important; color: #e8e6e3 !important; }';
-    document.documentElement.appendChild(fallbackStyle);
-    fallbackStyle.classList.add('darkreader');
-    fallbackStyle.classList.add('darkreader--fallback');
-    fallbackStyle.media = 'screen';
+    createOrUpdateFallback();
+}
+
+function createOrUpdateFallback() {
+    createNodeAsap({
+        selectNode: () => document.querySelector('.darkreader--fallback'),
+        createNode: (target) => {
+            const fallbackStyle = document.createElement('style');
+            fallbackStyle.textContent = fallBackCSS;
+            target.appendChild(fallbackStyle);
+            fallbackStyle.classList.add('darkreader');
+            fallbackStyle.classList.add('darkreader--fallback');
+            fallbackStyle.media = 'screen';
+        },
+        updateNode: () => Function.prototype,
+        selectTarget: () => document.head,
+        createTarget: () => {
+            const head = document.createElement('head');
+            document.documentElement.insertBefore(head, document.documentElement.firstElementChild);
+            return head;
+        },
+        isTargetMutation: (mutation) => mutation.target.nodeName.toLowerCase() === 'head',
+    });
 }

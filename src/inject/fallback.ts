9454b0bebb1ed0f82896a973d896f76930c43899
Gusted <williamzijl7@hotmail.com>
2020-09-23 09:08:59 +0200

    Use anothermethod to inject Fallback - Use the CreateNodeAsap function to make sure the fallback will be injected correctly into the site.

--- src/inject/fallback.ts
+++ src/inject/fallback.ts
@@ -1,11 +1,34 @@
+import {createNodeAsap, removeNode} from "./utils/dom";
+
 if (
     matchMedia('(prefers-color-scheme: dark)').matches &&
     !document.querySelector('.darkreader--fallback')
 ) {
-    const fallbackStyle = document.createElement('style');
-    fallbackStyle.textContent = 'html, body, body :not(iframe) { background-color: #181a1b !important; border-color: #776e62 !important; color: #e8e6e3 !important; }';
-    document.documentElement.appendChild(fallbackStyle);
-    fallbackStyle.classList.add('darkreader');
-    fallbackStyle.classList.add('darkreader--fallback');
-    fallbackStyle.media = 'screen';
+    createOrUpdateFallback('html, body, body :not(iframe) { background-color: #181a1b !important; border-color: #776e62 !important; color: #e8e6e3 !important; }');
+}
+
+export function createOrUpdateFallback(css: string) {
+    createNodeAsap({
+        selectNode: () => document.querySelector('darkreader--fallback'),
+        createNode: (target) => {
+            const fallbackStyle = document.createElement('style');
+            fallbackStyle.textContent = css;
+            target.appendChild(fallbackStyle);
+            fallbackStyle.classList.add('darkreader');
+            fallbackStyle.classList.add('darkreader--fallback');
+            fallbackStyle.media = 'screen';
+        },
+        updateNode: (existing) => {
+            if (css.replace(/^\s+/gm, '') !== existing.textContent.replace(/^\s+/gm, '')) {
+                existing.textContent = css;
+            }
+        },
+        selectTarget: () => document.head,
+        createTarget: () => {
+            const head = document.createElement('head');
+            document.documentElement.insertBefore(head, document.documentElement.firstElementChild);
+            return head;
+        },
+        isTargetMutation: (mutation) => mutation.target.nodeName.toLowerCase() === 'head',
+    });
 }

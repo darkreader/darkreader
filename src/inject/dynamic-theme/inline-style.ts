95befcbf327deb24f055d6254c6a1c5eefb9d30f
Gusted <williamzijl7@hotmail.com>
2020-10-15 23:23:49 +0200

    Throttle Inline attribute - Max trottle the inline attribute changer to 50 attributes in 10 seconds. - Should partially resolve conflicts with certain elements that intend to revert our changes to a element. - Resolves #3864 - Resolves #1222

--- src/inject/dynamic-theme/inline-style.ts
+++ src/inject/dynamic-theme/inline-style.ts
@@ -4,6 +4,8 @@ import {iterateCSSDeclarations} from './css-rules';
 import {getModifiableCSSDeclaration} from './modify-css';
 import {FilterConfig} from '../../definitions';
 import {IS_SHADOW_DOM_SUPPORTED} from '../../utils/platform';
+import {getDuration} from '../../utils/time';
+import {throttle} from '../utils/throttle';
 
 interface Overrides {
     [cssProp: string]: {
@@ -168,7 +170,14 @@ function deepWatchForInlineStyles(
     });
     treeObservers.set(root, treeObserver);
 
-    const attrObserver = new MutationObserver((mutations) => {
+    let attributecount = 0;
+    let start = null;
+    const ATTEMPTS_INTERVAL = getDuration({seconds: 10});
+    const MAX_ATTEMPTS_COUNT = 50;
+    let cache: MutationRecord[];
+    let timeoutId: number = null;
+
+    const handleAttributionMutations = throttle((mutations: MutationRecord[]) => {
         mutations.forEach((m) => {
             if (INLINE_STYLE_ATTRS.includes(m.attributeName)) {
                 elementStyleDidChange(m.target as HTMLElement);
@@ -177,6 +186,31 @@ function deepWatchForInlineStyles(
                 .filter(({store, dataAttr}) => store.has(m.target) && !(m.target as HTMLElement).hasAttribute(dataAttr))
                 .forEach(({dataAttr}) => (m.target as HTMLElement).setAttribute(dataAttr, ''));
         });
+    });
+    const attrObserver = new MutationObserver((mutations) => {
+        if (timeoutId) {
+            cache.push(...mutations);
+            return;
+        }
+        attributecount++;
+        const now = Date.now();
+        if (start == null) {
+            start = now;
+        } else if (attributecount >= MAX_ATTEMPTS_COUNT) {
+            if (now - start < ATTEMPTS_INTERVAL) {
+                timeoutId = setTimeout(() => {
+                    start = null;
+                    attributecount = 0;
+                    timeoutId = null;
+                    handleAttributionMutations(cache);
+                }, 2000);
+                cache.push(...mutations);
+                return;
+            }
+            start = now;
+            attributecount = 1;
+        }
+        handleAttributionMutations(mutations);
     });
     attrObserver.observe(root, {
         attributes: true,

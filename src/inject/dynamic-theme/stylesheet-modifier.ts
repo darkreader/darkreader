824dde918106e3fe3a325b235cb9bf68eda0ebc1
Gusted <williamzijl7@hotmail.com>
2020-10-22 15:48:43 +0200

    Optimize Temp Style usage (#3874)
    - Re-use the temporarily CSStylesheet so it won't need to rely on slow DOM 'document.append' or creating a new CSSStyleSheet object.
    - Don't cache the HTMLStyleElement, but instead make a copy of the .sheet of the style and then remove the element again to have a valid/use-able CSSStyleSheet.
    - Don't return a new CSSStyleSheet(when supported) instead cache it so it can be re-used.
    - Attempt to fix #3870

--- src/inject/dynamic-theme/stylesheet-modifier.ts
+++ src/inject/dynamic-theme/stylesheet-modifier.ts
@@ -20,17 +20,22 @@ function getThemeKey(theme: Theme) {
     return themeCacheKeys.map((p) => `${p}:${theme[p]}`).join(';');
 }
 
-function getTempCSSStyleSheet(): {sheet: CSSStyleSheet; remove: () => void} {
-    if (isCSSStyleSheetConstructorSupported) {
-        return {sheet: new CSSStyleSheet(), remove: () => null};
+let tempStyle: CSSStyleSheet = null;
+
+function getTempCSSStyleSheet(): CSSStyleSheet {
+    if (tempStyle) {
+        return tempStyle;
     }
-    const style = document.createElement('style');
-    style.classList.add('darkreader');
-    style.classList.add('darkreader--temp');
-    style.media = 'screen';
-    style.textContent = '';
-    (document.head || document).append(style);
-    return {sheet: style.sheet, remove: () => style.remove()};
+    if (isCSSStyleSheetConstructorSupported) {
+        tempStyle = new CSSStyleSheet();
+        return tempStyle;
+    } else {
+        const tempStyleElement = document.createElement('style');
+        document.head.append(tempStyleElement);
+        tempStyle = tempStyleElement.sheet;
+        document.head.removeChild(tempStyleElement);
+        return tempStyle;
+    }
 }
 
 const asyncQueue = createAsyncTasksQueue();
@@ -73,7 +78,7 @@ export function createStyleSheetModifier() {
 
             // Put CSS text with inserted CSS variables into separate <style> element
             // to properly handle composite properties (e.g. background -> background-color)
-            let vars: {sheet: CSSStyleSheet; remove: () => void};
+            let vars: CSSStyleSheet;
             let varsRule: CSSStyleRule = null;
             if (variables.size > 0 || cssText.includes('var(')) {
                 const cssTextWithVariables = replaceCSSVariables(cssText, variables);
@@ -81,8 +86,8 @@ export function createStyleSheetModifier() {
                     rulesTextCache.set(cssText, cssTextWithVariables);
                     textDiffersFromPrev = true;
                     vars = getTempCSSStyleSheet();
-                    vars.sheet.insertRule(cssTextWithVariables);
-                    varsRule = vars.sheet.cssRules[0] as CSSStyleRule;
+                    vars.insertRule(cssTextWithVariables);
+                    varsRule = vars.cssRules[0] as CSSStyleRule;
                 }
             }
 
@@ -110,7 +115,7 @@ export function createStyleSheetModifier() {
             }
             rulesModCache.set(cssText, modRule);
 
-            vars && vars.remove();
+            vars && vars.deleteRule(0);
         });
 
         notFoundCacheKeys.forEach((key) => {

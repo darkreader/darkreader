cd2aa9750cd92d5ab5ffe09059abc99d8f29c8ed
Arnaud Lier <zeprofdecoding@gmail.com>
2020-11-18 23:22:13 +0100

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ import ThemeEngines from '../generators/theme-engines';
 import {createOrUpdateDynamicTheme, removeDynamicTheme} from '../inject/dynamic-theme';
 import {collectCSS} from '../inject/dynamic-theme/css-collection';
 
-let isEnabled = false;
+let isDarkReaderEnabled = false;
 const isIFrame = (() => {
     try {
         return window.self !== window.top;
@@ -23,16 +23,16 @@ export function enable(themeOptions: Partial<Theme> = {}, fixes: DynamicThemeFix
         throw new Error('Theme engine is not supported.');
     }
     createOrUpdateDynamicTheme(theme, fixes, isIFrame);
-    enabled = true;
+    isDarkReaderEnabled = true;
 }
 
 export function isEnabled() {
-    return enabled;
+    return isDarkReaderEnabled;
 }
 
 export function disable() {
     removeDynamicTheme();
-    enabled = false;
+    isDarkReaderEnabled = false;
 }
 
 const darkScheme = matchMedia('(prefers-color-scheme: dark)');

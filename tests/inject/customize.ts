308dea7ec504b7efe83f59d45fc3bfe4944bc7af
Alexander Shutau <shutovby@gmail.com>
2020-11-19 13:30:41 +0300

    Use dark colors for Karma launcher (#4197)

---
+++ tests/inject/customize.ts
@@ -0,0 +1,10 @@
+(() => {
+    const topDoc = window.top.document;
+    const style = topDoc.createElement('style');
+    style.textContent = [
+        'body { background-color: #222222; color: #dddddd; }',
+        '#banner { background-color: #226644; }',
+        '.executing { background-color: #662233; }',
+    ].join('\n');
+    topDoc.head.append(style);
+})();

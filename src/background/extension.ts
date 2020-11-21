0af068c0b9b26b498f979948c924e75b32d133da
Alex <8125011+proteriax@users.noreply.github.com>
2020-11-21 05:55:58 -0500

    Update extension.ts

--- src/background/extension.ts
+++ src/background/extension.ts
@@ -49,7 +49,7 @@ export class Extension {
             },
             onColorSchemeChange: this.onColorSchemeChange,
         });
-        this.user = new UserStorage({onRemoteSettingsChange: () => this.onSettingsChanged});
+        this.user = new UserStorage({onRemoteSettingsChange: () => this.onSettingsChanged()});
         this.awaiting = [];
     }
 

2daa02b6ac6e65ac92c4b7e3abc5d43fd113b90c
Jake <git@ja-ke.tech>
2020-12-03 23:00:33 +0100

    Revert "Fix fallback script condition"
    This reverts commit 1e3c27a364c1e5804090bc2f6c0eded55e6c4f94.

--- src/inject/fallback.ts
+++ src/inject/fallback.ts
@@ -1,8 +1,6 @@
-import {isThunderbird} from '../utils/platform';
-
 if (
     document.documentElement instanceof HTMLHtmlElement &&
-    (matchMedia('(prefers-color-scheme: dark)').matches || isThunderbird) &&
+    matchMedia('(prefers-color-scheme: dark)').matches &&
     !document.querySelector('.darkreader--fallback')
 ) {
     const css = 'html, body, body :not(iframe) { background-color: #181a1b !important; border-color: #776e62 !important; color: #e8e6e3 !important; } html, body { opacity: 1 !important; }';

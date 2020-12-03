59c49bc68c7f559c8e151c416b9fbb88e1a4e179
Alexander Shutau <shutovby@gmail.com>
2020-12-03 20:20:12 +0300

    CSS variables tests (old implementation)

--- src/inject/dynamic-theme/index.ts
+++ src/inject/dynamic-theme/index.ts
@@ -478,6 +478,7 @@ export function removeDynamicTheme() {
 }
 
 export function cleanDynamicThemeCache() {
+    variables.clear();
     stopWatchingForDocumentVisibility();
     cancelRendering();
     stopWatchingForUpdates();

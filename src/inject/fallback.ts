1e3c27a364c1e5804090bc2f6c0eded55e6c4f94
Jake <git@ja-ke.tech>
2020-12-03 21:16:23 +0100

    Fix fallback script condition

--- src/inject/fallback.ts
+++ src/inject/fallback.ts
@@ -1,6 +1,8 @@
+import {isThunderbird} from '../utils/platform';
+
 if (
     document.documentElement instanceof HTMLHtmlElement &&
-    matchMedia('(prefers-color-scheme: dark)').matches &&
+    (matchMedia('(prefers-color-scheme: dark)').matches || isThunderbird) &&
     !document.querySelector('.darkreader--fallback')
 ) {
     const css = 'html, body, body :not(iframe) { background-color: #181a1b !important; border-color: #776e62 !important; color: #e8e6e3 !important; } html, body { opacity: 1 !important; }';

755ea517410a6da44d4223ab1c94d68656e5fef3
Alexander Shutau <shutovby@gmail.com>
2020-12-03 21:43:36 +0300

    Restore dark testing browser background

--- tests/inject/customize.ts
+++ tests/inject/customize.ts
@@ -2,6 +2,7 @@
     const topDoc = window.top.document;
     const style = topDoc.createElement('style');
     style.textContent = [
+        'body { background-color: #222222; color: #dddddd; }',
         '#banner { background-color: #226644; }',
         '.executing { background-color: #662233; }',
     ].join('\n');

ca64b966a48fddea49a37d1497b13b2d8bf35d66
Gusted <williamzijl7@hotmail.com>
2020-12-14 21:52:16 +0100

    Improve dead code elimination
    - Use global variable to let ESBuild `define` sort this out and use dead code elimination when necessary.

--- src/inject/utils/log.ts
+++ src/inject/utils/log.ts
@@ -1,10 +1,9 @@
 declare const __DEBUG__: boolean;
-const DEBUG = __DEBUG__;
 
 export function logInfo(...args) {
-    DEBUG && console.info(...args);
+    __DEBUG__ && console.info(...args);
 }
 
 export function logWarn(...args) {
-    DEBUG && console.warn(...args);
+    __DEBUG__ && console.warn(...args);
 }

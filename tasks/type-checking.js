80550c3ac2ee719f32bbfc3722a62c259dea5856
Gusted <williamzijl7@hotmail.com>
2021-01-30 23:24:38 +0100

    Improvements

--- tasks/type-checking.js
+++ tasks/type-checking.js
@@ -1,11 +1,11 @@
-const {exec} = require('child_process');
+const {execFile} = require('child_process');
 const {createTask} = require('./task');
 
 async function typeCheck() {
     await new Promise((resolve, reject) => {
-        exec(`${process.execPath} ${require.resolve('typescript/lib/tsc.js')} --project "src/tsconfig.json"`, (err) => {
+        execFile(process.execPath, [require.resolve('typescript/lib/tsc.js'), '--project', 'src/tsconfig.json'], (err) => {
             if (err) {
-                reject(new Error(`tsc exited with error: ${err}`));
+                reject(new Error(`tsc has exited with error: ${err.message}`));
             } else {
                 resolve();
             }

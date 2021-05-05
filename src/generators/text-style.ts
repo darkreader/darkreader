a0fb4ddf7624a472ee1df930b321ead50483ad9d
Andrew Hung <66189242+AndrewDaGuy@users.noreply.github.com>
2021-05-05 12:58:54 +0000

    Exclude video.js and font awesome icons (#5733)

--- src/generators/text-style.ts
+++ src/generators/text-style.ts
@@ -4,7 +4,7 @@ export function createTextStyle(config: FilterConfig): string {
     const lines: string[] = [];
     // Don't target pre elements as they are preformatted element's e.g. code blocks
     // Exclude font libraries to preserve icons
-    lines.push('*:not(pre, .far, .fa, .glyphicon) {');
+    lines.push('*:not(pre, .far, .fa, .glyphicon, [class*="vjs-"], .fab, .fa-github) {');
 
     if (config.useFont && config.fontFamily) {
         // TODO: Validate...

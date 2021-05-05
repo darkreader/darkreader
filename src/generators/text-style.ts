116021daf7c9e056e22d30e59a0024e018875672
Andrew Hung <66189242+AndrewDaGuy@users.noreply.github.com>
2021-05-05 12:57:26 +0000

    Shortened class thingys

--- src/generators/text-style.ts
+++ src/generators/text-style.ts
@@ -4,7 +4,7 @@ export function createTextStyle(config: FilterConfig): string {
     const lines: string[] = [];
     // Don't target pre elements as they are preformatted element's e.g. code blocks
     // Exclude font libraries to preserve icons
-    lines.push('*:not(pre, .far, .fa, .glyphicon, .vjs-icon-play, .vjs-icon-pause, .vjs-icon-volume-mute, .vjs-icon-volume-low, .vjs-icon-volume-mid, .vjs-icon-volume-mid, .vjs-icon-volume-mid, .vjs-icon-volume-high, .vjs-icon-fullscreen-enter, .vjs-icon-fullscreen-exit, .vjs-icon-square, .vjs-icon-spinner, .vjs-icon-subtitles, .vjs-icon-captions, .vjs-icon-chapters, .vjs-icon-share, .vjs-icon-cog, .vjs-icon-circle, .vjs-icon-circle-outline, .vjs-icon-circle-inner-circle, .fab, .fa-github) {');
+    lines.push('*:not(pre, .far, .fa, .glyphicon, [class*="vjs-"], .fab, .fa-github) {');
 
     if (config.useFont && config.fontFamily) {
         // TODO: Validate...

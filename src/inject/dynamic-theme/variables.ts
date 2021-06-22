bde4c4f391b53a19e874be5c43be1b78e37cfb7c
Gusted <williamzijl7@hotmail.com>
2021-06-22 23:53:16 +0200

    Code-Style

--- src/inject/dynamic-theme/variables.ts
+++ src/inject/dynamic-theme/variables.ts
@@ -4,7 +4,6 @@ import {iterateCSSRules, iterateCSSDeclarations} from './css-rules';
 import {tryParseColor, getBgImageModifier} from './modify-css';
 import type {CSSValueModifier} from './modify-css';
 import type {Theme} from '../../definitions';
-import {getTempCSSStyleSheet} from '../utils/dom';
 
 export interface ModifiedVarDeclaration {
     property: string;

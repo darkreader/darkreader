darkreader/darkreader

commit 792bfada88d97cafb7ad17577abaf2534ee3bb3e

Avoid persistent ports between UI and background (#6113)
Use message passing instead of persistent ports for communication
between UI and background context. This simplifies logic a bit and
avoids unnecessary numerous callbacks and Port message event listeners.

src/background/messenger.ts
@@ -1,3 +1,4 @@
+import {isFirefox} from '../utils/platform';
 import type {ExtensionData, FilterConfig, TabInfo, Message, UserSettings} from '../definitions';
 
 export interface ExtensionAdapter {
@@ -19,36 +20,59 @@ export interface ExtensionAdapter {
 }
 
 export default class Messenger {
-    private reporters: Set<(info: ExtensionData) => void>;
     private adapter: ExtensionAdapter;
+    private changeListenerCount: number;
 
     constructor(adapter: ExtensionAdapter) {
-        this.reporters = new Set();
         this.adapter = adapter;
-        chrome.runtime.onConnect.addListener((port) => {
-            if (port.name === 'ui') {
-                port.onMessage.addListener(async (message) => await this.onUIMessage(port, message));
+        this.changeListenerCount = 0;
+        chrome.runtime.onMessage.addListener((message: Message, _: any, sendResponse: (response: any) => void) => {
+            if (message.from === 'ui') {
+                this.onUIMessage(message, sendResponse);
                 this.adapter.onPopupOpen();
+                return ([
+                    'get-data',
+                    'get-active-tab-info'
+                ].includes(message.type));
             }
         });
+
+        // This is a work-around for Firefox bug which does not let to responding to onMessage handler above.
+        if (isFirefox) {
+            chrome.runtime.onConnect.addListener((port) => {
+                let promise: Promise<ExtensionData | TabInfo>;
+                switch (port.name) {
+                    case 'ui-get-data':
+                        promise = this.adapter.collect();
+                        break;
+                    case 'ui-get-active-tab-info':
+                        promise = this.adapter.getActiveTabInfo();
+                        break;
+                    default:
+                        return;
+                }
+                promise.then((data) => port.postMessage({data}))
+                    .catch((error) => port.postMessage({error}));
+            });
+        }
     }
 
-    private async onUIMessage(port: chrome.runtime.Port, {type, id, data}: Message) {
+    private onUIMessage({type, data}: Message, sendResponse: (response: any) => void) {
         switch (type) {
             case 'get-data': {
-                const data = await this.adapter.collect();
-                port.postMessage({id, data});
+                this.adapter.collect().then((data) => sendResponse({data}));
                 break;
             }
             case 'get-active-tab-info': {
-                const data = await this.adapter.getActiveTabInfo();
-                port.postMessage({id, data});
+                this.adapter.getActiveTabInfo().then((data) => sendResponse({data}));
                 break;
             }
             case 'subscribe-to-changes': {
-                const report = (data) => port.postMessage({id, data});
-                this.reporters.add(report);
-                port.onDisconnect.addListener(() => this.reporters.delete(report));
+                this.changeListenerCount++;
+                break;
+            }
+            case 'unsubscribe-from-changes': {
+                this.changeListenerCount--;
                 break;
             }
             case 'change-settings': {
@@ -72,12 +96,12 @@ export default class Messenger {
                 break;
             }
             case 'load-config': {
-                await this.adapter.loadConfig(data);
+                this.adapter.loadConfig(data);
                 break;
             }
             case 'apply-dev-dynamic-theme-fixes': {
                 const error = this.adapter.applyDevDynamicThemeFixes(data);
-                port.postMessage({id, error: (error ? error.message : null)});
+                sendResponse({error: (error ? error.message : null)});
                 break;
             }
             case 'reset-dev-dynamic-theme-fixes': {
@@ -86,7 +110,7 @@ export default class Messenger {
             }
             case 'apply-dev-inversion-fixes': {
                 const error = this.adapter.applyDevInversionFixes(data);
-                port.postMessage({id, error: (error ? error.message : null)});
+                sendResponse({error: (error ? error.message : null)});
                 break;
             }
             case 'reset-dev-inversion-fixes': {
@@ -95,7 +119,7 @@ export default class Messenger {
             }
             case 'apply-dev-static-themes': {
                 const error = this.adapter.applyDevStaticThemes(data);
-                port.postMessage({id, error: error ? error.message : null});
+                sendResponse({error: error ? error.message : null});
                 break;
             }
             case 'reset-dev-static-themes': {
@@ -106,6 +130,8 @@ export default class Messenger {
     }
 
     reportChanges(data: ExtensionData) {
-        this.reporters.forEach((report) => report(data));
+        if (this.changeListenerCount > 0 || isFirefox) {
+            chrome.runtime.sendMessage({name: 'background', type: 'changes', data});
+        }
     }
 }

src/definitions.d.ts
@@ -109,6 +109,7 @@ export interface TabInfo {
 
 export interface Message {
     type: string;
+    from?: string;
     data?: any;
     id?: any;
     error?: any;

src/ui/connect/connector.ts
@@ -1,107 +1,137 @@
+import {isFirefox} from 'utils/platform';
 import type {ExtensionData, ExtensionActions, FilterConfig, TabInfo, Message, UserSettings} from '../../definitions';
 
 export default class Connector implements ExtensionActions {
-    private port: chrome.runtime.Port;
-    private counter: number;
+    private changeSubscribers: Set<(data: ExtensionData) => void>;
 
     constructor() {
-        this.counter = 0;
-        this.port = chrome.runtime.connect({name: 'ui'});
+        this.changeSubscribers = new Set();
     }
 
-    private getRequestId() {
-        return ++this.counter;
-    }
-
-    private async sendRequest<T>(request: Message, executor: (response: Message, resolve: (data?: T) => void, reject: (error: Error) => void) => void) {
-        const id = this.getRequestId();
+    private async sendRequest<T>(request: Message) {
         return new Promise<T>((resolve, reject) => {
-            const listener = ({id: responseId, ...response}: Message) => {
-                if (responseId === id) {
-                    executor(response, resolve, reject);
-                    this.port.onMessage.removeListener(listener);
+            chrome.runtime.sendMessage({from: 'ui', ...request}, ({data, error}) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
                 }
-            };
-            this.port.onMessage.addListener(listener);
-            this.port.postMessage({...request, id});
+            });
+        });
+    }
+
+    private async firefoxSendRequestWithResponse<T>(name: string) {
+        return new Promise<T>((resolve, reject) => {
+            const dataPort = chrome.runtime.connect({name});
+            dataPort.onDisconnect.addListener(() => reject());
+            dataPort.onMessage.addListener(({data, error}) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
+                }
+                dataPort.disconnect();
+            });
         });
     }
 
     async getData() {
-        return await this.sendRequest<ExtensionData>({type: 'get-data'}, ({data}, resolve) => resolve(data));
+        if (isFirefox) {
+            return await this.firefoxSendRequestWithResponse<ExtensionData>('ui-get-data');
+        }
+        return await this.sendRequest<ExtensionData>({type: 'get-data'});
     }
 
     async getActiveTabInfo() {
-        return await this.sendRequest<TabInfo>({type: 'get-active-tab-info'}, ({data}, resolve) => resolve(data));
+        if (isFirefox) {
+            return await this.firefoxSendRequestWithResponse<TabInfo>('ui-get-active-tab-info');
+        }
+        return await this.sendRequest<TabInfo>({type: 'get-active-tab-info'});
+    }
+
+    private onChangesReceived = ({from, type, data}: Message) => {
+        if (from === 'background' && type === 'changes') {
+            this.changeSubscribers.forEach((callback) => callback(data));
+        }
+    };
+
+    private sendMessage(data: any) {
+        chrome.runtime.sendMessage({from: 'ui', ...data});
     }
 
     subscribeToChanges(callback: (data: ExtensionData) => void) {
-        const id = this.getRequestId();
-        this.port.onMessage.addListener(({id: responseId, data}: Message) => {
-            if (responseId === id) {
-                callback(data);
+        this.changeSubscribers.add(callback);
+        if (this.changeSubscribers.size === 1) {
+            chrome.runtime.onMessage.addListener(this.onChangesReceived);
+            if (!isFirefox) {
+                this.sendMessage({type: 'subscribe-to-changes'});
             }
-        });
-        this.port.postMessage({type: 'subscribe-to-changes', id});
+        }
     }
 
     enable() {
-        this.port.postMessage({type: 'enable'});
+        this.sendMessage({type: 'enable'});
     }
 
     disable() {
-        this.port.postMessage({type: 'disable'});
+        this.sendMessage({type: 'disable'});
     }
 
     setShortcut(command: string, shortcut: string) {
-        this.port.postMessage({type: 'set-shortcut', data: {command, shortcut}});
+        this.sendMessage({type: 'set-shortcut', data: {command, shortcut}});
     }
 
     changeSettings(settings: Partial<UserSettings>) {
-        this.port.postMessage({type: 'change-settings', data: settings});
+        this.sendMessage({type: 'change-settings', data: settings});
     }
 
     setTheme(theme: Partial<FilterConfig>) {
-        this.port.postMessage({type: 'set-theme', data: theme});
+        this.sendMessage({type: 'set-theme', data: theme});
     }
 
     toggleURL(url: string) {
-        this.port.postMessage({type: 'toggle-url', data: url});
+        this.sendMessage({type: 'toggle-url', data: url});
     }
 
     markNewsAsRead(ids: string[]) {
-        this.port.postMessage({type: 'mark-news-as-read', data: ids});
+        this.sendMessage({type: 'mark-news-as-read', data: ids});
     }
 
     loadConfig(options: {local: boolean}) {
-        this.port.postMessage({type: 'load-config', data: options});
+        this.sendMessage({type: 'load-config', data: options});
     }
 
     async applyDevDynamicThemeFixes(text: string) {
-        return await this.sendRequest<void>({type: 'apply-dev-dynamic-theme-fixes', data: text}, ({error}, resolve, reject) => error ? reject(error) : resolve());
+        return await this.sendRequest<void>({type: 'apply-dev-dynamic-theme-fixes', data: text});
     }
 
     resetDevDynamicThemeFixes() {
-        this.port.postMessage({type: 'reset-dev-dynamic-theme-fixes'});
+        this.sendMessage({type: 'reset-dev-dynamic-theme-fixes'});
     }
 
     async applyDevInversionFixes(text: string) {
-        return await this.sendRequest<void>({type: 'apply-dev-inversion-fixes', data: text}, ({error}, resolve, reject) => error ? reject(error) : resolve());
+        return await this.sendRequest<void>({type: 'apply-dev-inversion-fixes', data: text});
     }
 
     resetDevInversionFixes() {
-        this.port.postMessage({type: 'reset-dev-inversion-fixes'});
+        this.sendMessage({type: 'reset-dev-inversion-fixes'});
     }
 
     async applyDevStaticThemes(text: string) {
-        return await this.sendRequest<void>({type: 'apply-dev-static-themes', data: text}, ({error}, resolve, reject) => error ? reject(error) : resolve());
+        return await this.sendRequest<void>({type: 'apply-dev-static-themes', data: text});
     }
 
     resetDevStaticThemes() {
-        this.port.postMessage({type: 'reset-dev-static-themes'});
+        this.sendMessage({type: 'reset-dev-static-themes'});
     }
 
     disconnect() {
-        this.port.disconnect();
+        if (this.changeSubscribers.size > 0) {
+            this.changeSubscribers.clear();
+            chrome.runtime.onMessage.removeListener(this.onChangesReceived);
+            if (!isFirefox) {
+                this.sendMessage({type: 'unsubscribe-from-changes'});
+            }
+        }
     }
 }

commit 61098fe48de546d3340df27ca8bc0d8ce0519b66

Fix regression introduced in PR #6113
During review I renamed one message object attribute in definition,
but accidentally left the old name in one instance. This commit updates
that name and adds type annotation (which would have thrown an error at
build time if I annotated the type).

src/background/messenger.ts
@@ -131,7 +131,12 @@ export default class Messenger {
 
     reportChanges(data: ExtensionData) {
         if (this.changeListenerCount > 0 || isFirefox) {
-            chrome.runtime.sendMessage({name: 'background', type: 'changes', data});
+            const message: Message = {
+                from: 'background',
+                type: 'changes',
+                data
+            };
+            chrome.runtime.sendMessage(message);
         }
     }
 }
43a50c78808785c8de8578443717153fbbee0901
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2021-08-04 02:38:36 +0300

    Use crypto.randomUUID() in Utils UID (#6387)
    Chrome 92 added support for crypto.randomUUID() function which returns formatted hex-encoded Universally Unique Identifiers (UUID) based on RFC4122. The new generateUID implementation based on crypto.randomUUID() is approximatelly 2x faster than the old one based on crypto.getRandomValues().
    
    Note: this method leads to reduced entropy of the resulting string (by 6 bits) because crypto.randomUUID() produces UUID with predefined version identifiers. This entropy reduction is not critical given the overall entropy of the string and the way this function is used.

--- src/utils/uid.ts
+++ src/utils/uid.ts
@@ -3,5 +3,11 @@ function hexify(number: number) {
 }
 
 export function generateUID() {
+    if ('randomUUID' in crypto) {
+        // TODO: remove any cast once type declarations are updated
+        const uuid = (crypto as any).randomUUID();
+        return uuid.substring(0, 8) + uuid.substring(9, 13) + uuid.substring(14, 18) + uuid.substring(19, 23) + uuid.substring(24);
+    }
+
     return Array.from(crypto.getRandomValues(new Uint8Array(16))).map((x) => hexify(x)).join('');
 }

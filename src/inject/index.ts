a705fce53762d052e902fb77414dc8e66a5336e8
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2021-08-06 10:25:20 +0300

    Refactor: stricter type declaration on message literals
    Declare Message type on calls to chrome.runtime.sendMessage(...) for more rigorous type checking at build time.
    
    This commit bumps @types/chrome because it relies on change introduced in version 0.0.154.

--- src/inject/index.ts
+++ src/inject/index.ts
@@ -29,7 +29,7 @@ function onMessage({type, data}: Message) {
             break;
         }
         case 'export-css': {
-            collectCSS().then((collectedCSS) => chrome.runtime.sendMessage({type: 'export-css-response', data: collectedCSS}));
+            collectCSS().then((collectedCSS) => chrome.runtime.sendMessage<Message>({type: 'export-css-response', data: collectedCSS}));
             break;
         }
         case 'unsupported-sender':
@@ -53,24 +53,24 @@ function onMessage({type, data}: Message) {
 // TODO: Use background page color scheme watcher when browser bugs fixed.
 const colorSchemeWatcher = watchForColorSchemeChange(({isDark}) => {
     logInfo('Media query was changed');
-    chrome.runtime.sendMessage({type: 'color-scheme-change', data: {isDark}});
+    chrome.runtime.sendMessage<Message>({type: 'color-scheme-change', data: {isDark}});
 });
 
 chrome.runtime.onMessage.addListener(onMessage);
-chrome.runtime.sendMessage({type: 'frame-connect'});
+chrome.runtime.sendMessage<Message>({type: 'frame-connect'});
 
 function onPageHide(e: PageTransitionEvent) {
     if (e.persisted === false) {
-        chrome.runtime.sendMessage({type: 'frame-forget'});
+        chrome.runtime.sendMessage<Message>({type: 'frame-forget'});
     }
 }
 
 function onFreeze() {
-    chrome.runtime.sendMessage({type: 'frame-freeze'});
+    chrome.runtime.sendMessage<Message>({type: 'frame-freeze'});
 }
 
 function onResume() {
-    chrome.runtime.sendMessage({type: 'frame-resume'});
+    chrome.runtime.sendMessage<Message>({type: 'frame-resume'});
 }
 
 addEventListener('pagehide', onPageHide);

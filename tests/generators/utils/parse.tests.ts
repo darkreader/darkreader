darkreader/darkreader

commit 2cf54561f7981f243d5d9fafd61e7f3cfd764f80

Just-in-time parsing of config files (#6647)
New parser takes advantage of the following observations:
 - Most users visits only a tiny subset of sites which have configs
 - Storing two numbers (indecies of rule start and end) is cheaper than storing the whole parsed rule

src/generators/dynamic-theme.ts
@@ -1,5 +1,6 @@
 import {formatSitesFixesConfig} from './utils/format';
-import {parseSitesFixesConfig} from './utils/parse';
+import {parseSitesFixesConfig, getSitesFixesFor} from './utils/parse';
+import type {SitePropsIndex} from './utils/parse';
 import {parseArray, formatArray} from '../utils/text';
 import {compareURLPatterns, isURLInList} from '../utils/url';
 import type {DynamicThemeFix} from '../definitions';
@@ -45,23 +46,35 @@ export function formatDynamicThemeFixes(dynamicThemeFixes: DynamicThemeFix[]) {
     });
 }
 
-export function getDynamicThemeFixesFor(url: string, frameURL: string, fixes: DynamicThemeFix[], enabledForPDF: boolean) {
-    if (fixes.length === 0 || fixes[0].url[0] !== '*') {
+export function getDynamicThemeFixesFor(url: string, frameURL: string, text: string, index: SitePropsIndex<DynamicThemeFix>, enabledForPDF: boolean) {
+    const fixes = getSitesFixesFor(frameURL || url, text, index, {
+        commands: Object.keys(dynamicThemeFixesCommands),
+        getCommandPropName: (command) => dynamicThemeFixesCommands[command],
+        parseCommandValue: (command, value) => {
+            if (command === 'CSS') {
+                return value.trim();
+            }
+            return parseArray(value);
+        },
+    });
+
+    if (fixes.length === 0 || fixes[fixes.length - 1].url[0] !== '*') {
         return null;
     }
+    const genericFix = fixes[fixes.length - 1];
 
     const common = {
-        url: fixes[0].url,
-        invert: fixes[0].invert || [],
-        css: fixes[0].css || [],
-        ignoreInlineStyle: fixes[0].ignoreInlineStyle || [],
-        ignoreImageAnalysis: fixes[0].ignoreImageAnalysis || [],
+        url: genericFix.url,
+        invert: genericFix.invert || [],
+        css: genericFix.css || [],
+        ignoreInlineStyle: genericFix.ignoreInlineStyle || [],
+        ignoreImageAnalysis: genericFix.ignoreImageAnalysis || [],
     };
     if (enabledForPDF) {
         common.invert = common.invert.concat('embed[type="application/pdf"]');
     }
     const sortedBySpecificity = fixes
-        .slice(1)
+        .slice(0, fixes.length - 1)
         .map((theme) => {
             return {
                 specificity: isURLInList(frameURL || url, theme.url) ? theme.url[0].length : 0,

src/generators/utils/parse.ts
@@ -1,10 +1,18 @@
+import {isURLMatched, isFullyQualifiedDomain} from '../../utils/url';
 import {parseArray} from '../../utils/text';
 
 interface SiteProps {
     url: string[];
 }
 
-interface SitesFixesParserOptions<T> {
+export interface SitePropsIndex<T extends SiteProps> {
+    offsets: string;
+    domains: {[domain: string]: number | number[]};
+    domainPatterns: {[domainPattern: string]: number | number[]};
+    cache: {[offsetId: number]: T};
+}
+
+export interface SitesFixesParserOptions<T> {
     commands: string[];
     getCommandPropName: (command: string) => keyof T;
     parseCommandValue: (command: string, value: string) => any;
@@ -18,7 +26,7 @@ export function parseSitesFixesConfig<T extends SiteProps>(text: string, options
         const lines = block.split('\n');
         const commandIndices: number[] = [];
         lines.forEach((ln, i) => {
-            if (ln.match(/^\s*[A-Z]+(\s[A-Z]+)*\s*$/)) {
+            if (ln.match(/^[A-Z]+(\s[A-Z]+){0,2}$/)) {
                 commandIndices.push(i);
             }
         });
@@ -46,4 +54,137 @@ export function parseSitesFixesConfig<T extends SiteProps>(text: string, options
     });
 
     return sites;
+}
+
+// URL patterns are guaranteed to not have protocol and leading '/'
+function getDomain(url: string) {
+    try {
+        return (new URL(url)).hostname.toLowerCase();
+    } catch (error) {
+        return url.split('/')[0].toLowerCase();
+    }
+}
+
+/*
+ * Encode all offsets into a string, where each record is 7 bytes long:
+ *  - 4 bytes for start offset
+ *  - 3 bytes for record length (end offset - start offset)
+ * Both values are stored in base 36 (radix 36) notation.
+ * Maximum supported numbers:
+ *  - start offset must be no more than parseInt('zzzz', 36) = 1679615
+ *  - length must be no more than parseInt('zzz', 36) = 46655
+ *
+ * We have to encode offsets into a string to be able to save them in
+ * chrome.storage.local for use in non-persistent background contexts.
+ */
+function encodeOffsets(offsets: Array<[number, number]>): string {
+    return offsets.map(([offset, length]) => {
+        const stringOffset = offset.toString(36);
+        const stringLength = length.toString(36);
+        return '0'.repeat(4 - stringOffset.length) + stringOffset + '0'.repeat(3 - stringLength.length) + stringLength;
+    }).join('');
+}
+
+function decodeOffset(offsets: string, index: number): [number, number] {
+    const base = (4 + 3) * index;
+    const offset = parseInt(offsets.substring(base + 0, base + 4), 36);
+    const length = parseInt(offsets.substring(base + 4, base + 4 + 3), 36);
+    return [
+        offset,
+        offset + length,
+    ];
+}
+
+export function indexSitesFixesConfig<T extends SiteProps>(text: string): SitePropsIndex<T> {
+    const domains: {[domain: string]: number | number[]} = {};
+    const domainPatterns: {[domainPattern: string]: number | number[]} = {};
+    // Array of tuples, where first number is an offset of record start and second number is record length.
+    const offsets: Array<[number, number]> = [];
+
+    function processBlock(recordStart: number, recordEnd: number, index: number) {
+        // TODO: more formal definition of URLs and delimiters
+        const block = text.substring(recordStart, recordEnd);
+        const lines = block.split('\n');
+        const commandIndices: number[] = [];
+        lines.forEach((ln, i) => {
+            if (ln.match(/^[A-Z]+(\s[A-Z]+){0,2}$/)) {
+                commandIndices.push(i);
+            }
+        });
+
+        if (commandIndices.length === 0) {
+            return;
+        }
+
+        const urls = parseArray(lines.slice(0, commandIndices[0]).join('\n'));
+        for (const url of urls) {
+            const domain = getDomain(url);
+            if (isFullyQualifiedDomain(domain)) {
+                if (!domains[domain]) {
+                    domains[domain] = index;
+                } else if (typeof domains[domain] === 'number' && domains[domain] !== index) {
+                    domains[domain] = [(domains[domain] as number), index];
+                } else if (typeof domains[domain] === 'object' && !((domains[domain] as number[]).includes(index))) {
+                    (domains[domain] as number[]).push(index);
+                }
+                continue;
+            }
+
+            if (!domainPatterns[domain]) {
+                domainPatterns[domain] = index;
+            } else if (typeof domainPatterns[domain] === 'number' && domainPatterns[domain] !== index) {
+                domainPatterns[domain] = [(domainPatterns[domain] as number), index];
+            } else if (typeof domainPatterns[domain] === 'object' && !((domainPatterns[domain] as number[]).includes(index))) {
+                (domainPatterns[domain] as number[]).push(index);
+            }
+        }
+        offsets.push([recordStart, recordEnd - recordStart]);
+    }
+
+    let recordStart = 0;
+    // Delimiter between two blocks
+    const delimiterRegex = /\s*={2,}\s*/gm;
+    let delimiter: RegExpMatchArray;
+    let count = 0;
+    while ((delimiter = delimiterRegex.exec(text))) {
+        const nextDelimiterStart = delimiter.index;
+        const nextDelimiterEnd = delimiter.index + delimiter[0].length;
+
+        processBlock(recordStart, nextDelimiterStart, count);
+
+        recordStart = nextDelimiterEnd;
+        count++;
+    }
+    processBlock(recordStart, text.length, count);
+
+    return {offsets: encodeOffsets(offsets), domains, domainPatterns, cache: {}};
+}
+
+export function parseSiteFixConfig<T extends SiteProps>(text: string, options: SitesFixesParserOptions<T>, recordStart: number, recordEnd: number): T {
+    const block = text.substring(recordStart, recordEnd);
+    return parseSitesFixesConfig<T>(block, options)[0];
+}
+
+export function getSitesFixesFor<T extends SiteProps>(url: string, text: string, index: SitePropsIndex<T>, options: SitesFixesParserOptions<T>): T[] {
+    const records: T[] = [];
+    let recordIds: number[] = [];
+    const domain = getDomain(url);
+    if (index.domains[domain]) {
+        recordIds = recordIds.concat(index.domains[domain]);
+    }
+    for (const pattern of Object.keys(index.domainPatterns)) {
+        if (isURLMatched(url, pattern)) {
+            recordIds = recordIds.concat(index.domainPatterns[pattern]);
+        }
+    }
+
+    for (const id of ((new Set(recordIds)).keys())) {
+        if (!index.cache[id]) {
+            const [start, end] = decodeOffset(index.offsets, id);
+            index.cache[id] = parseSiteFixConfig<T>(text, options, start, end);
+        }
+        records.push(index.cache[id]);
+    }
+
+    return records;
 }

tests/generators/utils/parse.tests.ts
@@ -1,0 +1,200 @@
+import {indexSitesFixesConfig, getSitesFixesFor} from '../../../src/generators/utils/parse';
+import type {SitesFixesParserOptions} from '../../../src/generators/utils/parse';
+
+test('Index config', () => {
+    const config = [
+        '*',
+        '',
+        'DIRECTIVE',
+        'hi',
+        '',
+        'MULTILINEDIRECTIVE',
+        'hello',
+        'world',
+        '',
+        '==================',
+        '',
+        'example.com',
+        '',
+        'CSS',
+        'div {',
+        '  color: green;',
+        '}'
+    ].join('\n');
+
+    const options: SitesFixesParserOptions<any> = {
+        commands: ['DIRECTIVE1', 'MULTILINEDIRECTIVE'],
+        getCommandPropName: (command) => command,
+        parseCommandValue: (_, value) => value.trim(),
+    };
+    const index = indexSitesFixesConfig(config);
+
+    const fixes = getSitesFixesFor('example.com', config, index, options);
+    expect(fixes).toEqual([
+        {
+            'url': ['example.com'],
+            'CSS': 'div {\n  color: green;\n}'
+        }, {
+            'url': ['*'],
+            'DIRECTIVE': 'hi',
+            'MULTILINEDIRECTIVE':
+            'hello\nworld'
+        }]);
+});
+
+test('Empty config', () => {
+    const config = [
+        '*',
+        '',
+        'DIRECTIVE',
+        'hello world',
+        '',
+        '====================',
+        '',
+        'invalid.example',
+        ''
+    ].join('\n');
+
+    const options: SitesFixesParserOptions<any> = {
+        commands: ['DIRECTIVE'],
+        getCommandPropName: (command) => command,
+        parseCommandValue: (_, value) => value.trim(),
+    };
+    const index = indexSitesFixesConfig(config);
+
+    const fixesGeneric = getSitesFixesFor('example.com', config, index, options);
+    const fixesInvalid = getSitesFixesFor('invalid.example', config, index, options);
+    expect(fixesGeneric).toEqual([{
+        'url': ['*'],
+        'DIRECTIVE': 'hello world',
+    }]);
+    expect(fixesInvalid).toEqual(fixesGeneric);
+});
+
+test('Domain appearing in multiple records', () => {
+    const config = [
+        '*',
+        '',
+        'DIRECTIVE',
+        'hello world',
+        '',
+        '====================',
+        '',
+        'example.com',
+        '*.example.net',
+        '',
+        'DIRECTIVE',
+        'one',
+        '',
+        '====================',
+        '',
+        'example.com',
+        '*.example.net',
+        '',
+        'DIRECTIVE',
+        'two',
+        '',
+        '====================',
+        '',
+        'example.com',
+        '*.example.net',
+        '',
+        'DIRECTIVE',
+        'three',
+        ''
+    ].join('\n');
+
+    const options: SitesFixesParserOptions<any> = {
+        commands: ['DIRECTIVE'],
+        getCommandPropName: (command) => command,
+        parseCommandValue: (_, value) => value.trim(),
+    };
+    const index = indexSitesFixesConfig(config);
+
+    const fixesFQD = getSitesFixesFor('example.com', config, index, options);
+    const fixesWildcard = getSitesFixesFor('sub.example.net', config, index, options);
+    expect(fixesFQD).toEqual([
+        {
+            'url': ['example.com', '*.example.net'],
+            'DIRECTIVE': 'one'
+        }, {
+            'url': ['example.com', '*.example.net'],
+            'DIRECTIVE': 'two'
+        }, {
+            'url': ['example.com', '*.example.net'],
+            'DIRECTIVE': 'three'
+        }, {
+            'url': ['*'],
+            'DIRECTIVE': 'hello world'
+        }]);
+    expect(fixesWildcard).toEqual([
+        {
+            'url': ['*'],
+            'DIRECTIVE': 'hello world'
+        }, {
+            'url': ['example.com', '*.example.net'],
+            'DIRECTIVE': 'one'
+        }, {
+            'url': ['example.com', '*.example.net'],
+            'DIRECTIVE': 'two'
+        }, {
+            'url': ['example.com', '*.example.net'],
+            'DIRECTIVE': 'three'
+        }]);
+});
+
+test('Domain appearing multiple times within the same record', () => {
+    const config = [
+        '*',
+        '',
+        'DIRECTIVE',
+        'hello world',
+        '',
+        '====================',
+        '',
+        'example.com',
+        '*.example.net',
+        'example.com',
+        '*.example.net',
+        '',
+        'DIRECTIVE',
+        'one',
+        ''
+    ].join('\n');
+
+    const options: SitesFixesParserOptions<any> = {
+        commands: ['DIRECTIVE'],
+        getCommandPropName: (command) => command,
+        parseCommandValue: (_, value) => value.trim(),
+    };
+    const index = indexSitesFixesConfig(config);
+
+    const fixesFQD = getSitesFixesFor('example.com', config, index, options);
+    const fixesWildcard = getSitesFixesFor('sub.example.net', config, index, options);
+    expect(fixesFQD).toEqual([
+        {
+            'url': [
+                'example.com',
+                '*.example.net',
+                'example.com',
+                '*.example.net'
+            ],
+            'DIRECTIVE': 'one'
+        }, {
+            'url': ['*'],
+            'DIRECTIVE': 'hello world'
+        }]);
+    expect(fixesWildcard).toEqual([
+        {
+            'url': ['*'],
+            'DIRECTIVE': 'hello world'
+        }, {
+            'url': [
+                'example.com',
+                '*.example.net',
+                'example.com',
+                '*.example.net'
+            ],
+            'DIRECTIVE': 'one'
+        }]);
+});

src/background/config-manager.ts
@@ -1,10 +1,9 @@
 import {readText} from './utils/network';
 import {parseArray} from '../utils/text';
 import {getDuration} from '../utils/time';
-import {parseInversionFixes} from '../generators/css-filter';
-import {parseDynamicThemeFixes} from '../generators/dynamic-theme';
-import {parseStaticThemes} from '../generators/static-theme';
+import {indexSitesFixesConfig} from '../generators/utils/parse';
 import type {InversionFix, StaticTheme, DynamicThemeFix} from '../definitions';
+import type {SitePropsIndex} from '../generators/utils/parse';
 
 const CONFIG_URLs = {
     darkSites: {
@@ -35,9 +34,12 @@ interface Config {
 
 export default class ConfigManager {
     DARK_SITES?: string[];
-    DYNAMIC_THEME_FIXES?: DynamicThemeFix[];
-    INVERSION_FIXES?: InversionFix[];
-    STATIC_THEMES?: StaticTheme[];
+    DYNAMIC_THEME_FIXES_INDEX?: SitePropsIndex<DynamicThemeFix>;
+    DYNAMIC_THEME_FIXES_RAW?: string;
+    INVERSION_FIXES_INDEX?: SitePropsIndex<InversionFix>;
+    INVERSION_FIXES_RAW?: string;
+    STATIC_THEMES_INDEX?: SitePropsIndex<StaticTheme>;
+    STATIC_THEMES_RAW?: string;
 
     raw = {
         darkSites: null as string,
@@ -137,16 +139,19 @@ export default class ConfigManager {
 
     handleDynamicThemeFixes() {
         const $fixes = this.overrides.dynamicThemeFixes || this.raw.dynamicThemeFixes;
-        this.DYNAMIC_THEME_FIXES = parseDynamicThemeFixes($fixes);
+        this.DYNAMIC_THEME_FIXES_INDEX = indexSitesFixesConfig<DynamicThemeFix>($fixes);
+        this.DYNAMIC_THEME_FIXES_RAW = $fixes;
     }
 
     handleInversionFixes() {
         const $fixes = this.overrides.inversionFixes || this.raw.inversionFixes;
-        this.INVERSION_FIXES = parseInversionFixes($fixes);
+        this.INVERSION_FIXES_INDEX = indexSitesFixesConfig<InversionFix>($fixes);
+        this.INVERSION_FIXES_RAW = $fixes;
     }
 
     handleStaticThemes() {
         const $themes = this.overrides.staticThemes || this.raw.staticThemes;
-        this.STATIC_THEMES = parseStaticThemes($themes);
+        this.STATIC_THEMES_INDEX = indexSitesFixesConfig<StaticTheme>($themes);
+        this.STATIC_THEMES_RAW = $themes;
     }
 }

src/background/devtools.ts
@@ -94,7 +94,7 @@ export default class DevTools {
 
     getDynamicThemeFixesText() {
         const $fixes = this.getSavedDynamicThemeFixes();
-        const fixes = $fixes ? parseDynamicThemeFixes($fixes) : this.config.DYNAMIC_THEME_FIXES;
+        const fixes = $fixes ? parseDynamicThemeFixes($fixes) : parseDynamicThemeFixes(this.config.DYNAMIC_THEME_FIXES_RAW);
         return formatDynamicThemeFixes(fixes);
     }
 
@@ -132,7 +132,7 @@ export default class DevTools {
 
     getInversionFixesText() {
         const $fixes = this.getSavedInversionFixes();
-        const fixes = $fixes ? parseInversionFixes($fixes) : this.config.INVERSION_FIXES;
+        const fixes = $fixes ? parseInversionFixes($fixes) : parseInversionFixes(this.config.INVERSION_FIXES_RAW);
         return formatInversionFixes(fixes);
     }
 
@@ -170,7 +170,7 @@ export default class DevTools {
 
     getStaticThemesText() {
         const $themes = this.getSavedStaticThemes();
-        const themes = $themes ? parseStaticThemes($themes) : this.config.STATIC_THEMES;
+        const themes = $themes ? parseStaticThemes($themes) : parseStaticThemes(this.config.STATIC_THEMES_RAW);
         return formatStaticThemes(themes);
     }
 

src/background/extension.ts
@@ -430,20 +430,20 @@ export class Extension {
                 case ThemeEngines.cssFilter: {
                     return {
                         type: MessageType.BG_ADD_CSS_FILTER,
-                        data: createCSSFilterStylesheet(theme, url, frameURL, this.config.INVERSION_FIXES),
+                        data: createCSSFilterStylesheet(theme, url, frameURL, this.config.INVERSION_FIXES_RAW, this.config.INVERSION_FIXES_INDEX),
                     };
                 }
                 case ThemeEngines.svgFilter: {
                     if (isFirefox) {
                         return {
                             type: MessageType.BG_ADD_CSS_FILTER,
-                            data: createSVGFilterStylesheet(theme, url, frameURL, this.config.INVERSION_FIXES),
+                            data: createSVGFilterStylesheet(theme, url, frameURL, this.config.INVERSION_FIXES_RAW, this.config.INVERSION_FIXES_INDEX),
                         };
                     }
                     return {
                         type: MessageType.BG_ADD_SVG_FILTER,
                         data: {
-                            css: createSVGFilterStylesheet(theme, url, frameURL, this.config.INVERSION_FIXES),
+                            css: createSVGFilterStylesheet(theme, url, frameURL, this.config.INVERSION_FIXES_RAW, this.config.INVERSION_FIXES_INDEX),
                             svgMatrix: getSVGFilterMatrixValue(theme),
                             svgReverseMatrix: getSVGReverseFilterMatrixValue(),
                         },
@@ -454,13 +454,13 @@ export class Extension {
                         type: MessageType.BG_ADD_STATIC_THEME,
                         data: theme.stylesheet && theme.stylesheet.trim() ?
                             theme.stylesheet :
-                            createStaticStylesheet(theme, url, frameURL, this.config.STATIC_THEMES),
+                            createStaticStylesheet(theme, url, frameURL, this.config.STATIC_THEMES_RAW, this.config.STATIC_THEMES_INDEX),
                     };
                 }
                 case ThemeEngines.dynamicTheme: {
                     const filter = {...theme};
                     delete filter.engine;
-                    const fixes = getDynamicThemeFixesFor(url, frameURL, this.config.DYNAMIC_THEME_FIXES, this.user.settings.enableForPDF);
+                    const fixes = getDynamicThemeFixesFor(url, frameURL, this.config.DYNAMIC_THEME_FIXES_RAW, this.config.DYNAMIC_THEME_FIXES_INDEX, this.user.settings.enableForPDF);
                     const isIFrame = frameURL != null;
                     return {
                         type: MessageType.BG_ADD_DYNAMIC_THEME,

src/generators/css-filter.ts
@@ -1,6 +1,7 @@
 import {formatSitesFixesConfig} from './utils/format';
 import {applyColorMatrix, createFilterMatrix} from './utils/matrix';
-import {parseSitesFixesConfig} from './utils/parse';
+import {parseSitesFixesConfig, getSitesFixesFor} from './utils/parse';
+import type {SitePropsIndex} from './utils/parse';
 import {parseArray, formatArray} from '../utils/text';
 import {compareURLPatterns, isURLInList} from '../utils/url';
 import {createTextStyle} from './text-style';
@@ -26,14 +27,14 @@ export function hasPatchForChromiumIssue501582() {
     );
 }
 
-export default function createCSSFilterStyleSheet(config: FilterConfig, url: string, frameURL: string, inversionFixes: InversionFix[]) {
+export default function createCSSFilterStyleSheet(config: FilterConfig, url: string, frameURL: string, fixes: string, index: SitePropsIndex<InversionFix>) {
     const filterValue = getCSSFilterValue(config);
     const reverseFilterValue = 'invert(100%) hue-rotate(180deg)';
-    return cssFilterStyleSheetTemplate(filterValue, reverseFilterValue, config, url, frameURL, inversionFixes);
+    return cssFilterStyleSheetTemplate(filterValue, reverseFilterValue, config, url, frameURL, fixes, index);
 }
 
-export function cssFilterStyleSheetTemplate(filterValue: string, reverseFilterValue: string, config: FilterConfig, url: string, frameURL: string, inversionFixes: InversionFix[]) {
-    const fix = getInversionFixesFor(frameURL || url, inversionFixes);
+export function cssFilterStyleSheetTemplate(filterValue: string, reverseFilterValue: string, config: FilterConfig, url: string, frameURL: string, fixes: string, index: SitePropsIndex<InversionFix>) {
+    const fix = getInversionFixesFor(frameURL || url, fixes, index);
 
     const lines: string[] = [];
 
@@ -173,7 +174,18 @@ function createReverseRule(reverseFilterValue: string, fix: InversionFix): strin
 * @param url Site URL.
 * @param inversionFixes List of inversion fixes.
 */
-export function getInversionFixesFor(url: string, inversionFixes: InversionFix[]): InversionFix {
+export function getInversionFixesFor(url: string, fixes: string, index: SitePropsIndex<InversionFix>): InversionFix {
+    const inversionFixes = getSitesFixesFor<InversionFix>(url, fixes, index, {
+        commands: Object.keys(inversionFixesCommands),
+        getCommandPropName: (command) => inversionFixesCommands[command],
+        parseCommandValue: (command, value) => {
+            if (command === 'CSS') {
+                return value.trim();
+            }
+            return parseArray(value);
+        },
+    });
+
     const common = {
         url: inversionFixes[0].url,
         invert: inversionFixes[0].invert || [],

commit 9fbdb83eac267cbe1a530233c2959222216d89b4

Fix: ensure that first fix is generic fix
This fixes regression introduced in commit
2cf54561f7981f243d5d9fafd61e7f3cfd764f80.
This commit ensures that generic fix appears first in the list
returned by getSitesFixesFor().

src/generators/dynamic-theme.ts
@@ -58,10 +58,10 @@ export function getDynamicThemeFixesFor(url: string, frameURL: string, text: str
         },
     });
 
-    if (fixes.length === 0 || fixes[fixes.length - 1].url[0] !== '*') {
+    if (fixes.length === 0 || fixes[0].url[0] !== '*') {
         return null;
     }
-    const genericFix = fixes[fixes.length - 1];
+    const genericFix = fixes[0];
 
     const common = {
         url: genericFix.url,
@@ -74,7 +74,7 @@ export function getDynamicThemeFixesFor(url: string, frameURL: string, text: str
         common.invert = common.invert.concat('embed[type="application/pdf"]');
     }
     const sortedBySpecificity = fixes
-        .slice(0, fixes.length - 1)
+        .slice(1)
         .map((theme) => {
             return {
                 specificity: isURLInList(frameURL || url, theme.url) ? theme.url[0].length : 0,

src/generators/utils/parse.ts
@@ -165,20 +165,30 @@ export function parseSiteFixConfig<T extends SiteProps>(text: string, options: S
     return parseSitesFixesConfig<T>(block, options)[0];
 }
 
+/*
+ * Given a URL, fixes, and an index, finds the applicable fixes.
+ * Note that dependents assume that the first returned fix is a generic fix (has URL pattern '*').
+ */
 export function getSitesFixesFor<T extends SiteProps>(url: string, text: string, index: SitePropsIndex<T>, options: SitesFixesParserOptions<T>): T[] {
     const records: T[] = [];
     let recordIds: number[] = [];
     const domain = getDomain(url);
-    if (index.domains[domain]) {
-        recordIds = recordIds.concat(index.domains[domain]);
-    }
     for (const pattern of Object.keys(index.domainPatterns)) {
         if (isURLMatched(url, pattern)) {
             recordIds = recordIds.concat(index.domainPatterns[pattern]);
         }
     }
+    if (index.domains[domain]) {
+        recordIds = recordIds.concat(index.domains[domain]);
+    }
 
-    for (const id of ((new Set(recordIds)).keys())) {
+    const set = new Set();
+    for (const id of recordIds) {
+        if (set.has(id)) {
+            // This record was already added to the list
+            continue;
+        }
+        set.add(id);
         if (!index.cache[id]) {
             const [start, end] = decodeOffset(index.offsets, id);
             index.cache[id] = parseSiteFixConfig<T>(text, options, start, end);

tests/generators/utils/parse.tests.ts
@@ -32,13 +32,13 @@ test('Index config', () => {
     const fixes = getSitesFixesFor('example.com', config, index, options);
     expect(fixes).toEqual([
         {
-            'url': ['example.com'],
-            'CSS': 'div {\n  color: green;\n}'
-        }, {
             'url': ['*'],
             'DIRECTIVE': 'hi',
             'MULTILINEDIRECTIVE':
             'hello\nworld'
+        }, {
+            'url': ['example.com'],
+            'CSS': 'div {\n  color: green;\n}'
         }]);
 });
 
@@ -115,6 +115,9 @@ test('Domain appearing in multiple records', () => {
     const fixesWildcard = getSitesFixesFor('sub.example.net', config, index, options);
     expect(fixesFQD).toEqual([
         {
+            'url': ['*'],
+            'DIRECTIVE': 'hello world'
+        }, {
             'url': ['example.com', '*.example.net'],
             'DIRECTIVE': 'one'
         }, {
@@ -123,9 +126,6 @@ test('Domain appearing in multiple records', () => {
         }, {
             'url': ['example.com', '*.example.net'],
             'DIRECTIVE': 'three'
-        }, {
-            'url': ['*'],
-            'DIRECTIVE': 'hello world'
         }]);
     expect(fixesWildcard).toEqual([
         {
@@ -173,6 +173,9 @@ test('Domain appearing multiple times within the same record', () => {
     const fixesWildcard = getSitesFixesFor('sub.example.net', config, index, options);
     expect(fixesFQD).toEqual([
         {
+            'url': ['*'],
+            'DIRECTIVE': 'hello world'
+        }, {
             'url': [
                 'example.com',
                 '*.example.net',
@@ -180,9 +183,6 @@ test('Domain appearing multiple times within the same record', () => {
                 '*.example.net'
             ],
             'DIRECTIVE': 'one'
-        }, {
-            'url': ['*'],
-            'DIRECTIVE': 'hello world'
         }]);
     expect(fixesWildcard).toEqual([
         {
@@ -196,5 +196,56 @@ test('Domain appearing multiple times within the same record', () => {
                 '*.example.net'
             ],
             'DIRECTIVE': 'one'
+        }]);
+});
+
+test('The generic fix appears first', () => {
+    const config = [
+        '*',
+        '',
+        'DIRECTIVE',
+        'hello world',
+        '',
+        '====================',
+        '',
+        '*.example.com',
+        '',
+        'DIRECTIVE',
+        'wildcard',
+        '',
+        '====================',
+        '',
+        'sub.example.com',
+        '',
+        'DIRECTIVE',
+        'sub',
+        '',
+        '====================',
+        '',
+        'long.sub.example.com',
+        '',
+        'DIRECTIVE',
+        'long',
+        ''
+    ].join('\n');
+
+    const options: SitesFixesParserOptions<any> = {
+        commands: ['DIRECTIVE'],
+        getCommandPropName: (command) => command,
+        parseCommandValue: (_, value) => value.trim(),
+    };
+    const index = indexSitesFixesConfig(config);
+
+    const fixesFQD = getSitesFixesFor('long.sub.example.com', config, index, options);
+    expect(fixesFQD).toEqual([
+        {
+            'url':['*'],
+            'DIRECTIVE':'hello world'
+        }, {
+            'url':['*.example.com'],
+            'DIRECTIVE':'wildcard'
+        }, {
+            'url':['long.sub.example.com'],
+            'DIRECTIVE':'long'
         }]);
 });
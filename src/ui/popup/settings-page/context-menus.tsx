0ef4187bfbb7ac90de5e3bd20ac1f19de664c36e
Anton Bershanskiy <bershanskiy@pm.me>
2021-09-22 12:42:26 +0300

    Fix: context menus on Firefox

--- src/ui/popup/settings-page/context-menus.tsx
+++ src/ui/popup/settings-page/context-menus.tsx
@@ -1,11 +1,15 @@
 import {m} from 'malevic';
-import {isMobile} from '../../../utils/platform';
+import {isFirefox, isMobile} from '../../../utils/platform';
 import CheckButton from '../check-button';
 import type {ViewProps} from '../types';
 
 export default function ContextMenusGroup(props: ViewProps) {
     function onContextMenusChange(checked: boolean) {
         if (checked) {
+            if (isFirefox) {
+                props.actions.changeSettings({enableContextMenus: true});
+                return;
+            }
             // We have to request permissions in foreground context in response to user action.
             // 'contextMenus' permission is granted automatically without any permission prompts.
             chrome.permissions.request({permissions: ['contextMenus']}, (hasPermission: boolean) => {

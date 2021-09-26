fb38b59fcb80012e8878fa84243b8012a4f97c5a
Anton Bershanskiy <bershanskiy@pm.me>
2021-09-27 00:21:58 +0300

    Fix: update Vivaldi detector

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -4,14 +4,33 @@ const platform = typeof navigator === 'undefined' ? 'some platform' : navigator.
 export const isChromium = userAgent.includes('chrome') || userAgent.includes('chromium');
 export const isThunderbird = userAgent.includes('thunderbird');
 export const isFirefox = userAgent.includes('firefox') || isThunderbird;
-export const isVivaldi = userAgent.includes('vivaldi');
+// Vivaldi attempts to hide itself from User Agent sniffing so we use the following facts:
+//  1. Vivaldi does not support navigator.userAgentData at all (it is undefined)
+//  2. Vivaldi still reports correct version of Chromium engine in User Agent like so:
+//     Chrome/[xx.x.xxxx.xx]
+//  3. Old versions of Vivaldi (prior to Vivaldi 2.10) contain "Vivaldi" in User Agent
+//     Details here: https://vivaldi.com/blog/user-agent-changes/
+//                   https://vivaldi.com/press/releases/vivaldi-2-10-no-strings-attached/
+export const isVivaldi = (function () {
+    const match = userAgent.match(/chrome\/[0-9]*/);
+    // If navigator.userAgentData is supported, it is not Vivaldi
+    return !(typeof navigator === 'object' && typeof (navigator as any).userAgentData === 'object') && match && match[0] && Number(match[0].substring('chrome/'.length)) > 92;
+})()
+// If User Agent contains Vivaldi name, it is Vivaldi
+|| userAgent.includes('vivaldi');
 export const isYaBrowser = userAgent.includes('yabrowser');
-export const isOpera = userAgent.includes('opr') || userAgent.includes('opera');
+// Browser is known to be Opera if:
+//  1. navigator.userAgentData entry contains "Opera" or "Opera GX" brand, or
+//  2. navigator.userAgent contains "OPR" or "Opera"
+export const isOpera = Boolean(typeof navigator === 'object' && (navigator as any).userAgentData && (navigator as any).userAgentData.brands.filter((brand) => brand.brand === 'Opera' || brand.brand === 'Opera GX')) || userAgent.includes('opr') || userAgent.includes('opera');
 export const isEdge = userAgent.includes('edg');
 export const isSafari = userAgent.includes('safari') && !isChromium;
 export const isWindows = platform.startsWith('win');
 export const isMacOS = platform.startsWith('mac');
-export const isMobile = userAgent.includes('mobile');
+// Browser is mobile if at least one is correct:
+//  1. navigator.userAgentData.mobile is true
+//  2. User Agent string includes 'mobile' (case-insensitive)
+export const isMobile = (typeof navigator === 'object' && (navigator as any).userAgentData && (navigator as any).userAgentData.mobile === true) || userAgent.includes('mobile');
 export const isShadowDomSupported = typeof ShadowRoot === 'function';
 export const isMatchMediaChangeEventListenerSupported = (
     typeof MediaQueryList === 'function' &&

0cfe94582f539bf291e8161900264c8730981ca2
Anton Bershanskiy <bershanskiy@pm.me>
2021-09-27 12:24:22 +0300

    Fix: add workaround for closed conduit warning

--- src/background/tab-manager.ts
+++ src/background/tab-manager.ts
@@ -117,6 +117,7 @@ export default class TabManager {
                         frameURL: frameId === 0 ? null : senderURL,
                     });
                     this.stateManager.saveState();
+                    sendResponse({type: '¯\\_(ツ)_/¯'});
                     break;
                 }
                 case MessageType.CS_FRAME_FORGET: {

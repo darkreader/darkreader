8fb578301b47db34895451edf0d06402c27862e7
Gusted <williamzijl7@hotmail.com>
2021-09-29 17:28:48 +0200

    fix: add mutex to writing
    - This patch fixes 2 issues.
    - 1) For some reason it was possible for a data race to occur(I still haven't a clue how this happend) when writing data. So it's now behind a mutex lock which solves this, and could be removed later when I find why the f*ck a data race can occur.
    - 2) The mutex provides a method to check if it's locked. And the lock functionallity is async I've added a setTimeout to 500ms(this should be within error or of marigin) so that the call from `subscribeToOuterSettingsChange` can be executed and seen that the mutex is locked and shouldn't overwrite the data with the old data which it previously had.
    - Resolves a bunch of issues.
    - Resolves #6848
    - Resolves #6873

---
+++ src/utils/mutex.ts
@@ -0,0 +1,52 @@
+
+// This is the Mutex(Not to confuse with RW-Mutex). This is to ensure that certain access are only done
+// by 1 callback/operation(a current fallpit of PromiseBarrier). This ensures that their is a global mutex
+// Which can be called by a finite amount of callbacks. We should ensure that the mutex can only be locked
+// by the first callback which has requrested it and only give the next callback the mutex lock once it's
+
+import {logWarn} from './log';
+
+// unlocked by the callback.
+export class Mutex {
+    private awaitingResolves = [] as Array<() => void>;
+    private locked = false;
+
+    public isLocked() {
+        return this.locked;
+    }
+
+    // Request a lock from the mutex.
+    public async lock() {
+        // Easiest path, the mutex is not locked.
+        // We lock the mutex and
+        if (!this.locked) {
+            this.locked = true;
+            return;
+        }
+        return new Promise<void>((resolve) => {
+            // Harder path, we will push the resolve
+            // into the array and let it be for now.
+            this.awaitingResolves.push(resolve);
+        });
+    }
+
+    // Unlock the mutex.
+    public unlock() {
+        if (!this.locked) {
+            logWarn('An unlocked mutex was tried to be unlocked.');
+            return;
+        }
+        this.locked = false;
+        setTimeout(() => this.executeNextOperation());
+    }
+
+    // Execute the next lock actions.
+    private executeNextOperation() {
+        if (this.awaitingResolves.length > 0) {
+            // Get the first entry.
+            const resolve = this.awaitingResolves.shift();
+            // Simply execute the resolve.
+            resolve();
+        }
+    }
+}

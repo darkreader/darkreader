darkreader/darkreader

commit c86e5a7417ef7ee569f0770b482800d82a543467

fix: correct implement variable box-shadow (#6931)
- Ensure that shadow modifier doesn't catch any `calc(...)`.
- Ensures that for the box-shadow a pass-trough the shadow modifier is done.
- Resolves #6928

src/inject/dynamic-theme/modify-css.ts
@@ -444,10 +444,10 @@ export function getBgImageModifier(
     }
 }
 
-function getShadowModifier(value: string): CSSValueModifier {
+export function getShadowModifier(value: string): CSSValueModifier {
     try {
         let index = 0;
-        const colorMatches = getMatches(/(^|\s)([a-z]+\(.+?\)|#[0-9a-f]+|[a-z]+)(.*?(inset|outset)?($|,))/ig, value, 2);
+        const colorMatches = getMatches(/(^|\s)(?!calc)([a-z]+\(.+?\)|#[0-9a-f]+|[a-z]+)(.*?(inset|outset)?($|,))/ig, value, 2);
         const modifiers = colorMatches.map((match, i) => {
             const prefixIndex = index;
             const matchIndex = value.indexOf(match, index);

src/inject/dynamic-theme/variables.ts
@@ -1,7 +1,7 @@
 import {modifyBackgroundColor, modifyBorderColor, modifyForegroundColor} from '../../generators/modify-colors';
 import {getParenthesesRange} from '../../utils/text';
 import {iterateCSSRules, iterateCSSDeclarations} from './css-rules';
-import {tryParseColor, getBgImageModifier} from './modify-css';
+import {tryParseColor, getBgImageModifier, getShadowModifier} from './modify-css';
 import type {CSSValueModifier} from './modify-css';
 import type {Theme} from '../../definitions';
 
@@ -249,20 +249,28 @@ export class VariablesStore {
         if (property === 'background' || property === 'background-image' || property === 'box-shadow') {
             return (theme) => {
                 const unknownVars = new Set<string>();
-                const modify = () => replaceCSSVariablesNames(
-                    sourceValue,
-                    (v) => {
-                        if (this.isVarType(v, VAR_TYPE_BGCOLOR)) {
-                            return wrapBgColorVariableName(v);
-                        }
-                        if (this.isVarType(v, VAR_TYPE_BGIMG)) {
-                            return wrapBgImgVariableName(v);
-                        }
-                        unknownVars.add(v);
-                        return v;
-                    },
-                    (fallback) => tryModifyBgColor(fallback, theme),
-                );
+                const modify = () => {
+                    const variableReplaced = replaceCSSVariablesNames(
+                        sourceValue,
+                        (v) => {
+                            if (this.isVarType(v, VAR_TYPE_BGCOLOR)) {
+                                return wrapBgColorVariableName(v);
+                            }
+                            if (this.isVarType(v, VAR_TYPE_BGIMG)) {
+                                return wrapBgImgVariableName(v);
+                            }
+                            unknownVars.add(v);
+                            return v;
+                        },
+                        (fallback) => tryModifyBgColor(fallback, theme),
+                    );
+                    // Check if property is box-shadow and if so, do a pass-trough to modify the shadow
+                    if (property === 'box-shadow') {
+                        const shadowModifier = getShadowModifier(variableReplaced);
+                        return shadowModifier(theme) || variableReplaced;
+                    }
+                    return variableReplaced;
+                };
                 const modified = modify();
                 if (unknownVars.size > 0) {
                     return new Promise<string>((resolve) => {
@@ -275,6 +283,7 @@ export class VariablesStore {
                         this.subscribeForVarTypeChange(firstUnknownVar, callback);
                     });
                 }
+
                 return modified;
             };
         }

tests/inject/dynamic/variables.tests.ts
@@ -1192,4 +1192,24 @@ describe('CSS VARIABLES OVERRIDE', () => {
 
         expect(elementStyle.caretColor).toBe('rgb(140, 255, 140)');
     });
+
+    it('should modify the box-shadow actual color values in a variable', async () => {
+        container.innerHTML = multiline(
+            '<style>',
+            '    h1 {',
+            '        --offset: 8px;',
+            '    }',
+            '    h1 {',
+            '        box-shadow: calc(var(--offset)*-1 - 1px) 0 0 0 #fff',
+            '    }',
+            '</style>',
+            '<h1>COmplicated shit :(</h1>',
+        );
+        createOrUpdateDynamicTheme(theme, null, false);
+
+        await timeout(0);
+        const elementStyle = getComputedStyle(container.querySelector('h1'));
+
+        expect(elementStyle.boxShadow).toBe('rgb(0, 0, 0) -9px 0px 0px 0px');
+    });
 });

commit f43031b41401ab427d2d5dc74d35045ab19c5165

fix: correctly implement box-shadow raw value handling
- Fixes a regression from #6931
- This should ignore when box-shadow's don't have any color within the value and just should be handled by the variable implementation.
- Resolves the box shadow of github's nav bar.

src/inject/dynamic-theme/modify-css.ts
@@ -14,6 +14,14 @@ import {isFirefox} from '../../utils/platform';
 
 export type CSSValueModifier = (theme: Theme) => string | Promise<string>;
 
+export interface CSSValueModifierResult {
+    result: string;
+    matchesLength: number;
+    unparseableMatchesLength: number;
+}
+
+export type CSSValueModifierWithInfo = (theme: Theme) => CSSValueModifierResult;
+
 export interface ModifiableCSSDeclaration {
     property: string;
     value: string | CSSValueModifier | CSSVariableModifier;
@@ -444,10 +452,11 @@ export function getBgImageModifier(
     }
 }
 
-export function getShadowModifier(value: string): CSSValueModifier {
+export function getShadowModifierWithInfo(value: string): CSSValueModifierWithInfo {
     try {
         let index = 0;
         const colorMatches = getMatches(/(^|\s)(?!calc)([a-z]+\(.+?\)|#[0-9a-f]+|[a-z]+)(.*?(inset|outset)?($|,))/ig, value, 2);
+        let notParsed = 0;
         const modifiers = colorMatches.map((match, i) => {
             const prefixIndex = index;
             const matchIndex = value.indexOf(match, index);
@@ -455,16 +464,32 @@ export function getShadowModifier(value: string): CSSValueModifier {
             index = matchEnd;
             const rgb = tryParseColor(match);
             if (!rgb) {
+                notParsed++;
                 return () => value.substring(prefixIndex, matchEnd);
             }
             return (filter: FilterConfig) => `${value.substring(prefixIndex, matchIndex)}${modifyShadowColor(rgb, filter)}${i === colorMatches.length - 1 ? value.substring(matchEnd) : ''}`;
         });
 
-        return (filter: FilterConfig) => modifiers.map((modify) => modify(filter)).join('');
+        return (filter: FilterConfig) => {
+            const modified = modifiers.map((modify) => modify(filter)).join('');
+            return {
+                matchesLength: colorMatches.length,
+                unparseableMatchesLength: notParsed,
+                result: modified,
+            };
+        };
     } catch (err) {
         logWarn(`Unable to parse shadow ${value}`, err);
         return null;
     }
+}
+
+export function getShadowModifier(value: string): CSSValueModifier {
+    const shadowModifier = getShadowModifierWithInfo(value);
+    if (!shadowModifier) {
+        return null;
+    }
+    return (theme: Theme) => shadowModifier(theme).result;
 }
 
 function getVariableModifier(

src/inject/dynamic-theme/variables.ts
@@ -1,7 +1,7 @@
 import {modifyBackgroundColor, modifyBorderColor, modifyForegroundColor} from '../../generators/modify-colors';
 import {getParenthesesRange} from '../../utils/text';
 import {iterateCSSRules, iterateCSSDeclarations} from './css-rules';
-import {tryParseColor, getBgImageModifier, getShadowModifier} from './modify-css';
+import {tryParseColor, getBgImageModifier, getShadowModifierWithInfo} from './modify-css';
 import type {CSSValueModifier} from './modify-css';
 import type {Theme} from '../../definitions';
 
@@ -265,8 +265,11 @@ export class VariablesStore {
                     );
                     // Check if property is box-shadow and if so, do a pass-trough to modify the shadow
                     if (property === 'box-shadow') {
-                        const shadowModifier = getShadowModifier(variableReplaced);
-                        return shadowModifier(theme) || variableReplaced;
+                        const shadowModifier = getShadowModifierWithInfo(variableReplaced);
+                        const modifiedShadow = shadowModifier(theme);
+                        if (modifiedShadow.unparseableMatchesLength !== modifiedShadow.matchesLength) {
+                            return modifiedShadow.result;
+                        }
                     }
                     return variableReplaced;
                 };

tests/inject/dynamic/variables.tests.ts
@@ -1212,4 +1212,24 @@ describe('CSS VARIABLES OVERRIDE', () => {
 
         expect(elementStyle.boxShadow).toBe('rgb(0, 0, 0) -9px 0px 0px 0px');
     });
+
+    it(`shouldn't modify the raw value of box-shadow when their is no color`, async () => {
+        container.innerHTML = multiline(
+            '<style>',
+            '    h1 {',
+            '        --color-border-muted: green;',
+            '    }',
+            '    h1 {',
+            '        box-shadow: inset 0 -1px 0 var(--color-border-muted)',
+            '    }',
+            '</style>',
+            '<h1>COmplicated shit :(</h1>',
+        );
+        createOrUpdateDynamicTheme(theme, null, false);
+
+        await timeout(0);
+        const elementStyle = getComputedStyle(container.querySelector('h1'));
+
+        expect(elementStyle.boxShadow).toBe('rgb(0, 102, 0) 0px -1px 0px 0px inset');
+    });
 });
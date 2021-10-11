c01ca91f554e340339f2384b0c8c8c00c93472f4
Anton Bershanskiy <bershanskiy@pm.me>
2021-10-11 23:11:20 +0300

    feedback

--- src/ui/controls/tab-panel/tab.tsx
+++ src/ui/controls/tab-panel/tab.tsx
@@ -1,6 +1,10 @@
 import {m} from 'malevic';
 
-export default function Tab({isActive}: {isActive: boolean}, ...children: Malevic.Child[]) {
+interface TabProps {
+    isActive: boolean;
+}
+
+export default function Tab({isActive}: TabProps, ...children: Malevic.Child[]) {
     const tabCls = {
         'tab-panel__tab': true,
         'tab-panel__tab--active': isActive

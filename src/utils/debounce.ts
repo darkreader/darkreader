96889ef62a951b4bad9e5290c3bd785a274475a1
Anton Bershanskiy <bershanskiy@pm.me>
2021-10-20 00:38:49 +0300

    f

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -2,7 +2,6 @@ type AnyFn = (...args: any[]) => any;
 
 export function debounce<F extends AnyFn>(delay: number, fn: F): F {
     let timeoutId: number = null;
-
     return ((...args: any[]) => {
         if (timeoutId) {
             clearTimeout(timeoutId);

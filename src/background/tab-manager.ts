7317bf649e5cdfd976fce6592bea7a9f9bb4db37
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2021-10-24 21:44:06 +0300

    Refactor: make static attributes private (#7177)

--- src/background/tab-manager.ts
+++ src/background/tab-manager.ts
@@ -55,7 +55,7 @@ export default class TabManager {
     private fileLoader: {get: (params: FetchRequestParameters) => Promise<string>} = null;
     private getTabMessage: (url: string, frameUrl: string) => Message;
     private timestamp: number = null;
-    static LOCAL_STORAGE_KEY = 'TabManager-state';
+    private static LOCAL_STORAGE_KEY = 'TabManager-state';
 
     constructor({getConnectionMessage, onColorSchemeChange, getTabMessage}: TabManagerOptions) {
         this.stateManager = new StateManager<TabManagerState>(TabManager.LOCAL_STORAGE_KEY, this, {tabs: {}, timestamp: 0});

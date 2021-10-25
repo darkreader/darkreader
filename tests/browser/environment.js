05a2ed044b21b195c4a234ee630355295027d5b5
Anton Bershanskiy <bershanskiy@pm.me>
2021-10-25 11:07:49 +0300

    Refactor: remove responseType from tests

--- tests/browser/environment.js
+++ tests/browser/environment.js
@@ -182,10 +182,10 @@ class PuppeteerEnvironment extends JestNodeEnvironment {
                 ws.on('message', (data) => {
                     const message = JSON.parse(data);
                     if (message.type === 'error') {
-                        const reject = rejectors.get(message.id).reject;
+                        const reject = rejectors.get(message.id);
                         reject(message.data);
                     } else {
-                        const resolve = resolvers.get(message.id).resolve;
+                        const resolve = resolvers.get(message.id);
                         resolve(message.data);
                     }
                     resolvers.delete(message.id);
@@ -196,9 +196,8 @@ class PuppeteerEnvironment extends JestNodeEnvironment {
 
             function sendToUIPage(message) {
                 return new Promise((resolve, reject) => {
-                    const responseType = `${message.type}-response`;
-                    resolvers.set(idCount, {responseType, resolve});
-                    rejectors.set(idCount, {responseType, reject});
+                    resolvers.set(idCount, resolve);
+                    rejectors.set(idCount, reject);
                     const json = JSON.stringify({...message, id: idCount});
                     sockets.forEach((ws) => ws.send(json));
                     idCount++;

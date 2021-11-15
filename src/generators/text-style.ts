9d96fdb4084f067e904a6bef46b5330311bc0408
Matt Alexander <github@mattalxndr.me>
2021-11-15 06:09:16 -0500

    Revert "Beautify selectors on fonts (#7288)" (#7377)
    This reverts commit ecfa1d6a2011d2885ccb893585ca853a51e0a21e.

--- src/generators/text-style.ts
+++ src/generators/text-style.ts
@@ -1,27 +1,10 @@
 import type {FilterConfig} from '../definitions';
 
 export function createTextStyle(config: FilterConfig): string {
+    const lines: string[] = [];
     // Don't target pre elements as they are preformatted element's e.g. code blocks
     // Exclude font libraries to preserve icons
-
-    const excludingSelectors: string = [
-        'pre, pre *, code, .glyphicon, [class*="vjs-"], .icofont, .typcn, mu, [class*="mu-"], .glyphicon, .icon',
-
-        // Font Awesome
-        '.fab, .fa-github, .fas',
-
-        // Material Icons
-        '.material',
-
-        // GitHub
-        '.blob-code-inner, .blob-code-inner *',
-
-        // Gitlab
-        '.monaco-editor',
-    ].join(', ');
-
-    const lines: string[] = [];
-    lines.push(`*:not(${ excludingSelectors }) {`);
+    lines.push('*:not(pre, pre *, code, .far, .fa, .glyphicon, [class*="vjs-"], .fab, .fa-github, .fas, .material-icons, .icofont, .typcn, mu, [class*="mu-"], .glyphicon, .icon) {');
 
     if (config.useFont && config.fontFamily) {
         // TODO: Validate...

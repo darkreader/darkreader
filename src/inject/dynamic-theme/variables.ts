8af414d726c43268f0cc23abdb20c8b3735e7c91
Gusted <williamzijl7@hotmail.com>
2021-11-22 10:54:27 +0100

    fix: use `replaceCSSVariablesNames` on border-color
    - Alway use `replaceCSSVariablesNames` on border-color, as `unstableVarValues` might have the incorrect(as it doesn't take into account that there are multiple variables of the same variable name).
    - Add a helper function that handle raw values like `231,123,131` such that it will be transformed into `rgb(231, 123, 131)`.
    - Resolves #7445

--- src/inject/dynamic-theme/variables.ts
+++ src/inject/dynamic-theme/variables.ts
@@ -4,6 +4,7 @@ import {iterateCSSRules, iterateCSSDeclarations} from './css-rules';
 import {tryParseColor, getBgImageModifier, getShadowModifierWithInfo} from './modify-css';
 import type {CSSValueModifier} from './modify-css';
 import type {Theme} from '../../definitions';
+import type {RGBA} from '../../utils/color';
 
 export interface ModifiedVarDeclaration {
     property: string;
@@ -293,28 +294,11 @@ export class VariablesStore {
             };
         }
         if (property.startsWith('border') || property.startsWith('outline')) {
-            if (sourceValue.endsWith(')')) {
-                const colorTypeMatch = sourceValue.match(/((rgb|hsl)a?)\(/);
-                if (colorTypeMatch) {
-                    const index = colorTypeMatch.index;
-                    return (theme) => {
-                        const value = insertVarValues(sourceValue, this.unstableVarValues);
-                        if (!value) {
-                            return sourceValue;
-                        }
-                        const beginning = sourceValue.substring(0, index);
-                        const color = sourceValue.substring(index, sourceValue.length);
-                        const inserted = insertVarValues(color, this.unstableVarValues);
-                        const modified = tryModifyBorderColor(inserted, theme);
-                        return `${beginning}${modified}`;
-                    };
-                }
-            }
             return (theme) => {
                 return replaceCSSVariablesNames(
                     sourceValue,
                     (v) => wrapBorderColorVariableName(v),
-                    (fallback) => tryModifyTextColor(fallback, theme),
+                    (fallback) => tryModifyBorderColor(fallback, theme),
                 );
             };
         }
@@ -648,19 +632,54 @@ function isConstructedColorVar(value: string) {
     return value.match(/^\s*(rgb|hsl)a?\(/);
 }
 
+// ex. 131,123,132 | 1,341, 122
+const rawValueRegex = /^\d{1,3}, ?\d{1,3}, ?\d{1,3}$/;
+
+function parseRawValue(color: string) {
+    if (rawValueRegex.test(color)) {
+        // Convert the raw value into a use-able rgb(...) value, such that it can
+        // be properly used with other functions that expect such value.
+        const splitted = color.split(',');
+        let resultInRGB = 'rgb(';
+        splitted.forEach((number) => {
+            resultInRGB += `${number.trim()}, `;
+        });
+        resultInRGB = resultInRGB.substr(0, resultInRGB.length - 2);
+        resultInRGB += ')';
+        return {isRaw: true, color: resultInRGB};
+    }
+    return {isRaw: false, color: color};
+}
+
+function handleRawValue(color: string, theme: Theme, modifyFunction: (rgb: RGBA, theme: Theme) => string) {
+    const {isRaw, color: newColor} = parseRawValue(color);
+
+    const rgb = tryParseColor(newColor);
+    if (rgb) {
+        const outputColor = modifyFunction(rgb, theme);
+
+        // If it's raw, we need to convert it back to the "raw" format.
+        if (isRaw) {
+            // This should techincally never fail(returning empty string),
+            // but just to be safe we will return outputColor.
+            const outputInRGB = tryParseColor(outputColor);
+            return outputInRGB ? `${outputInRGB.r}, ${outputInRGB.g}, ${outputInRGB.b}` : outputColor;
+        }
+        return outputColor;
+    }
+    return newColor;
+}
+
 function tryModifyBgColor(color: string, theme: Theme) {
-    const rgb = tryParseColor(color);
-    return rgb ? modifyBackgroundColor(rgb, theme) : color;
+    return handleRawValue(color, theme, modifyBackgroundColor);
 }
 
 function tryModifyTextColor(color: string, theme: Theme) {
-    const rgb = tryParseColor(color);
-    return rgb ? modifyForegroundColor(rgb, theme) : color;
+    return handleRawValue(color, theme, modifyForegroundColor);
 }
 
 function tryModifyBorderColor(color: string, theme: Theme) {
-    const rgb = tryParseColor(color);
-    return rgb ? modifyBorderColor(rgb, theme) : color;
+    return handleRawValue(color, theme, modifyBorderColor);
 }
 
 function insertVarValues(source: string, varValues: Map<string, string>, stack = new Set<string>()) {

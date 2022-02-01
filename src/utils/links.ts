2945815244ba95c4c8190864852863bf6587cdea
Alexander Shutau <shutovby@gmail.com>
2022-02-01 17:39:51 +0000

    Use Open Collective Donate page

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -2,7 +2,7 @@ import {getUILanguage} from './locales';
 
 export const BLOG_URL = 'https://darkreader.org/blog/';
 export const DEVTOOLS_DOCS_URL = 'https://github.com/darkreader/darkreader/blob/master/CONTRIBUTING.md';
-export const DONATE_URL = 'https://opencollective.com/darkreader';
+export const DONATE_URL = 'https://opencollective.com/darkreader/donate';
 export const GITHUB_URL = 'https://github.com/darkreader/darkreader';
 export const PRIVACY_URL = 'https://darkreader.org/privacy/';
 export const TWITTER_URL = 'https://twitter.com/darkreaderapp';

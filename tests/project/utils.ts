828f6c3839145b99c34d1bd1a78787e543373c77
Matt Alexander <matt@alxndr.me>
2022-03-08 12:02:20 -0600

    Fix tasks/build.js ignoring Ctrl+C (#8348)
    Co-authored-by: Gusted <williamzijl7@hotmail.com>

--- tests/project/utils.ts
+++ tests/project/utils.ts
@@ -1,25 +1,58 @@
-import type {ForkOptions} from 'child_process';
-import {fork} from 'child_process';
+import type {Readable} from 'stream';
+import {PassThrough} from 'stream';
+import type {ChildProcess} from 'child_process';
+import {once} from 'events';
+import getStream from 'get-stream';
+import {promiseWithTimeout} from '../support/test-utils';
 
-export interface ResolvedChildProcess {
-    code: number;
-    signal: NodeJS.Signals;
-    stdout: string;
-    stderr: string;
+export type ChildClosedOptions = getStream.OptionsWithEncoding & {serialization?: false | 'json'; timeout?: number};
+
+export type ChildClosedPayload = {stdout: string; stderr: string; response?: any};
+
+/**
+ * Returns a promise that resolves with a child process's close event
+ * Also handles character set decoding, deserialization from JSON, accepts a timeout in ms
+ *
+ * @example
+ * const child = fork('./script.js');
+ * const {stdout, stderr, response} = await watchChild(child);
+ * const {exitCode} = child;
+ */
+export async function childClosed(child: ChildProcess, options?: ChildClosedOptions): Promise<ChildClosedPayload> {
+    const deserialize = (output: string) => options?.serialization === 'json' ? JSON.parse(output) : output;
+    const collect = async (readable: Readable) => getStream(readable || new PassThrough(), options);
+    const childPromises = Promise.all([once(child, 'close'), collect(child.stdout), collect(child.stderr)])
+        .then(([, stdout, stderr]) => ({stdout, stderr, response: deserialize(stdout)}));
+    if (options?.timeout) {
+        return promiseWithTimeout(options.timeout, childPromises);
+    }
+    return childPromises;
 }
 
-export async function forka(modulePath: string, args?: readonly string[], options?: ForkOptions): Promise<ResolvedChildProcess> {
-    return new Promise((resolve) => {
-        const proc = fork(modulePath, args, {silent: true, ...options});
-        let stdout = '', stderr = '';
-        proc.stderr.on('data', (chunk) => {
-            stderr += chunk.toString();
+/**
+ * Returns a promise that resolves when a certain condition has been met, based on the content of a stream.
+ *
+ * @example
+ * await watchStream(child.stdout).forMatch(/Config loaded/)
+ * thisRunsAfterConfigLoads();
+ */
+export const watchStream = (readable: Readable, options?: {encoding: BufferEncoding}) => {
+    if (!readable.readableEncoding) {
+        readable.setEncoding(options?.encoding || 'utf-8');
+    }
+    const forCondition = async (cb: {(value: string): boolean}) => {
+        await new Promise<void>((resolve, reject) => {
+            readable.on('close', reject).on('data', (chunk: string) => {
+                if (chunk && cb(chunk)) {
+                    resolve();
+                }
+            });
         });
-        proc.stdout.on('data', (chunk) => {
-            stdout += chunk.toString();
-        });
-        proc.on('close', (code, signal) => {
-            resolve({code, signal, stdout: stdout || undefined, stderr: stderr || undefined});
-        });
-    });
-}
+    };
+
+    const forMatch = async (regExp: RegExp) => {
+        return await forCondition((chunk: string) => chunk.match(regExp) != null);
+    };
+
+    return {forCondition, forMatch};
+};
--- tests/support/test-utils.ts
+++ tests/support/test-utils.ts
@@ -13,3 +13,17 @@ export function multiline(...lines: string[]) {
 export function timeout(delay: number) {
     return new Promise((resolve) => setTimeout(resolve, delay));
 }
+
+export function promiseWithTimeout<T>(ms: number, promise: Promise<T>): Promise<T> {
+    let id: ReturnType<typeof setTimeout>;
+    return Promise.race<T>([promise, new Promise<never>((_, reject) => {
+        id = setTimeout(() => reject(new TimeoutError(ms)), ms);
+    })]).finally(() => clearTimeout(id));
+}
+
+class TimeoutError extends Error {
+    constructor(delay: number) {
+        super(`timeout exceeded (${delay} ms)`);
+        this.name = 'TimeoutError';
+    }
+}

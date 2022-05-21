32c20a1c70f0b28835ec4402a16d5171c5d2ba1e
Gusted <williamzijl7@hotmail.com>
2022-05-21 21:57:10 +0200

    Add even more comments

--- src/utils/math-eval.ts
+++ src/utils/math-eval.ts
@@ -1,7 +1,7 @@
 // evalMath is a function that's able to evaluates a mathematical expression and return it's ouput.
 //
 // Internally it uses the Shunting Yard algoritm. First it produces a reverse polish notation(RPN) stack.
-// Example: 1 + 2 * 3 -> [1, 2, 3, *, +]
+// Example: 1 + 2 * 3 -> [1, 2, 3, *, +] which with parentheses means 1 (2 3 *) +
 //
 // Then it evaluates the RPN stack and returns the output.
 export function evalMath(expression: string): number {
@@ -15,43 +15,57 @@ export function evalMath(expression: string): number {
     for (let i = 0, len = expression.length; i < len; i++) {
         const token = expression[i];
 
+        // Skip if the token is empty or a whitespace.
         if (!token || token === ' ') {
             continue;
         }
 
+        // Is the token a operator?
         if (operators.has(token)) {
             const op = operators.get(token);
 
+            // Go trough the workingstack and determine it's place in the workingStack
             while (workingStack.length) {
                 const currentOp = operators.get(workingStack[0]);
                 if (!currentOp) {
                     break;
                 }
 
+                // Is the current operation equal or less than the current operation?
+                // Then move that operation to the rpnStack.
                 if (op.lessOrEqualThan(currentOp)) {
                     rpnStack.push(workingStack.shift());
                 } else {
                     break;
                 }
             }
+            // Add the operation to the workingStack.
             workingStack.unshift(token);
+        // Otherwise was the last token a operator?
         } else if (!lastToken || operators.has(lastToken)) {
             rpnStack.push(token);
+        // Otherwise just append the result to the last token(e.g. multiple digits numbers).
         } else {
             rpnStack[rpnStack.length - 1] += token;
         }
+        // Set the last token.
         lastToken = token;
     }
 
+    // Push the working stack on top of the rpnStack.
     rpnStack.push(...workingStack);
 
+    // Now evaluate the rpnStack.
     const stack: number[] = [];
     for (let i = 0, len = rpnStack.length; i < len; i++) {
         const op = operators.get(rpnStack[i]);
         if (op) {
+            // Get the arguments of for the operation(first two in the stack).
             const args = stack.splice(0, 2);
+            // Excute it, because of reverse notation we first pass second item then the first item.
             stack.push(op.exec(args[1], args[0]));
         } else {
+            // Add the number to the stack.
             stack.unshift(parseFloat(rpnStack[i]));
         }
     }

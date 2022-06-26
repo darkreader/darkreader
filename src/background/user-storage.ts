700de0a81cbc6c0f1eac637f7646d34f46893a4a
D0otDo0t <nenda258@gmail.com>
2022-06-26 23:46:45 +1000

    fixing vims mistakes (sorry hopefully last one)

--- src/background/user-storage.ts
+++ src/background/user-storage.ts
@@ -121,19 +121,19 @@ export default class UserStorage {
             settings.theme.darkSchemeBackgroundColor = this.rootVariables.getPropertyValue(settings.theme.darkSchemeCssVariableBg).trim();
         }
         if (settings.theme.darkSchemeCssVariableText && isValidHexColor(this.rootVariables.getPropertyValue(settings.theme.darkSchemeCssVariableText).trim())){
-	    settings.theme.darkSchemeTextColor = this.rootVariables.getPropertyValue(settings.theme.darkSchemeCssVariableText).trim();
+            settings.theme.darkSchemeTextColor = this.rootVariables.getPropertyValue(settings.theme.darkSchemeCssVariableText).trim();
         }
         if (settings.theme.lightSchemeCssVariableBg && isValidHexColor(this.rootVariables.getPropertyValue(settings.theme.lightSchemeCssVariableBg).trim())){
-	    settings.theme.lightSchemeBackgroundColor = this.rootVariables.getPropertyValue(settings.theme.lightSchemeCssVariableBg).trim();
+            settings.theme.lightSchemeBackgroundColor = this.rootVariables.getPropertyValue(settings.theme.lightSchemeCssVariableBg).trim();
         }
         if (settings.theme.lightSchemeCssVariableText && isValidHexColor(this.rootVariables.getPropertyValue(settings.theme.lightSchemeCssVariableText).trim())){
-	    settings.theme.lightSchemeTextColor = this.rootVariables.getPropertyValue(settings.theme.lightSchemeCssVariableText).trim();
+            settings.theme.lightSchemeTextColor = this.rootVariables.getPropertyValue(settings.theme.lightSchemeCssVariableText).trim();
         }
         if (settings.theme.cssVariableScrollBar && isValidHexColor(this.rootVariables.getPropertyValue(settings.theme.cssVariableScrollBar).trim())){
-	    settings.theme.scrollbarColor = this.rootVariables.getPropertyValue(settings.theme.cssVariableScrollBar).trim();
+            settings.theme.scrollbarColor = this.rootVariables.getPropertyValue(settings.theme.cssVariableScrollBar).trim();
         }
         if (settings.theme.cssVariableSelection && isValidHexColor(this.rootVariables.getPropertyValue(settings.theme.cssVariableSelection).trim())){
-	    settings.theme.selectionColor = this.rootVariables.getPropertyValue(settings.theme.cssVariableSelection).trim();
+            settings.theme.selectionColor = this.rootVariables.getPropertyValue(settings.theme.cssVariableSelection).trim();
         }
     }
 

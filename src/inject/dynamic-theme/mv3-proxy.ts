adb60e8e907dad0c66b8aeeeffd53881a425dd93
Anton Bershanskiy <8knots@protonmail.com>
2022-06-28 14:35:57 +0300

    cleanup

--- src/inject/dynamic-theme/mv3-proxy.ts
+++ src/inject/dynamic-theme/mv3-proxy.ts
@@ -4,34 +4,45 @@ import {logInfo} from '../../utils/log';
 document.currentScript.remove();
 
 const key = 'darkreaderProxyInjected';
+const EVENT_DONE = '__darkreader__stylesheetProxy__done';
+const EVENT_ARG = '__darkreader__stylesheetProxy__arg';
+
+function injectProxyAndCleanup(arg: boolean) {
+    injectProxy(arg);
+    doneReceiver();
+    document.dispatchEvent(new CustomEvent(EVENT_DONE));
+}
+
+function regularPath() {
+    const argString = document.currentScript.dataset.arg;
+    if (argString !== undefined) {
+        document.documentElement.dataset[key] = 'true';
+        const arg: boolean = JSON.parse(argString);
+        logInfo(`MV3 proxy injector: regular path runs injectProxy(${arg}).`);
+        injectProxyAndCleanup(arg);
+    }
+}
 
 function dataReceiver(e: any) {
-    document.removeEventListener('__darkreader__stylesheetProxy__arg', dataReceiver);
+    document.removeEventListener(EVENT_ARG, dataReceiver);
     if (document.documentElement.dataset[key] !== undefined) {
         logInfo(`MV3 proxy injector: dedicated path exits because everything is done.`);
         return;
     }
     document.documentElement.dataset[key] = 'true';
     logInfo(`MV3 proxy injector: dedicated path runs injectProxy(${e.detail}).`);
-    injectProxy(e.detail);
+    injectProxyAndCleanup(e.detail);
 }
 
-function regularPath() {
-    if (document.documentElement.dataset[key] !== undefined) {
-        return;
-    }
-    const argString = document.currentScript.dataset.arg;
-    if (argString !== undefined) {
-        document.documentElement.dataset[key] = 'true';
-        const arg: boolean = JSON.parse(argString);
-        logInfo(`MV3 proxy injector: regular path runs injectProxy(${arg}).`);
-        injectProxy(arg);
-    }
+function doneReceiver() {
+    document.removeEventListener(EVENT_ARG, dataReceiver);
+    document.removeEventListener(EVENT_DONE, doneReceiver);
 }
 
 function dedicatedPath() {
     logInfo('MV3 proxy injector: dedicated path setup...');
-    document.addEventListener('__darkreader__stylesheetProxy__arg', dataReceiver);
+    document.addEventListener(EVENT_ARG, dataReceiver);
+    document.addEventListener(EVENT_DONE, doneReceiver);
 }
 
 function inject() {

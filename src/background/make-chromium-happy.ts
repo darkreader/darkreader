f78cf89e98259fcbb2a92a8c700e1a917da4eede
Anton Bershanskiy <8knots@protonmail.com>
2022-07-01 14:53:07 +0300

    Chromium-only path

--- src/background/make-chromium-happy.ts
+++ src/background/make-chromium-happy.ts
@@ -1,10 +1,14 @@
 import {MessageType} from '../utils/message';
 import type {Message} from '../definitions';
+import {isChromium} from '../utils/platform';
 
 // This function exists to prevent Chrome from logging an error about
 // closed conduit. It just sends a dummy message in response to incomming message
 // to utilise open conduit. This response message is not even used on the other side.
 export function makeChromiumHappy() {
+    if (!isChromium) {
+        return;
+    }
     chrome.runtime.onMessage.addListener((message: Message, _, sendResponse) => {
         if (![
             // Messenger

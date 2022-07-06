9dace7740762ef21cfe68ecf48722f014126ca88
Anton Bershanskiy <8knots@protonmail.com>
2022-07-06 23:56:51 +0300

    f

--- tasks/bundle-js.js
+++ tasks/bundle-js.js
@@ -112,7 +112,7 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, {debug, watch}) {
                 preventAssignment: true,
                 ...replace,
                 '__DEBUG__': debug ? 'true' : 'false',
-                '__MV3__':  platform === PLATFORM.CHROME_MV3,
+                '__MV3__': platform === PLATFORM.CHROME_MV3,
                 '__PORT__': watch ? String(PORT) : '-1',
                 '__TEST__': 'false',
                 '__WATCH__': watch ? 'true' : 'false',

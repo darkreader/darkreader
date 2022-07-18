21ad4e7ac245188987696e10c045013f73f8e56e
Anton Bershanskiy <8knots@proton.me>
2022-07-18 00:25:34 -0700

    feedback

--- src/ui/controls/time-range-picker/index.tsx
+++ src/ui/controls/time-range-picker/index.tsx
@@ -58,7 +58,7 @@ export default function TimeRangePicker(props: TimePickerProps) {
             <TextBox
                 class="time-range-picker__input time-range-picker__input--end"
                 type="time"
-                placeholder="9:00"
+                placeholder="09:00"
                 onrender={setEndTime}
                 onchange={(e) => onEndTimeChange((e.target as HTMLInputElement).value)}
                 onkeypress={(e) => {

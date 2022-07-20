darkreader/darkreader

commit 9df0ce70c8b21d7aa9fc3e50667847c6a238a7e0

Refactor parsing colors (#9337)
- Use parse with cache where possible.
- Don't throw errors, instead return null.
- Resolves #9334

src/inject/dynamic-theme/modify-css.ts
@@ -1,5 +1,4 @@
-import type {RGBA} from '../../utils/color';
-import {lowerCalcExpression, parse, rgbToHSL, hslToString} from '../../utils/color';
+import {parseColorWithCache, rgbToHSL, hslToString} from '../../utils/color';
 import {clamp} from '../../utils/math';
 import {getMatches} from '../../utils/text';
 import {getAbsoluteURL} from '../../utils/url';
@@ -140,7 +139,7 @@ export function getSelectionColor(theme: Theme) {
         backgroundColorSelection = modifyBackgroundColor({r: 0, g: 96, b: 212}, {...theme, grayscale: 0});
         foregroundColorSelection = modifyForegroundColor({r: 255, g: 255, b: 255}, {...theme, grayscale: 0});
     } else {
-        const rgb = parse(theme.selectionColor);
+        const rgb = parseColorWithCache(theme.selectionColor);
         const hsl = rgbToHSL(rgb);
         backgroundColorSelection = theme.selectionColor;
         if (hsl.l < 0.5) {
@@ -182,7 +181,7 @@ function getModifiedScrollbarStyle(theme: Theme) {
         colorThumbActive = modifyBackgroundColor({r: 96, g: 96, b: 96}, theme);
         colorCorner = modifyBackgroundColor({r: 255, g: 255, b: 255}, theme);
     } else {
-        const rgb = parse(theme.scrollbarColor);
+        const rgb = parseColorWithCache(theme.scrollbarColor);
         const hsl = rgbToHSL(rgb);
         const isLight = hsl.l > 0.5;
         const lighten = (lighter: number) => ({...hsl, l: clamp(hsl.l + lighter, 0, 1)});
@@ -237,31 +236,6 @@ const unparsableColors = new Set([
     'none',
     'unset',
 ]);
-
-const colorParseCache = new Map<string, RGBA>();
-
-export function parseColorWithCache($color: string) {
-    $color = $color.trim();
-    if (colorParseCache.has($color)) {
-        return colorParseCache.get($color);
-    }
-    // We cannot _really_ parse any color which has the calc() expression
-    // So we try our best-efforts to remove those and then parse the value.
-    if ($color.includes('calc(')) {
-        $color = lowerCalcExpression($color);
-    }
-    const color = parse($color);
-    colorParseCache.set($color, color);
-    return color;
-}
-
-export function tryParseColor($color: string) {
-    try {
-        return parseColorWithCache($color);
-    } catch (err) {
-        return null;
-    }
-}
 
 function getColorModifier(prop: string, value: string): string | CSSValueModifier {
     if (unparsableColors.has(value.toLowerCase())) {
@@ -348,20 +322,20 @@ export function getBgImageModifier(
             const parts = getMatches(partsRegex, match, 1).map((part) => {
                 part = part.trim();
 
-                let rgb = tryParseColor(part);
+                let rgb = parseColorWithCache(part);
                 if (rgb) {
                     return (filter: FilterConfig) => modifyGradientColor(rgb, filter);
                 }
 
                 const space = part.lastIndexOf(' ');
-                rgb = tryParseColor(part.substring(0, space));
+                rgb = parseColorWithCache(part.substring(0, space));
                 if (rgb) {
                     return (filter: FilterConfig) => `${modifyGradientColor(rgb, filter)} ${part.substring(space + 1)}`;
                 }
 
                 const colorStopMatch = part.match(colorStopRegex);
                 if (colorStopMatch) {
-                    rgb = tryParseColor(colorStopMatch[3]);
+                    rgb = parseColorWithCache(colorStopMatch[3]);
                     if (rgb) {
                         return (filter: FilterConfig) => `${colorStopMatch[1]}(${colorStopMatch[2] ? `${colorStopMatch[2]}, ` : ''}${modifyGradientColor(rgb, filter)})`;
                     }
@@ -503,7 +477,7 @@ export function getShadowModifierWithInfo(value: string): CSSValueModifierWithIn
             const matchIndex = value.indexOf(match, index);
             const matchEnd = matchIndex + match.length;
             index = matchEnd;
-            const rgb = tryParseColor(match);
+            const rgb = parseColorWithCache(match);
             if (!rgb) {
                 notParsed++;
                 return () => value.substring(prefixIndex, matchEnd);
@@ -559,7 +533,6 @@ function getVariableDependantModifier(
 }
 
 export function cleanModificationCache() {
-    colorParseCache.clear();
     clearColorModificationCache();
     imageDetailsCache.clear();
     cleanImageProcessingCache();

src/background/window-theme.ts
@@ -1,5 +1,5 @@
 import type {RGBA} from '../utils/color';
-import {parse} from '../utils/color';
+import {parseColorWithCache} from '../utils/color';
 import {modifyBackgroundColor, modifyForegroundColor, modifyBorderColor} from '../generators/modify-colors';
 import type {FilterConfig} from '../definitions';
 
@@ -77,7 +77,7 @@ export function setWindowTheme(filter: FilterConfig) {
             'text': modifyForegroundColor,
             'border': modifyBorderColor,
         }[type];
-        const rgb = parse(value);
+        const rgb = parseColorWithCache(value);
         const modified = modify(rgb, filter);
         obj[key] = modified;
         return obj;

src/generators/modify-colors.ts
@@ -1,6 +1,6 @@
 import type {FilterConfig, Theme} from '../definitions';
 import type {RGBA, HSLA} from '../utils/color';
-import {parse, rgbToHSL, hslToRGB, rgbToString, rgbToHexString} from '../utils/color';
+import {parseToHSLWithCache, rgbToHSL, hslToRGB, rgbToString, rgbToHexString} from '../utils/color';
 import {scale} from '../utils/math';
 import {applyColorMatrix, createFilterMatrix} from './utils/matrix';
 
@@ -21,21 +21,9 @@ function getFgPole(theme: Theme) {
 }
 
 const colorModificationCache = new Map<ColorFunction, Map<string, string>>();
-const colorParseCache = new Map<string, HSLA>();
-
-function parseToHSLWithCache(color: string) {
-    if (colorParseCache.has(color)) {
-        return colorParseCache.get(color);
-    }
-    const rgb = parse(color);
-    const hsl = rgbToHSL(rgb);
-    colorParseCache.set(color, hsl);
-    return hsl;
-}
 
 export function clearColorModificationCache() {
     colorModificationCache.clear();
-    colorParseCache.clear();
 }
 
 const rgbCacheKeys: Array<keyof RGBA> = ['r', 'g', 'b', 'a'];

src/inject/detector.ts
@@ -1,11 +1,11 @@
-import {parse, getSRGBLightness} from '../utils/color';
+import {getSRGBLightness, parseColorWithCache} from '../utils/color';
 
 function hasBuiltInDarkTheme() {
     const drStyles = document.querySelectorAll('.darkreader') as NodeListOf<HTMLStyleElement & {disabled: boolean}>;
     drStyles.forEach((style) => style.disabled = true);
 
-    const rootColor = parse(getComputedStyle(document.documentElement).backgroundColor);
-    const bodyColor = document.body ? parse(getComputedStyle(document.body).backgroundColor) : {r: 0, g: 0, b: 0, a: 0};
+    const rootColor = parseColorWithCache(getComputedStyle(document.documentElement).backgroundColor);
+    const bodyColor = document.body ? parseColorWithCache(getComputedStyle(document.body).backgroundColor) : {r: 0, g: 0, b: 0, a: 0};
     const rootLightness = (1 - rootColor.a) + rootColor.a * getSRGBLightness(rootColor.r, rootColor.g, rootColor.b);
     const finalLightness = (1 - bodyColor.a) * rootLightness + bodyColor.a * getSRGBLightness(bodyColor.r, bodyColor.g, bodyColor.b);
     const darkThemeDetected = finalLightness < 0.5;

src/inject/dynamic-theme/index.ts
@@ -1,6 +1,6 @@
 import {overrideInlineStyle, getInlineOverrideStyle, watchForInlineStyles, stopWatchingForInlineStyles, INLINE_STYLE_SELECTOR} from './inline-style';
 import {changeMetaThemeColorWhenAvailable, restoreMetaThemeColor} from './meta-theme-color';
-import {getModifiedUserAgentStyle, getModifiedFallbackStyle, cleanModificationCache, getSelectionColor, tryParseColor} from './modify-css';
+import {getModifiedUserAgentStyle, getModifiedFallbackStyle, cleanModificationCache, getSelectionColor} from './modify-css';
 import type {StyleElement, StyleManager} from './style-manager';
 import {manageStyle, getManageableStyles, cleanLoadingLinks} from './style-manager';
 import {watchForStyleChanges, stopWatchingForStyleChanges} from './watch';
@@ -18,7 +18,7 @@ import type {AdoptedStyleSheetManager} from './adopted-style-manger';
 import {createAdoptedStyleSheetOverride} from './adopted-style-manger';
 import {isFirefox} from '../../utils/platform';
 import {injectProxy} from './stylesheet-proxy';
-import {parse} from '../../utils/color';
+import {clearColorCache, parseColorWithCache} from '../../utils/color';
 import {parsedURLCache} from '../../utils/url';
 import {variablesStore} from './variables';
 
@@ -133,8 +133,8 @@ function createStaticStyleOverrides() {
     const {darkSchemeBackgroundColor, darkSchemeTextColor, lightSchemeBackgroundColor, lightSchemeTextColor, mode} = filter;
     let schemeBackgroundColor = mode === 0 ? lightSchemeBackgroundColor : darkSchemeBackgroundColor;
     let schemeTextColor = mode === 0 ? lightSchemeTextColor : darkSchemeTextColor;
-    schemeBackgroundColor = modifyBackgroundColor(parse(schemeBackgroundColor), filter);
-    schemeTextColor = modifyForegroundColor(parse(schemeTextColor), filter);
+    schemeBackgroundColor = modifyBackgroundColor(parseColorWithCache(schemeBackgroundColor), filter);
+    schemeTextColor = modifyForegroundColor(parseColorWithCache(schemeTextColor), filter);
     variableStyle.textContent = [
         `:root {`,
         `   --darkreader-neutral-background: ${schemeBackgroundColor};`,
@@ -191,7 +191,7 @@ function createShadowStaticStyleOverrides(root: ShadowRoot) {
 
 function replaceCSSTemplates($cssText: string) {
     return $cssText.replace(/\${(.+?)}/g, (_, $color) => {
-        const color = tryParseColor($color);
+        const color = parseColorWithCache($color);
         if (color) {
             return modifyColor(color, filter);
         }
@@ -561,4 +561,5 @@ export function cleanDynamicThemeCache() {
     cancelRendering();
     stopWatchingForUpdates();
     cleanModificationCache();
+    clearColorCache();
 }

src/inject/dynamic-theme/meta-theme-color.ts
@@ -1,4 +1,4 @@
-import {parse} from '../../utils/color';
+import {parseColorWithCache} from '../../utils/color';
 import {modifyBackgroundColor} from '../../generators/modify-colors';
 import {logWarn} from '../../utils/log';
 import type {FilterConfig} from '../../definitions';
@@ -10,12 +10,12 @@ let observer: MutationObserver = null;
 
 function changeMetaThemeColor(meta: HTMLMetaElement, theme: FilterConfig) {
     srcMetaThemeColor = srcMetaThemeColor || meta.content;
-    try {
-        const color = parse(srcMetaThemeColor);
-        meta.content = modifyBackgroundColor(color, theme);
-    } catch (err) {
-        logWarn(err);
+    const color = parseColorWithCache(srcMetaThemeColor);
+    if (!color) {
+        logWarn('Invalid meta color', color);
+        return;
     }
+    meta.content = modifyBackgroundColor(color, theme);
 }
 
 export function changeMetaThemeColorWhenAvailable(theme: FilterConfig) {

src/inject/dynamic-theme/variables.ts
@@ -1,10 +1,11 @@
 import {modifyBackgroundColor, modifyBorderColor, modifyForegroundColor} from '../../generators/modify-colors';
 import {getParenthesesRange} from '../../utils/text';
 import {iterateCSSRules, iterateCSSDeclarations} from './css-rules';
-import {tryParseColor, getBgImageModifier, getShadowModifierWithInfo} from './modify-css';
+import {getBgImageModifier, getShadowModifierWithInfo} from './modify-css';
 import type {CSSValueModifier} from './modify-css';
 import type {Theme} from '../../definitions';
 import type {RGBA} from '../../utils/color';
+import {parseColorWithCache} from '../../utils/color';
 
 export interface ModifiedVarDeclaration {
     property: string;
@@ -361,7 +362,7 @@ export class VariablesStore {
         }
         this.definedVars.add(varName);
 
-        const color = tryParseColor(value);
+        const color = parseColorWithCache(value);
         if (color) {
             this.unknownColorVars.add(varName);
         } else if (
@@ -654,7 +655,7 @@ function parseRawValue(color: string) {
 function handleRawValue(color: string, theme: Theme, modifyFunction: (rgb: RGBA, theme: Theme) => string) {
     const {isRaw, color: newColor} = parseRawValue(color);
 
-    const rgb = tryParseColor(newColor);
+    const rgb = parseColorWithCache(newColor);
     if (rgb) {
         const outputColor = modifyFunction(rgb, theme);
 
@@ -662,7 +663,7 @@ function handleRawValue(color: string, theme: Theme, modifyFunction: (rgb: RGBA,
         if (isRaw) {
             // This should techincally never fail(returning empty string),
             // but just to be safe we will return outputColor.
-            const outputInRGB = tryParseColor(outputColor);
+            const outputInRGB = parseColorWithCache(outputColor);
             return outputInRGB ? `${outputInRGB.r}, ${outputInRGB.g}, ${outputInRGB.b}` : outputColor;
         }
         return outputColor;

src/ui/controls/color-dropdown/index.tsx
@@ -2,7 +2,7 @@ import {m} from 'malevic';
 import {getContext} from 'malevic/dom';
 import ColorPicker from '../color-picker';
 import DropDown from '../dropdown';
-import {parse} from '../../../utils/color';
+import {parseColorWithCache} from '../../../utils/color';
 
 interface ColorDropDownProps {
     class?: string;
@@ -49,14 +49,7 @@ export default function ColorDropDown(props: ColorDropDownProps) {
         props.onChange(result);
     }
 
-    let isPickerVisible: boolean;
-
-    try {
-        parse(props.value);
-        isPickerVisible = true;
-    } catch (err) {
-        isPickerVisible = false;
-    }
+    const isPickerVisible = Boolean(parseColorWithCache(props.value));
 
     const prevValue = context.prev ? context.prev.props.value : null;
     const shouldFocusOnPicker = (

commit b4ac528b097efe8e5b88307a3da2da01405ad932

Ensure valid output from `parseColorWithCache`
- The new behavior doesn't work on `try { ... } catch { ... }`, so
instead check if the output isn't null.
- Regressed from #9337
- Resolves #9356

src/inject/dynamic-theme/modify-css.ts
@@ -241,19 +241,19 @@ function getColorModifier(prop: string, value: string): string | CSSValueModifie
     if (unparsableColors.has(value.toLowerCase())) {
         return value;
     }
-    try {
-        const rgb = parseColorWithCache(value);
-        if (prop.includes('background')) {
-            return (filter) => modifyBackgroundColor(rgb, filter);
-        }
-        if (prop.includes('border') || prop.includes('outline')) {
-            return (filter) => modifyBorderColor(rgb, filter);
-        }
-        return (filter) => modifyForegroundColor(rgb, filter);
-    } catch (err) {
-        logWarn('Color parse error', err);
+    const rgb = parseColorWithCache(value);
+    if (!rgb) {
+        logWarn("Couldn't parse color", value);
         return null;
     }
+
+    if (prop.includes('background')) {
+        return (filter) => modifyBackgroundColor(rgb, filter);
+    }
+    if (prop.includes('border') || prop.includes('outline')) {
+        return (filter) => modifyBorderColor(rgb, filter);
+    }
+    return (filter) => modifyForegroundColor(rgb, filter);
 }
 
 const imageDetailsCache = new Map<string, ImageDetails>();
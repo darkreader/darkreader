9f9bd63544649f95488ad20121e43233c7201e10
Anton Bershanskiy <8knots@proton.me>
2022-07-23 12:32:05 +0300

    Bundle API build within build/api

--- tasks/bundle-api.js
+++ tasks/bundle-api.js
@@ -10,18 +10,42 @@ import fs from 'fs';
 import os from 'os';
 import {createTask} from './task.js';
 import paths from './paths.js';
+import {copyFile, readFile, writeFile} from './utils.js';
 const {rootDir, rootPath} = paths;
 
-async function getVersion() {
-    const file = await fs.promises.readFile(new URL('../package.json', import.meta.url));
-    const p = JSON.parse(file);
-    return p.version;
+const getDestDir = (file) => `build/api/${file}`;
+
+async function getPackage() {
+    const file = await readFile('./package.json');
+    return JSON.parse(file);
+}
+
+async function copyStaticFiles() {
+    const files = [
+        'index.d.ts',
+        'LICENSE',
+        'README.md',
+    ];
+    const promises = [];
+    files.forEach((file) => promises.push(copyFile(file, getDestDir(file))));
+    await Promise.all(promises);
+}
+
+async function bundlePackageManifest(packageManifest) {
+    const minimalPackageManifest = {
+        ...packageManifest,
+        type: undefined,
+        scripts: undefined,
+        devDependencies: undefined,
+    };
+    return writeFile(getDestDir('package.json'), JSON.stringify(minimalPackageManifest, null, '  '));
 }
 
 async function bundleAPI({debug}) {
     const src = rootPath('src/api/index.ts');
-    const dest = 'darkreader.js';
-    const bundle = await rollup.rollup({
+    const dest = getDestDir('darkreader.js');
+    const packageManifestPromise = getPackage();
+    const bundlePromise = rollup.rollup({
         input: src,
         plugins: [
             rollupPluginNodeResolve(),
@@ -41,14 +65,18 @@ async function bundleAPI({debug}) {
             }),
         ].filter((x) => x)
     });
-    await bundle.write({
-        banner: `/**\n * Dark Reader v${await getVersion()}\n * https://darkreader.org/\n */\n`,
+    const [packageManifest, bundle] = await Promise.all([packageManifestPromise, bundlePromise]);
+    const bundleWritePromise = bundle.write({
+        banner: `/**\n * Dark Reader v${packageManifest.version}\n * https://darkreader.org/\n */\n`,
         file: dest,
         strict: true,
         format: 'umd',
         name: 'DarkReader',
         sourcemap: false,
     });
+    const bundlePackageManifestPromise = bundlePackageManifest(packageManifest);
+    const copyStaticFilesPromise = copyStaticFiles();
+    await Promise.all([bundleWritePromise, bundlePackageManifestPromise, copyStaticFilesPromise]);
 }
 
 const bundleAPITask = createTask(

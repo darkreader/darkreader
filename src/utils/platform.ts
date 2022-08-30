darkreader/darkreader

commit e416cb3fddff0d97faa7919919c979dd570f32d7

Refactor: compile out platform-specific code (#9671)

src/utils/platform.ts
@@ -1,3 +1,8 @@
+declare const __CHROMIUM_MV2__: boolean;
+declare const __CHROMIUM_MV3__: boolean;
+declare const __FIREFOX__: boolean;
+declare const __THUNDERBIRD__: boolean;
+
 interface UserAgentData {
     brands: Array<{
         brand: string;
@@ -22,14 +27,12 @@ const platform = isNavigatorDefined ? (navigator.userAgentData && typeof navigat
     navigator.userAgentData.platform.toLowerCase() : navigator.platform.toLowerCase()
     : 'some platform';
 
-export const isChromium = userAgent.includes('chrome') || userAgent.includes('chromium');
-export const isThunderbird = userAgent.includes('thunderbird');
-export const isFirefox = userAgent.includes('firefox') || userAgent.includes('librewolf') || isThunderbird;
+export const isFirefox = __FIREFOX__ || __THUNDERBIRD__ || userAgent.includes('firefox') || userAgent.includes('librewolf');
 export const isVivaldi = userAgent.includes('vivaldi');
 export const isYaBrowser = userAgent.includes('yabrowser');
 export const isOpera = userAgent.includes('opr') || userAgent.includes('opera');
 export const isEdge = userAgent.includes('edg');
-export const isSafari = userAgent.includes('safari') && !isChromium;
+export const isSafari = !__CHROMIUM_MV2__ && !__CHROMIUM_MV3__ && !__THUNDERBIRD__ && userAgent.includes('safari');
 export const isWindows = platform.startsWith('win');
 export const isMacOS = platform.startsWith('mac');
 export const isMobile = (isNavigatorDefined && navigator.userAgentData) ? navigator.userAgentData.mobile : userAgent.includes('mobile');

tasks/bundle-api.js
@@ -36,7 +36,8 @@ async function bundleAPI({debug}) {
             rollupPluginReplace({
                 preventAssignment: true,
                 '__DEBUG__': 'false',
-                '__MV3__': 'false',
+                '__CHROMIUM_MV2__': 'false',
+                '__CHROMIUM_MV3__': 'false',
                 '__TEST__': 'false',
             }),
         ].filter((x) => x)

src/background/content-script-manager.ts
@@ -1,6 +1,6 @@
 import {logWarn} from './utils/log';
 
-declare const __MV3__: boolean;
+declare const __CHROMIUM_MV3__: boolean;
 
 enum ContentScriptManagerState {
     UNKNOWN,
@@ -37,7 +37,7 @@ export default class ContentScriptManager {
     static state: ContentScriptManagerState;
 
     static async registerScripts(updateContentScripts: () => Promise<void>) {
-        if (!__MV3__) {
+        if (!__CHROMIUM_MV3__) {
             logWarn('ContentScriptManager is useful only within MV3 builds.');
             return;
         }
@@ -99,7 +99,7 @@ export default class ContentScriptManager {
     }
 
     static async unregisterScripts() {
-        if (!__MV3__) {
+        if (!__CHROMIUM_MV3__) {
             logWarn('ContentScriptManager is useful only within MV3 builds.');
             return;
         }

src/background/extension.ts
@@ -17,7 +17,7 @@ import createStaticStylesheet from '../generators/static-theme';
 import {createSVGFilterStylesheet, getSVGFilterMatrixValue, getSVGReverseFilterMatrixValue} from '../generators/svg-filter';
 import type {ExtensionData, FilterConfig, Shortcuts, UserSettings, TabInfo, TabData, Command} from '../definitions';
 import {isSystemDarkModeEnabled, runColorSchemeChangeDetector} from '../utils/media-query';
-import {isFirefox, isThunderbird} from '../utils/platform';
+import {isFirefox} from '../utils/platform';
 import {MessageType} from '../utils/message';
 import {logInfo, logWarn} from './utils/log';
 import {PromiseBarrier} from '../utils/promise-barrier';
@@ -37,7 +37,8 @@ interface SystemColorState {
     wasLastColorSchemeDark: boolean | null;
 }
 
-declare const __MV3__: boolean;
+declare const __CHROMIUM_MV3__: boolean;
+declare const __THUNDERBIRD__: boolean;
 
 export class Extension {
     private static autoState: AutomationState = '';
@@ -96,7 +97,7 @@ export class Extension {
     }
 
     private static async MV3syncSystemColorStateManager(isDark: boolean | null): Promise<void> {
-        if (!__MV3__) {
+        if (!__CHROMIUM_MV3__) {
             return;
         }
         if (!this.systemColorStateManager) {
@@ -141,7 +142,7 @@ export class Extension {
                 break;
             }
             case 'system':
-                if (__MV3__) {
+                if (__CHROMIUM_MV3__) {
                     isAutoDark = this.wasLastColorSchemeDark;
                     if (this.wasLastColorSchemeDark === null) {
                         logWarn('System color scheme is unknown. Defaulting to Dark.');
@@ -210,7 +211,7 @@ export class Extension {
         this.onAppToggle();
         logInfo('loaded', UserStorage.settings);
 
-        if (isThunderbird) {
+        if (__THUNDERBIRD__) {
             TabManager.registerMailDisplayScript();
         } else {
             TabManager.updateContentScript({runOnProtectedPages: UserStorage.settings.enableForProtectedPages});
@@ -516,12 +517,12 @@ export class Extension {
 
     private static onAppToggle() {
         if (this.isExtensionSwitchedOn()) {
-            if (__MV3__) {
+            if (__CHROMIUM_MV3__) {
                 ContentScriptManager.registerScripts(async () => TabManager.updateContentScript({runOnProtectedPages: UserStorage.settings.enableForProtectedPages}));
             }
             IconManager.setActive();
         } else {
-            if (__MV3__) {
+            if (__CHROMIUM_MV3__) {
                 ContentScriptManager.unregisterScripts();
             }
             IconManager.setInactive();

src/background/icon-manager.ts
@@ -1,5 +1,6 @@
 import {isNonPersistent} from '../utils/migration';
-import {isThunderbird} from '../utils/platform';
+
+declare const __THUNDERBIRD__: boolean;
 
 interface IconState {
     badgeText: string;
@@ -48,7 +49,7 @@ export default class IconManager {
     }
 
     static setActive() {
-        if (!chrome.browserAction.setIcon || isThunderbird) {
+        if (__THUNDERBIRD__ || !chrome.browserAction.setIcon) {
             // Fix for Firefox Android and Thunderbird.
             return;
         }
@@ -60,7 +61,7 @@ export default class IconManager {
     }
 
     static setInactive() {
-        if (!chrome.browserAction.setIcon || isThunderbird) {
+        if (__THUNDERBIRD__ || !chrome.browserAction.setIcon) {
             // Fix for Firefox Android and Thunderbird.
             return;
         }

src/background/index.ts
@@ -57,9 +57,9 @@ declare const __WATCH__: boolean;
 declare const __LOG__: string | false;
 declare const __PORT__: number;
 declare const __TEST__: number;
-declare const __MV3__: number;
+declare const __CHROMIUM_MV3__: number;
 
-if (__MV3__) {
+if (__CHROMIUM_MV3__) {
     chrome.runtime.onInstalled.addListener(async () => {
         try {
             (chrome.scripting as any).unregisterContentScripts(() => {

src/background/make-chromium-happy.ts
@@ -1,15 +1,14 @@
 import {MessageType} from '../utils/message';
 import type {Message} from '../definitions';
-import {isChromium} from '../utils/platform';
 import {isPanel} from './utils/tab';
 
-declare const __MV3__: boolean;
+declare const __CHROMIUM_MV2__: boolean;
 
 // This function exists to prevent Chrome from logging an error about
 // closed conduit. It just sends a dummy message in response to incomming message
 // to utilise open conduit. This response message is not even used on the other side.
 export function makeChromiumHappy() {
-    if (__MV3__ || !isChromium) {
+    if (!__CHROMIUM_MV2__) {
         return;
     }
     chrome.runtime.onMessage.addListener((message: Message, sender, sendResponse) => {

src/background/tab-manager.ts
@@ -2,14 +2,15 @@ import {canInjectScript} from '../background/utils/extension-api';
 import {createFileLoader} from './utils/network';
 import type {FetchRequestParameters} from './utils/network';
 import type {Message} from '../definitions';
-import {isFirefox, isThunderbird} from '../utils/platform';
+import {isFirefox} from '../utils/platform';
 import {MessageType} from '../utils/message';
 import {logWarn} from './utils/log';
 import {StateManager} from '../utils/state-manager';
 import {getURLHostOrProtocol} from '../utils/url';
 import {isPanel} from './utils/tab';
 
-declare const __MV3__: boolean;
+declare const __CHROMIUM_MV3__: boolean;
+declare const __THUNDERBIRD__: boolean;
 
 async function queryTabs(query: chrome.tabs.QueryInfo) {
     return new Promise<chrome.tabs.Tab[]>((resolve) => {
@@ -156,7 +157,7 @@ export default class TabManager {
                         chrome.tabs.sendMessage<Message>(sender.tab.id, {type: MessageType.BG_FETCH_RESPONSE, id, ...response}, {frameId: sender.frameId});
                     };
 
-                    if (isThunderbird) {
+                    if (__THUNDERBIRD__) {
                         // In thunderbird some CSS is loaded on a chrome:// URL.
                         // Thunderbird restricted Add-ons to load those URL's.
                         if ((message.data.url as string).startsWith('chrome://')) {
@@ -250,7 +251,7 @@ export default class TabManager {
             .filter((tab) => !Boolean(this.tabs[tab.id]))
             .forEach((tab) => {
                 if (!tab.discarded) {
-                    if (__MV3__) {
+                    if (__CHROMIUM_MV3__) {
                         chrome.scripting.executeScript({
                             target: {
                                 tabId: tab.id,

commit 2d42b5ef5677a6ec17f1eb23e6df08d254812034

Fix: restore variables used by bundle-api script (#9706)
Partially revert e416cb3fddff0d97faa7919919c979dd570f32d7 which caused a regression in darkreader.js

src/utils/platform.ts
@@ -27,12 +27,14 @@ const platform = isNavigatorDefined ? (navigator.userAgentData && typeof navigat
     navigator.userAgentData.platform.toLowerCase() : navigator.platform.toLowerCase()
     : 'some platform';
 
-export const isFirefox = __FIREFOX__ || __THUNDERBIRD__ || userAgent.includes('firefox') || userAgent.includes('librewolf');
+export const isChromium = __CHROMIUM_MV2__ || __CHROMIUM_MV3__ || userAgent.includes('chrome') || userAgent.includes('chromium');
+export const isThunderbird = __THUNDERBIRD__ || userAgent.includes('thunderbird');
+export const isFirefox = __FIREFOX__ || userAgent.includes('firefox') || userAgent.includes('librewolf') || isThunderbird;
 export const isVivaldi = userAgent.includes('vivaldi');
 export const isYaBrowser = userAgent.includes('yabrowser');
 export const isOpera = userAgent.includes('opr') || userAgent.includes('opera');
 export const isEdge = userAgent.includes('edg');
-export const isSafari = !__CHROMIUM_MV2__ && !__CHROMIUM_MV3__ && !__THUNDERBIRD__ && userAgent.includes('safari');
+export const isSafari = !__CHROMIUM_MV2__ && !__CHROMIUM_MV3__ && !__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('safari') && !isChromium;
 export const isWindows = platform.startsWith('win');
 export const isMacOS = platform.startsWith('mac');
 export const isMobile = (isNavigatorDefined && navigator.userAgentData) ? navigator.userAgentData.mobile : userAgent.includes('mobile');

tasks/bundle-api.js
@@ -35,10 +35,12 @@ async function bundleAPI({debug}) {
             }),
             rollupPluginReplace({
                 preventAssignment: true,
-                '__DEBUG__': 'false',
-                '__CHROMIUM_MV2__': 'false',
-                '__CHROMIUM_MV3__': 'false',
-                '__TEST__': 'false',
+                __DEBUG__: false,
+                __CHROMIUM_MV2__: false,
+                __CHROMIUM_MV3__: false,
+                __FIREFOX__: false,
+                __THUNDERBIRD__: false,
+                __TEST__: false,
             }),
         ].filter((x) => x)
     });
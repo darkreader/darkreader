1f5dc63aac3162a212c9e54d3d9b0dc4f89adac3
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2022-09-13 16:40:02 +0300

    Build system: fix code style task (#9813)

--- tasks/code-style.js
+++ tasks/code-style.js
@@ -2,7 +2,7 @@
 import prettier from 'prettier';
 import paths from './paths.js';
 import {createTask} from './task.js';
-import {log, readFile, writeFile, getPaths} from './utils.js';
+import {readFile, writeFile, getPaths} from './utils.js';
 const {getDestDir, PLATFORM} = paths;
 
 /** @type {import('prettier').Options} */
@@ -19,12 +19,8 @@ const options = {
 
 const extensions = ['html', 'css', 'js'];
 
-async function codeStyle({platforms, debug}) {
-    if (debug) {
-        throw new Error('code-style task does not support debug builds');
-    }
-    const platform = Object.values(PLATFORM).find((platform) => platforms[platform]);
-    const dir = getDestDir({debug, platform});
+async function processPlatform(platform) {
+    const dir = getDestDir({debug: false, platform});
     const files = await getPaths(extensions.map((ext) => `${dir}/**/*.${ext}`));
     for (const file of files) {
         const code = await readFile(file);
@@ -34,9 +30,19 @@ async function codeStyle({platforms, debug}) {
         });
         if (code !== formatted) {
             await writeFile(file, formatted);
-            debug && log.ok(file);
         }
     }
+}
+
+async function codeStyle({platforms, debug}) {
+    if (debug) {
+        throw new Error('code-style task does not support debug builds');
+    }
+    const promisses = [];
+    Object.values(PLATFORM)
+        .filter((platform) => platforms[platform])
+        .forEach((platform) => promisses.push(processPlatform(platform)));
+    await Promise.all(promisses);
 }
 
 const codeStyleTask = createTask(

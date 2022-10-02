darkreader/darkreader

commit 8cb71f4d4292b3c8d3f192ca9a3a3133b71b21cf

Refactor: compile out some more code (#9981)

tasks/bundle-js.js
@@ -150,15 +150,15 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
                 rollupPluginReplace({
                     preventAssignment: true,
                     ...replace,
-                    '__DEBUG__': debug ? 'true' : 'false',
-                    '__CHROMIUM_MV2__': platform === PLATFORM.CHROME,
-                    '__CHROMIUM_MV3__': platform === PLATFORM.CHROME_MV3,
-                    '__FIREFOX__': platform === PLATFORM.FIREFOX,
-                    '__THUNDERBIRD__': platform === PLATFORM.THUNDERBIRD,
-                    '__PORT__': watch ? String(PORT) : '-1',
-                    '__TEST__': test ? 'true' : 'false',
-                    '__WATCH__': watch ? 'true' : 'false',
-                    '__LOG__': log ? `"${log}"` : 'false',
+                    __DEBUG__: debug,
+                    __CHROMIUM_MV2__: platform === PLATFORM.CHROME,
+                    __CHROMIUM_MV3__: platform === PLATFORM.CHROME_MV3,
+                    __FIREFOX__: platform === PLATFORM.FIREFOX,
+                    __THUNDERBIRD__: platform === PLATFORM.THUNDERBIRD,
+                    __PORT__: watch ? String(PORT) : '-1',
+                    __TEST__: test,
+                    __WATCH_: watch,
+                    __LOG__: log ? `"${log}"` : false,
                 })
             ),
         ].filter((x) => x)

src/utils/platform.ts
@@ -2,6 +2,7 @@ declare const __CHROMIUM_MV2__: boolean;
 declare const __CHROMIUM_MV3__: boolean;
 declare const __FIREFOX__: boolean;
 declare const __THUNDERBIRD__: boolean;
+declare const __TEST__: boolean;
 
 interface UserAgentData {
     brands: Array<{
@@ -27,13 +28,14 @@ const platform = isNavigatorDefined ? (navigator.userAgentData && typeof navigat
     navigator.userAgentData.platform.toLowerCase() : navigator.platform.toLowerCase()
     : 'some platform';
 
-export const isChromium = __CHROMIUM_MV2__ || __CHROMIUM_MV3__ || userAgent.includes('chrome') || userAgent.includes('chromium');
-export const isThunderbird = __THUNDERBIRD__ || userAgent.includes('thunderbird');
-export const isFirefox = __FIREFOX__ || userAgent.includes('firefox') || userAgent.includes('librewolf') || isThunderbird;
-export const isVivaldi = userAgent.includes('vivaldi');
-export const isYaBrowser = userAgent.includes('yabrowser');
-export const isOpera = userAgent.includes('opr') || userAgent.includes('opera');
-export const isEdge = userAgent.includes('edg');
+// Note: if you are using these constants in tests, make sure they are not compiled out by adding __TEST__ to them
+export const isChromium = __CHROMIUM_MV2__ || __CHROMIUM_MV3__ || (!__FIREFOX__ && !__THUNDERBIRD__ && (userAgent.includes('chrome') || userAgent.includes('chromium')));
+export const isThunderbird = __THUNDERBIRD__ || (!__CHROMIUM_MV2__ && !__CHROMIUM_MV3__ && userAgent.includes('thunderbird'));
+export const isFirefox = __FIREFOX__ || isThunderbird || ((__TEST__ || (!__CHROMIUM_MV2__ && !__CHROMIUM_MV3__)) && (userAgent.includes('firefox') || userAgent.includes('librewolf')));
+export const isVivaldi = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('vivaldi'));
+export const isYaBrowser = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('yabrowser'));
+export const isOpera = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && (userAgent.includes('opr') || userAgent.includes('opera')));
+export const isEdge = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('edg'));
 export const isSafari = !__CHROMIUM_MV2__ && !__CHROMIUM_MV3__ && !__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('safari') && !isChromium;
 export const isWindows = platform.startsWith('win');
 export const isMacOS = platform.startsWith('mac');

tests/inject/karma.conf.js
@@ -53,14 +53,14 @@ export function configureKarma(config, env) {
                 }),
                 rollupPluginReplace({
                     preventAssignment: true,
-                    '__DEBUG__': 'false',
-                    '__FIREFOX__': 'false',
-                    '__CHROMIUM_MV2__': 'true',
-                    '__CHROMIUM_MV3__': 'false',
-                    '__THUNDERBIRD__': 'false',
-                    '__PORT__': '-1',
-                    '__TEST__': 'true',
-                    '__WATCH__': 'false',
+                    __DEBUG__: false,
+                    __FIREFOX__: false,
+                    __CHROMIUM_MV2__: true,
+                    __CHROMIUM_MV3__: false,
+                    __THUNDERBIRD__: false,
+                    __PORT__: '-1',
+                    __TEST__: true,
+                    __WATCH__: false,
                 }),
             ],
             output: {

commit 07277094371b44422ff17b529e40cdfb5c8890f7

Fix regression introduced in commit 8cb71f4 (#9982)

tasks/bundle-js.js
@@ -157,7 +157,7 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
                     __THUNDERBIRD__: platform === PLATFORM.THUNDERBIRD,
                     __PORT__: watch ? String(PORT) : '-1',
                     __TEST__: test,
-                    __WATCH_: watch,
+                    __WATCH__: watch,
                     __LOG__: log ? `"${log}"` : false,
                 })
             ),
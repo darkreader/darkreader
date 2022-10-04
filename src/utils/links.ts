43db0b04779c71eb020d4623e497742519a33780
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2022-10-04 17:09:12 +0300

    Update blog feed URL (#10001)

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,6 +1,7 @@
 import {getUILanguage} from './locales';
 
 export const BLOG_URL = 'https://darkreader.org/blog/';
+export const NEWS_URL = 'https://darkreader.org/blog/posts.json';
 export const DEVTOOLS_DOCS_URL = 'https://github.com/darkreader/darkreader/blob/main/CONTRIBUTING.md';
 export const DONATE_URL = 'https://opencollective.com/darkreader/donate';
 export const GITHUB_URL = 'https://github.com/darkreader/darkreader';

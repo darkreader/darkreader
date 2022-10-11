562eb6e2d827c31fbe4fc0c4e01462337e7c97bc
Anton Bershanskiy <8knots@protonmail.com>
2022-10-12 00:41:11 +0300

    f

--- src/inject/dynamic-theme/index.ts
+++ src/inject/dynamic-theme/index.ts
@@ -323,6 +323,7 @@ function onDOMReady() {
  *
  * The following code contains a workaround for extensions designed to prevent page from knowing when it is hidden
  * GitHub issue: https://github.com/darkreader/darkreader/issues/10004
+ * GitHub PR: https://github.com/darkreader/darkreader/pull/10047
  *
  * This code exploits the fact that most such extensions block only a subset of Page Lifecycle API,
  * which notifies page of being hidden but not of being shown, while Dark Reader really cares only about
@@ -333,13 +334,15 @@ function onDOMReady() {
  *    a previously hidden tab or previously hidden window (assuming DevTools are closed so window gets the focus)
  *    if document has focus, then we can assume that it is visible
  *  - some extensions overwrite document.hidden but not document.visibilityState
+ *  - Firefox has a bug: if extension overwrites document.hidden and document.visibilityState via Object.defineProperty,
+ *    then Firefox will reset them to true and 'hidden' when tab is activated, but document.hasFocus() will be true
+ *  - Safari supports document.visibilityState === 'prerender' which makes document.hidden === true even when document
+ *    is visible to the user
  */
 let didDocumentShowUp = !document.hidden;
 let documentVisibilityCallback: () => void = null;
 function documentVisibilityListener() {
-    // Note: Safari supports document.visibilityState === 'prerender'
-    // which makes document.hidden === true even when document is visible to the user
-    if (!document.hidden || document.visibilityState !== 'hidden') {
+    if (!document.hidden || document.visibilityState !== 'hidden' || document.hasFocus()) {
         stopWatchingForDocumentVisibility();
         didDocumentShowUp = true;
         const documentVisibilityCallback_ = documentVisibilityCallback;

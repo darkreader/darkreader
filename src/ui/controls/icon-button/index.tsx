bc6bfab77b9cf0290da7841319905eded977b6fc
FonecaBot2 <112948270+FonecaBot2@users.noreply.github.com>
2022-10-14 18:15:44 -0300

    Add delete all sites button (#9829)
    - Add a new button to the sitelist page, which will clear the site list.

---
+++ src/ui/controls/icon-button/index.tsx
@@ -0,0 +1,15 @@
+import {m} from 'malevic';
+import {mergeClass, omitAttrs} from '../utils';
+
+export default function IconButton(props: Malevic.NodeAttrs, ...children: Malevic.Child[]) {
+    const cls = mergeClass('icon-button', props.class);
+    const attrs = omitAttrs(['class'], props);
+
+    return (
+        <button class={cls} {...attrs}>
+            <span class="button__wrapper">
+                {...children}
+            </span>
+        </button>
+    );
+}

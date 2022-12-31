cc2bfae5885b9716b5131f4a65ab6c376a758189
Anton Bershanskiy <8knots@protonmail.com>
2022-12-31 17:23:49 +0200

    Revert switch to npm upgrade

--- tasks/dependencies.js
+++ tasks/dependencies.js
@@ -1,10 +1,26 @@
 // @ts-check
 import {exec} from 'node:child_process';
 import {fileURLToPath} from 'node:url';
+import {readFile, writeFile} from 'node:fs/promises';
 import {log} from './utils.js';
 
 const cwd = fileURLToPath(new URL('../', import.meta.url));
 const packagePath = `${cwd}/package.json`;
+
+async function getOutdated() {
+    return /** @type {Promise<object | null>} */(new Promise((resolve, reject) => {
+        exec('npm outdated --json', {cwd}, (error, stdout) => {
+            if (!error) {
+                log.error('Nothing to upgrade');
+                reject();
+                return;
+            }
+
+            const packages = JSON.parse(stdout.toString());
+            resolve(packages);
+        });
+    }));
+}
 
 /**
  *
@@ -27,7 +43,20 @@ async function buildAll() {
     await command('npm run build -- --release --api --chrome --chrome-mv3 --firefox --thunderbird');
 }
 
+async function patchPackage(outdated) {
+    const original = JSON.parse((await readFile(packagePath)).toString());
+    for (const pkg in outdated) {
+        original.devDependencies[pkg] = outdated[pkg].latest;
+    }
+    return original;
+}
+
 async function main() {
+    const outdated = await getOutdated();
+    if (!outdated){
+        return;
+    }
+
     log.ok('Building with old dependencies');
     await buildAll();
     log.ok('Built with old dependencies');
@@ -35,9 +64,17 @@ async function main() {
     await command('mv darkreader.js darkreader-old.js');
     log.ok('Moved built output');
 
-    log.ok('Installing new dependencies');
-    await command('npm upgrade');
+    const patched = await patchPackage(outdated);
+    log.ok('Upgrading own dependencies');
+    await writeFile(packagePath, `${JSON.stringify(patched, null, 2)}\n`);
     await command('npm i');
+    await command('git add package.json package-lock.json');
+    await command('git commit -m "Bump own dependencies"');
+
+    log.ok('Upgrading transitive dependencies');
+    await command('npm upgrade');
+    await command('git add package.json package-lock.json');
+    await command('git commit -m "Bump transitive dependencies"');
     log.ok('Installed new dependencies');
 
     await buildAll();
@@ -50,9 +87,6 @@ async function main() {
     await command('diff darkreader-old.js darkreader.js');
     log.ok('Dependency upgrade does not result in change to built output');
 
-    await command('git add package.json package-lock.json');
-    await command('git commit -m "Upgrade dependencies"');
-    log.ok('Created commit');
     // TODO: when moving this to CI, provide branch name in CI config, along with
     // a token
     await command('git push origin HEAD:bump-dependencies');

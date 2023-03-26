darkreader/darkreader

commit cb7f3c86806a147983803c83304789946913c699

fix rollup typescript2 race condition (#10950)
* print error on build failure

harmless and makes debugging easier when something goes wrong in the
build pipeline

* fix race condition in debug build

rollup-plugin-typescript2 has a race condition when used in parallel
with the same cacheRoot.
https://github.com/ezolenko/rollup-plugin-typescript2/issues/15

to work around this, use a unique cacheRoot per build.

fixes #10949

---------

Co-authored-by: gandalf3 <gandalf3@blendermonkey.com>

tasks/bundle-js.js
@@ -144,7 +144,7 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
                     sourceMap: debug ? true : false,
                     inlineSources: debug ? true : false,
                     noEmitOnError: watch ? false : true,
-                    cacheDir: debug ? `${fs.realpathSync(os.tmpdir())}/darkreader_typescript_cache` : undefined,
+                    cacheRoot: debug ? `${fs.realpathSync(os.tmpdir())}/darkreader_typescript_cache/${rollupPluginTypesctiptInstanceKey}` : undefined,
                 };
                 if (debug) {
                     config.verbosty = 3;

tasks/build.js
@@ -48,6 +48,7 @@ async function build({platforms, debug, watch, log: logging, test}) {
             log.ok('MISSION PASSED! RESPECT +');
         }
     } catch (err) {
+        console.log(err);
         log.error(`MISSION FAILED!`);
         process.exit(13);
     }

commit d4bb681b97e75d43db9d3f82db8aca1b46d5bd02

Fix build (#10974)
Fix regression introduced by commit cb7f3c86806a147983803c83304789946913c699

tasks/bundle-js.js
@@ -144,10 +144,10 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
                     sourceMap: debug ? true : false,
                     inlineSources: debug ? true : false,
                     noEmitOnError: watch ? false : true,
-                    cacheRoot: debug ? `${fs.realpathSync(os.tmpdir())}/darkreader_typescript_cache/${rollupPluginTypesctiptInstanceKey}` : undefined,
                 };
                 if (debug) {
-                    config.verbosty = 3;
+                    config.cacheRoot = `${fs.realpathSync(os.tmpdir())}/darkreader_typescript_cache/${rollupPluginTypesctiptInstanceKey}`;
+                    config.verbosity = 3;
                 }
                 return plugin(config);
             }),
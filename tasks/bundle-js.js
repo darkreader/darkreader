f868c31e086398e3457a28a96e92eb8e3b098ef7
Your Name <you@example.com>
2023-04-04 12:47:17 +0300

    f

--- tasks/bundle-js.js
+++ tasks/bundle-js.js
@@ -124,6 +124,17 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
     const bundle = await rollup.rollup({
         input: rootPath(src),
         plugins: [
+            // Firefox does not currently support tab.eval() functions fully, so we have to manually polyfill it via
+            // regular eval().
+            // This plugin is necessary to avoid (benign) warnings in the console during builds, it just replaces
+            // literally one occurence of eval() in our code even before TypeSctipt even encounters it.
+            // TODO(anton): remove this once Firefox supports tab.eval() via WebDriver BiDi
+            getRollupPluginInstance('removeEval', '', () => !debug && entry.src === 'src/background/index.ts' &&
+                rollupPluginReplace({
+                    preventAssignment: true,
+                    'eval(': 'void(',
+                })
+            ),
             getRollupPluginInstance('nodeResolve', '', rollupPluginNodeResolve),
             getRollupPluginInstance('typesctipt', rollupPluginTypesctiptInstanceKey, () =>
                 rollupPluginTypescript({
@@ -159,6 +170,8 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
             ),
         ].filter((x) => x)
     });
+    // TODO(anton): remove this once Firefox supports tab.eval() via WebDriver BiDi
+    freeRollupPluginInstance('removeEval', '');
     freeRollupPluginInstance('nodeResolve', '');
     freeRollupPluginInstance('typesctipt', rollupPluginTypesctiptInstanceKey);
     freeRollupPluginInstance('replace', rollupPluginReplaceInstanceKey);

f2b797c31871d911b8787d7cd1e1fc438d2b1a3c
Your Name <you@example.com>
2023-04-18 17:18:18 +0300

    f

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -30,7 +30,6 @@ const platform = isNavigatorDefined ? (navigator.userAgentData && typeof navigat
 
 // Note: if you are using these constants in tests, make sure they are not compiled out by adding __TEST__ to them
 export const isChromium = __CHROMIUM_MV2__ || __CHROMIUM_MV3__ || (!__FIREFOX__ && !__THUNDERBIRD__ && (userAgent.includes('chrome') || userAgent.includes('chromium')));
-export const isThunderbird = __THUNDERBIRD__ || (!__CHROMIUM_MV2__ && !__CHROMIUM_MV3__ && userAgent.includes('thunderbird'));
 export const isFirefox = __FIREFOX__ || __THUNDERBIRD__ || ((__TEST__ || (!__CHROMIUM_MV2__ && !__CHROMIUM_MV3__)) && (userAgent.includes('firefox') || userAgent.includes('thunderbird') || userAgent.includes('librewolf')));
 export const isVivaldi = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('vivaldi'));
 export const isYaBrowser = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('yabrowser'));

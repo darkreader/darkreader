darkreader/darkreader

commit e6a0651cfa11f4c2311cef5f9896a02423e06f8f

Refactor: rename compilation constants (#11201)

tasks/build.js
@@ -78,26 +78,52 @@ async function api(debug, watch) {
     }
 }
 
-async function run({api: api_, release, debug, platforms, watch, log, test, version}) {
-    if (release && Object.values(platforms).some(Boolean)) {
+async function run({release, debug, platforms, watch, log, test, version}) {
+    const regular = Object.keys(platforms).some((platform) => platform !== PLATFORM.API && platforms[platform]);
+    if (release && regular) {
         await build({platforms, version, debug: false, watch: false, log: null, test: false});
     }
-    if (debug && Object.values(platforms).some(Boolean)) {
+    if (debug && regular) {
         await build({platforms, version, debug, watch, log, test});
     }
-    if (api_) {
+    if (platforms[PLATFORM.API]) {
         await api(debug, watch);
     }
 }
 
 function getParams(args) {
-    const allPlatforms = !(args.includes('--api') || args.includes('--chrome') || args.includes('--chrome-mv3') || args.includes('--firefox') || args.includes('--thunderbird'));
-    const platforms = {
-        [PLATFORM.CHROME]: allPlatforms || args.includes('--chrome'),
-        [PLATFORM.CHROME_MV3]: allPlatforms || args.includes('--chrome-mv3'),
-        [PLATFORM.FIREFOX]: allPlatforms || args.includes('--firefox'),
-        [PLATFORM.THUNDERBIRD]: allPlatforms || args.includes('--thunderbird'),
+    const argMap = {
+        '--api': PLATFORM.API,
+        '--chrome': PLATFORM.CHROMIUM_MV2,
+        '--chrome-mv2': PLATFORM.CHROMIUM_MV2,
+        '--chrome-mv3': PLATFORM.CHROMIUM_MV3,
+        '--firefox': PLATFORM.FIREFOX_MV2,
+        '--firefox-mv2': PLATFORM.FIREFOX_MV2,
+        '--firefox-mv3': PLATFORM.FIREFOX_MV3,
+        '--thunderbird': PLATFORM.THUNDERBIRD,
     };
+    const platforms = {
+        [PLATFORM.CHROMIUM_MV2]: false,
+        [PLATFORM.CHROMIUM_MV3]: false,
+        [PLATFORM.FIREFOX_MV2]: false,
+        [PLATFORM.THUNDERBIRD]: false,
+    };
+    let allPlatforms = true;
+    for (const arg of args) {
+        if (argMap[arg]) {
+            platforms[argMap[arg]] = true;
+            allPlatforms = false;
+        }
+    }
+    if (allPlatforms) {
+        Object.keys(platforms).forEach((platform) => platforms[platform] = true);
+    }
+
+    // TODO(Anton): remove me
+    if (platforms[PLATFORM.FIREFOX_MV3]) {
+        platforms[PLATFORM.FIREFOX_MV3] = false;
+        console.log('Firefox MV3 build is not supported yet');
+    }
 
     const versionArg = args.find((a) => a.startsWith('--version='));
     const version = versionArg ? versionArg.substring('--version='.length) : null;
@@ -110,9 +136,8 @@ function getParams(args) {
     const log = logWarn ? 'warn' : (logInfo ? 'info' : null);
 
     const test = args.includes('--test');
-    const api = allPlatforms || args.includes('--api');
 
-    return {api, release, debug, platforms, watch, log, test, version};
+    return {release, debug, platforms, watch, log, test, version};
 }
 
 const args = process.argv.slice(2);

src/background/index.ts
@@ -52,7 +52,7 @@ declare const __LOG__: string | false;
 declare const __PORT__: number;
 declare const __TEST__: boolean;
 declare const __CHROMIUM_MV3__: boolean;
-declare const __FIREFOX__: boolean;
+declare const __FIREFOX_MV2__: boolean;
 
 if (__CHROMIUM_MV3__) {
     chrome.runtime.onInstalled.addListener(async () => {
@@ -144,7 +144,7 @@ if (__TEST__) {
     chrome.tabs.create({url: chrome.runtime.getURL('/ui/devtools/index.html'), active: false});
 
     let testTabId: number | null = null;
-    if (__FIREFOX__) {
+    if (__FIREFOX_MV2__) {
         chrome.tabs.create({url: 'about:blank', active: true}, ({id}) => testTabId = id!);
     }
 

src/background/tab-manager.ts
@@ -12,7 +12,7 @@ import {makeFirefoxHappy} from './make-firefox-happy';
 
 declare const __CHROMIUM_MV2__: boolean;
 declare const __CHROMIUM_MV3__: boolean;
-declare const __FIREFOX__: boolean;
+declare const __FIREFOX_MV2__: boolean;
 declare const __THUNDERBIRD__: boolean;
 
 // ContextId is a number on Firefox and documentId is a string in Chromium,
@@ -105,7 +105,7 @@ export default class TabManager {
                     const tabURL = sender.tab!.url!;
                     const {frameId} = sender;
                     const url = sender.url!;
-                    const documentId: documentId = (__CHROMIUM_MV3__ || __CHROMIUM_MV2__) ? (sender as any).documentId : ((__FIREFOX__ || __THUNDERBIRD__) ? (sender as any).contextId : null);
+                    const documentId: documentId = (__CHROMIUM_MV3__ || __CHROMIUM_MV2__) ? (sender as any).documentId : ((__FIREFOX_MV2__ || __THUNDERBIRD__) ? (sender as any).contextId : null);
 
                     TabManager.addFrame(tabId, frameId!, documentId, url, TabManager.timestamp);
 

src/inject/index.ts
@@ -16,7 +16,7 @@ let darkReaderDynamicThemeStateForTesting: 'loading' | 'ready' = 'loading';
 
 declare const __CHROMIUM_MV3__: boolean;
 declare const __THUNDERBIRD__: boolean;
-declare const __FIREFOX__: boolean;
+declare const __FIREFOX_MV2__: boolean;
 
 function cleanup() {
     unloaded = true;
@@ -227,7 +227,7 @@ if (__TEST__) {
     };
 
     // TODO(anton): remove this once Firefox supports tab.eval() via WebDriver BiDi
-    if (__FIREFOX__) {
+    if (__FIREFOX_MV2__) {
         function expectPageStyles(data: any) {
             const errors = [];
             const expectations = Array.isArray(data[0]) ? data : [data];

src/utils/platform.ts
@@ -1,6 +1,6 @@
 declare const __CHROMIUM_MV2__: boolean;
 declare const __CHROMIUM_MV3__: boolean;
-declare const __FIREFOX__: boolean;
+declare const __FIREFOX_MV2__: boolean;
 declare const __THUNDERBIRD__: boolean;
 declare const __TEST__: boolean;
 
@@ -29,13 +29,13 @@ const platform = isNavigatorDefined ? (navigator.userAgentData && typeof navigat
     : 'some platform';
 
 // Note: if you are using these constants in tests, make sure they are not compiled out by adding __TEST__ to them
-export const isChromium = __CHROMIUM_MV2__ || __CHROMIUM_MV3__ || (!__FIREFOX__ && !__THUNDERBIRD__ && (userAgent.includes('chrome') || userAgent.includes('chromium')));
-export const isFirefox = __FIREFOX__ || __THUNDERBIRD__ || ((__TEST__ || (!__CHROMIUM_MV2__ && !__CHROMIUM_MV3__)) && (userAgent.includes('firefox') || userAgent.includes('thunderbird') || userAgent.includes('librewolf')));
-export const isVivaldi = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('vivaldi'));
-export const isYaBrowser = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('yabrowser'));
-export const isOpera = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && (userAgent.includes('opr') || userAgent.includes('opera')));
-export const isEdge = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('edg'));
-export const isSafari = !__CHROMIUM_MV2__ && !__CHROMIUM_MV3__ && !__FIREFOX__ && !__THUNDERBIRD__ && userAgent.includes('safari') && !isChromium;
+export const isChromium = __CHROMIUM_MV2__ || __CHROMIUM_MV3__ || (!__FIREFOX_MV2__ && !__THUNDERBIRD__ && (userAgent.includes('chrome') || userAgent.includes('chromium')));
+export const isFirefox = __FIREFOX_MV2__ || __THUNDERBIRD__ || ((__TEST__ || (!__CHROMIUM_MV2__ && !__CHROMIUM_MV3__)) && (userAgent.includes('firefox') || userAgent.includes('thunderbird') || userAgent.includes('librewolf')));
+export const isVivaldi = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX_MV2__ && !__THUNDERBIRD__ && userAgent.includes('vivaldi'));
+export const isYaBrowser = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX_MV2__ && !__THUNDERBIRD__ && userAgent.includes('yabrowser'));
+export const isOpera = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX_MV2__ && !__THUNDERBIRD__ && (userAgent.includes('opr') || userAgent.includes('opera')));
+export const isEdge = (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (!__FIREFOX_MV2__ && !__THUNDERBIRD__ && userAgent.includes('edg'));
+export const isSafari = !__CHROMIUM_MV2__ && !__CHROMIUM_MV3__ && !__FIREFOX_MV2__ && !__THUNDERBIRD__ && userAgent.includes('safari') && !isChromium;
 export const isWindows = platform.startsWith('win');
 export const isMacOS = platform.startsWith('mac');
 export const isMobile = (isNavigatorDefined && navigator.userAgentData) ? navigator.userAgentData.mobile : userAgent.includes('mobile');
@@ -46,11 +46,11 @@ export const isMatchMediaChangeEventListenerSupported = __CHROMIUM_MV3__ || (
 );
 // Return true if browser is known to have a bug with Media Queries, specifically Chromium on Linux and Kiwi on Android
 // We assume that if we are on Android, then we are running in Kiwi since it is the only mobile browser we can install Dark Reader in
-export const isMatchMediaChangeEventListenerBuggy = !__TEST__ && !__FIREFOX__ && !__THUNDERBIRD__ && (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (
+export const isMatchMediaChangeEventListenerBuggy = !__TEST__ && !__FIREFOX_MV2__ && !__THUNDERBIRD__ && (__CHROMIUM_MV2__ || __CHROMIUM_MV3__) && (
     ((isNavigatorDefined && navigator.userAgentData) && ['Linux', 'Android'].includes(navigator.userAgentData.platform))
     || platform.startsWith('linux'));
 // Note: make sure that this value matches manifest.json keys
-export const isNonPersistent = !__FIREFOX__ && !__THUNDERBIRD__ && (__CHROMIUM_MV3__ || isSafari);
+export const isNonPersistent = !__FIREFOX_MV2__ && !__THUNDERBIRD__ && (__CHROMIUM_MV3__ || isSafari);
 
 export const chromiumVersion = (() => {
     const m = userAgent.match(/chrom(?:e|ium)(?:\/| )([^ ]+)/);

tasks/bundle-api.js
@@ -48,7 +48,7 @@ async function bundleAPI({debug, watch}) {
                 __DEBUG__: false,
                 __CHROMIUM_MV2__: false,
                 __CHROMIUM_MV3__: false,
-                __FIREFOX__: false,
+                __FIREFOX_MV2__: false,
                 __THUNDERBIRD__: false,
                 __TEST__: false,
             }),

tasks/bundle-js.js
@@ -18,7 +18,7 @@ const {getDestDir, PLATFORM, rootDir, rootPath} = paths;
  * @property {string | ((platform: string) => string)} dest
  * @property {string} reloadType
  * @property {string[]} [watchFiles]
- * @property {(typeof PLATFORM.CHROME) | undefined} [platform]
+ * @property {(typeof PLATFORM.CHROMIUM_MV3) | undefined} [platform]
  */
 
 /** @type {JSEntry[]} */
@@ -26,7 +26,7 @@ const jsEntries = [
     {
         src: 'src/background/index.ts',
         // Prior to Chrome 93, background service worker had to be in top-level directory
-        dest: (platform) => platform === PLATFORM.CHROME_MV3 ? 'background.js' : 'background/index.js',
+        dest: (platform) => platform === PLATFORM.CHROMIUM_MV3 ? 'background.js' : 'background/index.js',
         reloadType: reload.FULL,
     },
     {
@@ -38,7 +38,7 @@ const jsEntries = [
         src: 'src/inject/dynamic-theme/mv3-proxy.ts',
         dest: 'inject/proxy.js',
         reloadType: reload.FULL,
-        platform: PLATFORM.CHROME_MV3,
+        platform: PLATFORM.CHROMIUM_MV3,
     },
     {
         src: 'src/inject/fallback.ts',
@@ -49,7 +49,7 @@ const jsEntries = [
         src: 'src/inject/color-scheme-watcher.ts',
         dest: 'inject/color-scheme-watcher.js',
         reloadType: reload.FULL,
-        platform: PLATFORM.CHROME_MV3,
+        platform: PLATFORM.CHROMIUM_MV3,
     },
     {
         src: 'src/ui/devtools/index.tsx',
@@ -102,7 +102,7 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
     const destination = typeof dest === 'string' ? dest : dest(platform);
     let replace = {};
     switch (platform) {
-        case PLATFORM.FIREFOX:
+        case PLATFORM.FIREFOX_MV2:
         case PLATFORM.THUNDERBIRD:
             if (entry.src === 'src/ui/popup/index.tsx') {
                 break;
@@ -112,7 +112,7 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
                 'chrome.fontSettings': `chrome['font' + 'Settings']`
             };
             break;
-        case PLATFORM.CHROME_MV3:
+        case PLATFORM.CHROMIUM_MV3:
             replace = {
                 'chrome.browserAction.setIcon': 'chrome.action.setIcon',
                 'chrome.browserAction.setBadgeBackgroundColor': 'chrome.action.setBadgeBackgroundColor',
@@ -123,7 +123,7 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
 
     // See comment below
     // TODO(anton): remove this once Firefox supports tab.eval() via WebDriver BiDi
-    const mustRemoveEval = !test && (platform === PLATFORM.FIREFOX) && (entry.src === 'src/inject/index.ts');
+    const mustRemoveEval = !test && (platform === PLATFORM.FIREFOX_MV2) && (entry.src === 'src/inject/index.ts');
 
     const bundle = await rollup.rollup({
         input: rootPath(src),
@@ -154,7 +154,7 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
                     rootDir,
                     typescript,
                     tsconfig: rootPath('src/tsconfig.json'),
-                    compilerOptions: platform === PLATFORM.CHROME_MV3 ? {
+                    compilerOptions: platform === PLATFORM.CHROMIUM_MV3 ? {
                         target: 'ES2022',
                     } : undefined,
                     noImplicitAny: debug ? false : true,
@@ -171,9 +171,9 @@ async function bundleJS(/** @type {JSEntry} */entry, platform, debug, watch, log
                     preventAssignment: true,
                     ...replace,
                     __DEBUG__: debug,
-                    __CHROMIUM_MV2__: platform === PLATFORM.CHROME,
-                    __CHROMIUM_MV3__: platform === PLATFORM.CHROME_MV3,
-                    __FIREFOX__: platform === PLATFORM.FIREFOX,
+                    __CHROMIUM_MV2__: platform === PLATFORM.CHROMIUM_MV2,
+                    __CHROMIUM_MV3__: platform === PLATFORM.CHROMIUM_MV3,
+                    __FIREFOX_MV2__: platform === PLATFORM.FIREFOX_MV2,
                     __THUNDERBIRD__: platform === PLATFORM.THUNDERBIRD,
                     __PORT__: watch ? String(PORT) : '-1',
                     __TEST__: test,

tasks/bundle-manifest.js
@@ -7,19 +7,22 @@ const {PLATFORM, getDestDir, rootPath} = path;
 
 async function patchManifest(platform, debug, watch, test) {
     const manifest = await readJSON(rootPath('src/manifest.json'));
-    const manifestPatch = platform === PLATFORM.CHROME ? {} : await readJSON(rootPath(`src/manifest-${platform}.json`));
+    const manifestPatch = platform === PLATFORM.CHROMIUM_MV2 ? {} : await readJSON(rootPath(`src/manifest-${platform}.json`));
     const patched = {...manifest, ...manifestPatch};
-    if (platform === PLATFORM.CHROME_MV3) {
+    if (debug && platform === PLATFORM.CHROMIUM_MV3) {
+        patched.name = 'Dark Reader MV3';
+    }
+    if (platform === PLATFORM.CHROMIUM_MV3) {
         patched.browser_action = undefined;
     }
     if (debug) {
         patched.version = '1';
         patched.description = `Debug build, platform: ${platform}, watch: ${watch ? 'yes' : 'no'}.`;
     }
-    if (debug && !test && platform === PLATFORM.CHROME_MV3) {
+    if (debug && !test && platform === PLATFORM.CHROMIUM_MV3) {
         patched.permissions.push('tabs');
     }
-    if (debug && (platform === PLATFORM.CHROME || platform === PLATFORM.CHROME_MV3)) {
+    if (debug && (platform === PLATFORM.CHROMIUM_MV2 || platform === PLATFORM.CHROMIUM_MV3)) {
         patched.version_name = 'Debug';
     }
     return patched;

commit 952efd970198af43c9f7a64d61bd270490cd7fa0

Fix: fix build script regression (#11233)
Fix regression introduced in commit e6a0651cfa11f4c2311cef5f9896a02423e06f8f

tasks/build.js
@@ -42,6 +42,10 @@ const signedBuildTask = [
 
 async function build({platforms, debug, watch, log: logging, test, version}) {
     log.ok('BUILD');
+    platforms = {
+        ...platforms,
+        [PLATFORM.API]: false
+    };
     try {
         await runTasks(debug ? standardTask : (version ? signedBuildTask : buildTask), {platforms, debug, watch, log: logging, test, version});
         if (watch) {
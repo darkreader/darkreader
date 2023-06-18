c729e5ef3ef953b891f14eb0553b852230e35c8f
Your Name <you@example.com>
2023-06-19 00:34:48 +0300

    wip

--- tasks/bundle-html.js
+++ tasks/bundle-html.js
@@ -2,7 +2,7 @@
 import paths_ from './paths.js';
 import * as reload from './reload.js';
 import {createTask} from './task.js';
-import {copyFile, getPaths} from './utils.js';
+import {copyFile, getPaths, writeFile} from './utils.js';
 const {getDestDir, PLATFORM} = paths_;
 
 const srcDir = 'src';
@@ -10,27 +10,46 @@ const srcDir = 'src';
 /**
  * @typedef copyEntry
  * @property {string} dest
+ * @property {string} text
  * @property {string} reloadType
  * @property {(typeof PLATFORM.CHROMIUM_MV3)[] | undefined} [platforms]
  */
+
+const text  = `\
+<!DOCTYPE html>\
+<html>\
+\
+<head>\
+    <meta charset="utf-8">\
+    <title>Dark Reader background</title>\
+    <script src="index.js"></script>\
+</head>\
+\
+<body></body>\
+\
+</html>`;
 
 /** @type {copyEntry[]} */
 const copyEntries = [
     {
         dest: 'background/index.html',
+        text,
         reloadType: reload.FULL,
         platforms: [PLATFORM.CHROMIUM_MV2, PLATFORM.FIREFOX_MV2, PLATFORM.THUNDERBIRD],
     },
     {
         dest: 'ui/popup/index.html',
+        text,
         reloadType: reload.UI,
     },
     {
         dest: 'ui/devtools/index.html',
+        text,
         reloadType: reload.UI,
     },
     {
         dest: 'ui/stylesheet-editor/index.html',
+        text,
         reloadType: reload.UI,
     },
 ];
@@ -47,6 +66,14 @@ async function copyEntry(path, {debug, platform}) {
     const src = `${srcDir}/${cwdPath}`;
     const dest = `${destDir}/${cwdPath}`;
     await copyFile(src, dest);
+}
+
+async function writeEntry(path, {debug, platform}) {
+    const cwdPath = getCwdPath(path);
+    const destDir = getDestDir({debug, platform});
+    const src = `${srcDir}/${cwdPath}`;
+    const dest = `${destDir}/${cwdPath}`;
+    await writeFile(dest, '');
 }
 
 async function bundleHTML({platforms, debug}) {

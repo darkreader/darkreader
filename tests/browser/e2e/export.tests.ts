d03e7a8628451db429f1433cde6c112d75b1c34c
Your Name <you@example.com>
2023-06-29 21:33:04 +0300

    f

--- tests/browser/e2e/export.tests.ts
+++ tests/browser/e2e/export.tests.ts
@@ -4,7 +4,7 @@ describe('Export settings', () => {
     // TODO: remove flakes and remove this line
     jest.retryTimes(10, {logErrorsBeforeRetry: true});
 
-    it('Should trigger settings export', async () => {
+    it('Should download file', async () => {
         await timeout(1000);
         const p = new Promise<void>((resolve) => backgroundUtils.onDownload(resolve));
         await popupUtils.saveFile('example', 'content');

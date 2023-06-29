2430e44e4f32b87a0365e738bd85666a90580909
Your Name <you@example.com>
2023-06-29 22:07:58 +0300

    f

--- tests/browser/e2e/export.tests.ts
+++ tests/browser/e2e/export.tests.ts
@@ -9,5 +9,8 @@ describe('Export settings', () => {
         const p = new Promise<{ok: boolean}>((resolve) => backgroundUtils.onDownload(resolve));
         await popupUtils.saveFile('example', 'content');
         expect((await p).ok).toBe(true);
+        if (product === 'firefox') {
+            await timeout(1000);
+        }
     });
 });

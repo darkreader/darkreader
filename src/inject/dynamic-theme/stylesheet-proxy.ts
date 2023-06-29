darkreader/darkreader

commit bec13d4d10457e532b16c2f393b68bb4b9cd0796

Fix: check for existence of HTMLStyleElement.classList (#11342)

src/inject/dynamic-theme/stylesheet-proxy.ts
@@ -73,7 +73,7 @@ export function injectProxy(enableStyleSheetsProxy: boolean, enableCustomElement
 
     function proxyAddRule(selector?: string, style?: string, index?: number): number {
         addRuleDescriptor!.value.call(this, selector, style, index);
-        if (this.ownerNode && !this.ownerNode.classList.contains('darkreader')) {
+        if (this.ownerNode && !(this.ownerNode.classList && this.ownerNode.classList.contains('darkreader'))) {
             this.ownerNode.dispatchEvent(updateSheetEvent);
         }
         // Should always returns -1 https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet/addRule#Return_value.
@@ -82,7 +82,7 @@ export function injectProxy(enableStyleSheetsProxy: boolean, enableCustomElement
 
     function proxyInsertRule(rule: string, index?: number): number {
         const returnValue = insertRuleDescriptor!.value.call(this, rule, index);
-        if (this.ownerNode && !this.ownerNode.classList.contains('darkreader')) {
+        if (this.ownerNode && !(this.ownerNode.classList && this.ownerNode.classList.contains('darkreader'))) {
             this.ownerNode.dispatchEvent(updateSheetEvent);
         }
         return returnValue;
@@ -90,14 +90,14 @@ export function injectProxy(enableStyleSheetsProxy: boolean, enableCustomElement
 
     function proxyDeleteRule(index: number): void {
         deleteRuleDescriptor!.value.call(this, index);
-        if (this.ownerNode && !this.ownerNode.classList.contains('darkreader')) {
+        if (this.ownerNode && !(this.ownerNode.classList && this.ownerNode.classList.contains('darkreader'))) {
             this.ownerNode.dispatchEvent(updateSheetEvent);
         }
     }
 
     function proxyRemoveRule(index?: number): void {
         removeRuleDescriptor!.value.call(this, index);
-        if (this.ownerNode && !this.ownerNode.classList.contains('darkreader')) {
+        if (this.ownerNode && !(this.ownerNode.classList && this.ownerNode.classList.contains('darkreader'))) {
             this.ownerNode.dispatchEvent(updateSheetEvent);
         }
     }
@@ -106,13 +106,11 @@ export function injectProxy(enableStyleSheetsProxy: boolean, enableCustomElement
         const getCurrentValue = () => {
             const docSheets: StyleSheetList = documentStyleSheetsDescriptor!.get!.call(this);
 
-            const filteredSheets = [...docSheets].filter((styleSheet) => {
-                return styleSheet.ownerNode && !(styleSheet.ownerNode as Exclude<typeof styleSheet.ownerNode, ProcessingInstruction>).classList.contains('darkreader');
-            });
+            const filteredSheets = [...docSheets].filter((styleSheet) =>
+                styleSheet.ownerNode && !(styleSheet.ownerNode as Exclude<typeof styleSheet.ownerNode, ProcessingInstruction>).classList && (styleSheet.ownerNode as Exclude<typeof styleSheet.ownerNode, ProcessingInstruction>).classList.contains('darkreader')
+            );
 
-            (filteredSheets as unknown as StyleSheetList).item = (item: number) => {
-                return filteredSheets[item];
-            };
+            (filteredSheets as unknown as StyleSheetList).item = (item: number) => filteredSheets[item];
 
             return Object.setPrototypeOf(filteredSheets, StyleSheetList.prototype);
         };
@@ -144,9 +142,9 @@ export function injectProxy(enableStyleSheetsProxy: boolean, enableCustomElement
         const getCurrentElementValue = () => {
             const elements: NodeListOf<HTMLElement> = getElementsByTagNameDescriptor!.value.call(this, tagName);
 
-            return Object.setPrototypeOf([...elements].filter((element: HTMLElement) => {
-                return !element.classList.contains('darkreader');
-            }), NodeList.prototype);
+            return Object.setPrototypeOf([...elements].filter((element: HTMLElement) =>
+                element && !(element.classList && element.classList.contains('darkreader'))
+            ), NodeList.prototype);
         };
 
         let elements = getCurrentElementValue();

src/inject/detector.ts
@@ -24,7 +24,7 @@ function hasSomeStyle() {
         return true;
     }
     for (const style of document.styleSheets) {
-        if (style && style.ownerNode && !(style.ownerNode as HTMLElement).classList.contains('darkreader')) {
+        if (style && style.ownerNode && !((style.ownerNode as HTMLElement).classList && (style.ownerNode as HTMLElement).classList.contains('darkreader'))) {
             return true;
         }
     }

commit 11cdeaf339d6dfa4b7331c8f32c06ca8c1624e61

Fix: broken `document.styleSheets` proxy (#11454)
A logic error introduced in bec13d4d1 breaks the `document.styleSheets` proxy, filtering out _all_ stylesheets and style tags, preventing sites from dynamically accessing and altering their own styles.

Fixes #11454 and #11446

src/inject/dynamic-theme/stylesheet-proxy.ts
@@ -107,7 +107,7 @@ export function injectProxy(enableStyleSheetsProxy: boolean, enableCustomElement
             const docSheets: StyleSheetList = documentStyleSheetsDescriptor!.get!.call(this);
 
             const filteredSheets = [...docSheets].filter((styleSheet) =>
-                styleSheet.ownerNode && !(styleSheet.ownerNode as Exclude<typeof styleSheet.ownerNode, ProcessingInstruction>).classList && (styleSheet.ownerNode as Exclude<typeof styleSheet.ownerNode, ProcessingInstruction>).classList.contains('darkreader')
+                styleSheet.ownerNode && !(styleSheet.ownerNode as Exclude<typeof styleSheet.ownerNode, ProcessingInstruction>).classList?.contains('darkreader')
             );
 
             (filteredSheets as unknown as StyleSheetList).item = (item: number) => filteredSheets[item];
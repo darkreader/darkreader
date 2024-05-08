d220d4ae08acf9dda3a45e0498e05da7bdf5af24
oberrich <6305520+oberrich@users.noreply.github.com>
2024-05-08 20:29:46 +0200

    fix: linter errors

--- src/generators/text-style.ts
+++ src/generators/text-style.ts
@@ -2,27 +2,27 @@ import type {Theme} from '../definitions';
 
 export function createTextStyle(config: Theme): string {
     // Blacklist for common code elements and icon/symbol fonts
-    const blacklistedSelectors = [ 
-      // common html code elements
-      'pre', 'pre *', 'code',
-      // generic matches for icon/symbol fonts
-      '.icofont', '[style*="font-"]',
-      '[class*="icon"]', '[class*="Icon"]',
-      '[class*="symbol"]', '[class*="Symbol"]',
-      // see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-hidden#description
-      '[aria-hidden="true"]', 
-      // vendor: font awesome
-      '.fa', '.fab', '.fas', '.far', '[class*="fa-"]',
-      // vendor: material design
-      '[class*="material-symbol"]', '[class*="material-icon"]',
-      // vendor: glyph icons
-      '.glyphicon',
-      // vendor: videojs font
-      '[class*="vjs-"]',
-      // vendor: typicons
-      '.typcn',
-      // vendor: mui?
-      'mu', '[class*="mu-"]'
+    const blacklistedSelectors = [
+        // common html code elements
+        'pre', 'pre *', 'code',
+        // generic matches for icon/symbol fonts
+        '.icofont', '[style*="font-"]',
+        '[class*="icon"]', '[class*="Icon"]',
+        '[class*="symbol"]', '[class*="Symbol"]',
+        // see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-hidden#description
+        '[aria-hidden="true"]', 
+        // vendor: font awesome
+        '.fa', '.fab', '.fas', '.far', '[class*="fa-"]',
+        // vendor: material design
+        '[class*="material-symbol"]', '[class*="material-icon"]',
+        // vendor: glyph icons
+        '.glyphicon',
+        // vendor: videojs font
+        '[class*="vjs-"]',
+        // vendor: typicons
+        '.typcn',
+        // vendor: mui?
+        'mu', '[class*="mu-"]'
     ].join(', ');
 
     const lines: string[] = [];

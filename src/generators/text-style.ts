8b41e533243e1fd9df205afd1c84d5c17a7893b2
oberrich <6305520+oberrich@users.noreply.github.com>
2024-05-08 20:26:07 +0200

    refactor: dynamically create blacklist of css selectors
    This commit dynamically builds the blacklist as opposed to hardcoding it as string, this allows us to properly document each selector and provide context.

--- src/generators/text-style.ts
+++ src/generators/text-style.ts
@@ -1,11 +1,33 @@
 import type {Theme} from '../definitions';
 
 export function createTextStyle(config: Theme): string {
-    const lines: string[] = [];
-    // Don't target pre elements as they are preformatted element's e.g. code blocks
-    // Exclude font libraries to preserve icons
-    lines.push('*:not(pre, pre *, code, [class*="icon"], [class*="Icon"], [class*="symbol"], [class*="Symbol"], [aria-hidden="true"], .far, .fa, [class*="fa-"] .glyphicon, [class*="vjs-"], .fab, .fas, .icon-asset, [class*="material-symbol"], [style*="font-"], .material-icons, .icofont, .typcn, mu, [class*="mu-"], .glyphicon) {');
+    // Blacklist for common code elements and icon/symbol fonts
+    const blacklistedSelectors = [ 
+      // common html code elements
+      'pre', 'pre *', 'code',
+      // generic matches for icon/symbol fonts
+      '.icofont', '[style*="font-"]',
+      '[class*="icon"]', '[class*="Icon"]',
+      '[class*="symbol"]', '[class*="Symbol"]',
+      // see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-hidden#description
+      '[aria-hidden="true"]', 
+      // vendor: font awesome
+      '.fa', '.fab', '.fas', '.far', '[class*="fa-"]',
+      // vendor: material design
+      '[class*="material-symbol"]', '[class*="material-icon"]',
+      // vendor: glyph icons
+      '.glyphicon',
+      // vendor: videojs font
+      '[class*="vjs-"]',
+      // vendor: typicons
+      '.typcn',
+      // vendor: mui?
+      'mu', '[class*="mu-"]'
+    ].join(', ');
 
+    const lines: string[] = [];
+    lines.push(`*:not(${ blacklistedSelectors }) {`);
+    
     if (config.useFont && config.fontFamily) {
         // TODO: Validate...
         lines.push(`  font-family: ${config.fontFamily} !important;`);

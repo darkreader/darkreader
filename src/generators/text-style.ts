cd906a1f9f4fe439b69f3d759214d945a93be9be
oberrich <6305520+oberrich@users.noreply.github.com>
2024-05-08 19:22:43 +0200

    add uppercase variants to generic icon font matching

--- src/generators/text-style.ts
+++ src/generators/text-style.ts
@@ -4,7 +4,7 @@ export function createTextStyle(config: Theme): string {
     const lines: string[] = [];
     // Don't target pre elements as they are preformatted element's e.g. code blocks
     // Exclude font libraries to preserve icons
-    lines.push('*:not(pre, pre *, code, [class*="icon"], [class*="symbol"], [aria-hidden="true"], .far, .fa, [class*="fa-"] .glyphicon, [class*="vjs-"], .fab, .fas, .icon-asset, [class*="material-symbol"], [style*="font-"], .material-icons, .icofont, .typcn, mu, [class*="mu-"], .glyphicon) {');
+    lines.push('*:not(pre, pre *, code, [class*="icon"], [class*="Icon"], [class*="symbol"], [class*="Symbol"], [aria-hidden="true"], .far, .fa, [class*="fa-"] .glyphicon, [class*="vjs-"], .fab, .fas, .icon-asset, [class*="material-symbol"], [style*="font-"], .material-icons, .icofont, .typcn, mu, [class*="mu-"], .glyphicon) {');
 
     if (config.useFont && config.fontFamily) {
         // TODO: Validate...

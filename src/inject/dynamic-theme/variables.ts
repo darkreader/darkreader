aeb7b18c1372e7af87b19fc434f3da6719d35463
Alexander Shutau <shutovby@gmail.com>
2024-06-12 21:30:11 +0100

    Handle named background color fallback

--- src/inject/dynamic-theme/variables.ts
+++ src/inject/dynamic-theme/variables.ts
@@ -295,7 +295,7 @@ export class VariablesStore {
                 if (unknownVars.size > 0) {
                     // web.dev and voice.google.com issue where the variable is never defined, but the fallback is.
                     // TODO: Return a fallback value along with a way to subscribe for a change.
-                    const isFallbackResolved = modified.match(/^var\(.*?, (var\(--darkreader-bg--.*\))|(#[0-9A-Fa-f]+)|(rgba?\(.+\))|(hsla?\(.+\))\)$/);
+                    const isFallbackResolved = modified.match(/^var\(.*?, (var\(--darkreader-bg--.*\))|(#[0-9A-Fa-f]+)|([a-z]+)|(rgba?\(.+\))|(hsla?\(.+\))\)$/);
                     if (isFallbackResolved) {
                         return modified;
                     }

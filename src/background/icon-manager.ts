a5e003fb8921481c425fb6bbe224506e954f2c92
Akshat Shukla <shuklaakshat.217@gmail.com>
2025-01-06 19:48:13 +0530

    Show different Extension icons for Disabled, Enabled, Dark and Light Modes (#13596)
    * Show different Extension icons for Disabled, Enabled, Dark and Light Modes
    
    * setTheme in onSettingsChanged
    
    * Fix changed files
    
    * Fix package-lock.json
    
    * Use single setIcon function
    
    * Fix Linting issues
    
    * Remove Extra space
    
    * Avoid manifest changes
    
    * Avoid manifest changes
    
    ---------
    
    Co-authored-by: Alexander Shutau <shutovby@gmail.com>

--- src/background/icon-manager.ts
+++ src/background/icon-manager.ts
@@ -1,3 +1,4 @@
+import {FilterMode} from '../generators/css-filter';
 import {isNonPersistent} from '../utils/platform';
 
 declare const __THUNDERBIRD__: boolean;
@@ -9,13 +10,21 @@ interface IconState {
 
 export default class IconManager {
     private static readonly ICON_PATHS = {
-        active: {
+        activeDark: {
             19: '../icons/dr_active_19.png',
             38: '../icons/dr_active_38.png',
         },
-        inactive: {
-            19: '../icons/dr_inactive_19.png',
-            38: '../icons/dr_inactive_38.png',
+        activeLight: {
+            19: '../icons/dr_active_light_19.png',
+            38: '../icons/dr_active_light_38.png',
+        },
+        inactiveDark: {
+            19: '../icons/dr_inactive_dark_19.png',
+            38: '../icons/dr_inactive_dark_38.png',
+        },
+        inactiveLight: {
+            19: '../icons/dr_inactive_light_19.png',
+            38: '../icons/dr_inactive_light_38.png',
         },
     };
 
@@ -48,27 +57,26 @@ export default class IconManager {
         }
     }
 
-    static setActive(): void {
-        if (__THUNDERBIRD__ || !chrome.browserAction.setIcon) {
-            // Fix for Firefox Android and Thunderbird.
-            return;
-        }
-        IconManager.iconState.active = true;
-        chrome.browserAction.setIcon({
-            path: IconManager.ICON_PATHS.active,
-        });
-        IconManager.handleUpdate();
-    }
 
-    static setInactive(): void {
+    static setIcon({isActive = this.iconState.active, mode}: {isActive?: boolean; mode: FilterMode}): void {
         if (__THUNDERBIRD__ || !chrome.browserAction.setIcon) {
             // Fix for Firefox Android and Thunderbird.
             return;
         }
-        IconManager.iconState.active = false;
+
+        this.iconState.active = isActive;
+
+        let path = this.ICON_PATHS.activeDark;
+        if (isActive) {
+            path = (mode === FilterMode.dark) ? IconManager.ICON_PATHS.activeDark : IconManager.ICON_PATHS.activeLight;
+        } else {
+            path = (mode === FilterMode.dark) ? IconManager.ICON_PATHS.inactiveDark : IconManager.ICON_PATHS.inactiveLight;
+        }
+
         chrome.browserAction.setIcon({
-            path: IconManager.ICON_PATHS.inactive,
+            path: path,
         });
+
         IconManager.handleUpdate();
     }
 

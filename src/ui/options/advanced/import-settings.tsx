a7ec4dd3dbac25728305f81190e911e088c5f28c
dphin <vopa@oregonstate.edu>
2025-03-09 16:19:39 -0700

    Ran code-style and test

--- src/ui/options/advanced/import-settings.tsx
+++ src/ui/options/advanced/import-settings.tsx
@@ -41,7 +41,7 @@ export function ImportSettings(props: ViewProps): Malevic.Child {
 
     const warningDialog = context.store.isWarningDialogVisible ? (
         <MessageBox
-            caption='Are you sure you want to import? Please export your current settings if you would like to restore them later.'
+            caption="Are you sure you want to import? Please export your current settings if you would like to restore them later."
             onOK={importSettings}
             onCancel={hideWarningDialog}
         />

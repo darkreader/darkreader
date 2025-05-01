60aec55e09726456c26cb17ac8c9e8eb424ce7d0
Alexander Shutau <shutovby@gmail.com>
2025-05-01 17:37:15 +0100

    Fix build error

--- src/background/extension.ts
+++ src/background/extension.ts
@@ -355,7 +355,7 @@ export class Extension {
 
     private static registerContextMenus() {
         chrome.contextMenus.onClicked.addListener(async ({menuItemId, frameId, frameUrl, pageUrl}, tab) =>
-            Extension.onCommand(menuItemId as Command, tab && tab.id || null, frameId || null, frameUrl || pageUrl));
+            Extension.onCommand(menuItemId as Command, tab && tab.id || null, frameId || null, frameUrl || pageUrl || null));
         chrome.contextMenus.removeAll(() => {
             Extension.registeredContextMenus = false;
             chrome.contextMenus.create({

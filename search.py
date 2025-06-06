aece3ec9e7d002c80c9440d03b1d7124a7c8e1ea
moodynooby <immanasdoshi@gmail.com>
2025-06-06 19:15:17 +0530

    Clean UP

--- search.py
+++ search.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     # Set up your search configuration here
     SEARCH_DIRECTORY = "."  # Current directory, change this to your target folder
     SEARCH_WORDS = [input()]  # Words to search for
-    FILE_TYPES =  { '.css', '.html','.js','.ts','.tsx', '.less',} # File types to search in
+    FILE_TYPES =  { '.css', '.html','.js','.ts','.tsx', '.less','.json'} # File types to search in
     EXCLUDE_FOLDERS = {'node_modules', 'venv', '.github', '__pycache__', 'dist' ,'ext' , 'docs' , 'bundles' ,'_locales','tests','build'}  # Folders to skip
     WHOLE_WORD = False  # Set to True to match whole words only
 

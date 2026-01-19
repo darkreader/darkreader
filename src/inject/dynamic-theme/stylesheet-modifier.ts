f4088eb593b732bdabb86e4d39c219f3e3b30663
Alexander Shutau <shutovby@gmail.com>
2026-01-19 18:53:27 +0000

    Fix overriding background-size, -position and -repeat

--- src/inject/dynamic-theme/stylesheet-modifier.ts
+++ src/inject/dynamic-theme/stylesheet-modifier.ts
@@ -110,6 +110,9 @@ export function createStyleSheetModifier(): StyleSheetModifier {
                 const mod = getModifiableCSSDeclaration(property, value, rule, variablesStore, ignoreImageAnalysis, isAsyncCancelled);
                 if (mod) {
                     modDecs.push(mod);
+                    if (mod.specifics) {
+                        mod.specifics.forEach((ext) => modDecs.push(ext));
+                    }
                 }
             });
 

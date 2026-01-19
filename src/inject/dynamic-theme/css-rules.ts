9c602cb8c4060553f1e4af279cbe80454cfe3719
Alexander Shutau <shutovby@gmail.com>
2026-01-19 14:37:18 +0000

    Handle shorthand props first

--- src/inject/dynamic-theme/css-rules.ts
+++ src/inject/dynamic-theme/css-rules.ts
@@ -70,17 +70,6 @@ const shorthandVarDepPropRegexps = isSafari ? shorthandVarDependantProperties.ma
 }) : null;
 
 export function iterateCSSDeclarations(style: CSSStyleDeclaration, iterate: (property: string, value: string) => void): void {
-    forEach(style, (property) => {
-        const value = style.getPropertyValue(property).trim();
-        if (!value) {
-            return;
-        }
-        iterate(property, value);
-    });
-
-    // Bigger sites like gmail.com and google.com will love this optimization.
-    // As a side-effect, styles with a lot of `var(` will notice a maximum slowdown of ~50ms.
-    // Against the bigger sites that saves around ~150ms+ it's a good win.
     const cssText = style.cssText;
     if (cssText.includes('var(')) {
         if (isSafari) {
@@ -113,6 +102,14 @@ export function iterateCSSDeclarations(style: CSSStyleDeclaration, iterate: (pro
     if (cssText.includes('border-') && cssText.includes('-color: ;') && !style.getPropertyValue('border')) {
         handleEmptyShorthand('border', style, iterate);
     }
+
+    forEach(style, (property) => {
+        const value = style.getPropertyValue(property).trim();
+        if (!value) {
+            return;
+        }
+        iterate(property, value);
+    });
 }
 
 // `rule.cssText` fails when the rule has both

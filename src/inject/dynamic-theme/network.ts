e25364417535c2fab0a23c8bc6314827ceb2e69f
Alexander Shutau <shutovby@gmail.com>
2026-07-22 14:17:20 +0100

    Fix CSS redirect rejection

--- src/inject/dynamic-theme/network.ts
+++ src/inject/dynamic-theme/network.ts
@@ -1,5 +1,6 @@
 import type {MessageBGtoCS, MessageCStoBG} from '../../definitions';
 import {MessageTypeBGtoCS, MessageTypeCStoBG} from '../../utils/message';
+import {shouldIgnoreCors} from '../../utils/network';
 import {generateUID} from '../../utils/uid';
 
 interface FetchRequest {
@@ -8,8 +9,6 @@ interface FetchRequest {
     mimeType?: string;
     origin: string;
 }
-
-declare const __TEST__: boolean;
 
 const resolvers = new Map<string, (data: string) => void>();
 const rejectors = new Map<string, (reason?: any) => void>();
@@ -43,77 +42,3 @@ chrome.runtime.onMessage.addListener(({type, data, error, id}: MessageBGtoCS) =>
         }
     }
 });
-
-const MAX_CORS_HOSTS = 16;
-const corsHosts = new Set<string>();
-const checkedHosts = new Set<string>();
-const localAliases = [
-    '127-0-0-1.org.uk',
-    '42foo.com',
-    'domaincontrol.com',
-    'fbi.com',
-    'fuf.me',
-    'lacolhost.com',
-    'local.sisteminha.com',
-    'localfabriek.nl',
-    'localhost',
-    'localhst.co.uk',
-    'localmachine.info',
-    'localmachine.name',
-    'localtest.me',
-    'lvh.me',
-    'mouse-potato.com',
-    'nip.io',
-    'sslip.io',
-    'vcap.me',
-    'xip.io',
-    'yoogle.com',
-];
-const localSubDomains = [
-    '.corp',
-    '.direct',
-    '.home',
-    '.internal',
-    '.intranet',
-    '.lan',
-    '.local',
-    '.localdomain',
-    '.test',
-    '.zz',
-    ...localAliases.map((alias) => `.${alias}`),
-];
-
-function isIPHost(hostname: string): boolean {
-    if (hostname.startsWith('[')) {
-        return true;
-    }
-    const labels = hostname.split('.');
-    const last = labels[labels.length - 1];
-    return /^(0x[0-9a-f]+|\d+)$/i.test(last);
-}
-
-function shouldIgnoreCors(url: URL) {
-    if (__TEST__) {
-        return false;
-    }
-    const {host, port, protocol} = url;
-    const hostname = url.hostname.endsWith('.') ? url.hostname.slice(0, -1) : url.hostname;
-    if (!corsHosts.has(host)) {
-        corsHosts.add(host);
-    }
-    if (checkedHosts.has(host)) {
-        return false;
-    }
-    if (
-        corsHosts.size >= MAX_CORS_HOSTS ||
-        protocol !== 'https:' ||
-        port !== '' ||
-        localAliases.includes(hostname) ||
-        localSubDomains.some((sub) => hostname.endsWith(sub)) ||
-        isIPHost(hostname)
-    ) {
-        return true;
-    }
-    checkedHosts.add(host);
-    return false;
-}
--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,14 +1,17 @@
 import {isFirefox} from './platform';
 
+declare const __TEST__: boolean;
+
 async function getOKResponse(url: string, mimeType?: string, origin?: string): Promise<Response> {
     const credentials = origin && url.startsWith(`${origin}/`) ? undefined : 'omit';
+    const redirect = mimeType === 'text/css' ? undefined : 'error';
     const response = await fetch(
         url,
         {
             cache: 'force-cache',
             credentials,
             referrer: origin,
-            redirect: 'error',
+            redirect,
         },
     );
 
@@ -19,6 +22,10 @@ async function getOKResponse(url: string, mimeType?: string, origin?: string): P
 
     if (mimeType && !(response.headers.get('Content-Type') === mimeType || response.headers.get('Content-Type')!.startsWith(`${mimeType};`))) {
         throw new Error(`Mime type mismatch when loading ${url}`);
+    }
+
+    if (response.redirected && response.url && shouldIgnoreCors(new URL(response.url))) {
+        throw new Error('Cross-origin limit reached');
     }
 
     if (!response.ok) {
@@ -51,4 +58,78 @@ export async function readResponseAsDataURL(response: Response): Promise<string>
 export async function loadAsText(url: string, mimeType?: string, origin?: string): Promise<string> {
     const response = await getOKResponse(url, mimeType, origin);
     return await response.text();
+}
+
+const MAX_CORS_HOSTS = 16;
+const corsHosts = new Set<string>();
+const checkedHosts = new Set<string>();
+const localAliases = [
+    '127-0-0-1.org.uk',
+    '42foo.com',
+    'domaincontrol.com',
+    'fbi.com',
+    'fuf.me',
+    'lacolhost.com',
+    'local.sisteminha.com',
+    'localfabriek.nl',
+    'localhost',
+    'localhst.co.uk',
+    'localmachine.info',
+    'localmachine.name',
+    'localtest.me',
+    'lvh.me',
+    'mouse-potato.com',
+    'nip.io',
+    'sslip.io',
+    'vcap.me',
+    'xip.io',
+    'yoogle.com',
+];
+const localSubDomains = [
+    '.corp',
+    '.direct',
+    '.home',
+    '.internal',
+    '.intranet',
+    '.lan',
+    '.local',
+    '.localdomain',
+    '.test',
+    '.zz',
+    ...localAliases.map((alias) => `.${alias}`),
+];
+
+function isIPHost(hostname: string): boolean {
+    if (hostname.startsWith('[')) {
+        return true;
+    }
+    const labels = hostname.split('.');
+    const last = labels[labels.length - 1];
+    return /^(0x[0-9a-f]+|\d+)$/i.test(last);
+}
+
+export function shouldIgnoreCors(url: URL) {
+    if (__TEST__) {
+        return false;
+    }
+    const {host, port, protocol} = url;
+    const hostname = url.hostname.endsWith('.') ? url.hostname.slice(0, -1) : url.hostname;
+    if (!corsHosts.has(host)) {
+        corsHosts.add(host);
+    }
+    if (checkedHosts.has(host)) {
+        return false;
+    }
+    if (
+        corsHosts.size >= MAX_CORS_HOSTS ||
+        protocol !== 'https:' ||
+        port !== '' ||
+        localAliases.includes(hostname) ||
+        localSubDomains.some((sub) => hostname.endsWith(sub)) ||
+        isIPHost(hostname)
+    ) {
+        return true;
+    }
+    checkedHosts.add(host);
+    return false;
 }
